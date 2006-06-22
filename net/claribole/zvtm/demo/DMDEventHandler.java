8b4177b07a4a946f7f4da0e9db8b9a9d6ae2ac74
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2006-06-22 14:24:36 +0000

    demonstrating DragMag using a portal
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@61 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ net/claribole/zvtm/demo/DMDEventHandler.java
@@ -0,0 +1,181 @@
+/*   FILE: DMDEventHandler.java
+ *   DATE OF CREATION:  Sat Jun 17 13:58:59 2006
+ *   AUTHOR :           Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   MODIF:             Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   Copyright (c) INRIA, 2004-2006. All Rights Reserved
+ *   Licensed under the GNU LGPL. For full terms see the file COPYING.
+ *
+ * $Id$
+ */ 
+
+package net.claribole.zvtm.demo;
+
+import com.xerox.VTM.engine.*;
+import com.xerox.VTM.glyphs.Glyph;
+import com.xerox.VTM.glyphs.VRectangle;
+
+import net.claribole.zvtm.engine.*;
+
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+
+class DMDEventHandler implements ViewEventHandler, PortalEventHandler {
+
+    DragMagDemo application;
+
+    boolean dcamStickedToMouse = false;
+    boolean pcamStickedToMouse = false;
+    boolean portalStickedToMouse = false;
+
+    static final int PORTAL_INITIAL_X_OFFSET = 200;
+    static final int PORTAL_INITIAL_Y_OFFSET = 200;
+    boolean inPortal = false;
+
+    int lastJPX,lastJPY;    //remember last mouse coords to compute translation  (dragging)
+
+    DMDEventHandler(DragMagDemo appli){
+	application = appli;
+    }
+
+    public void press1(ViewPanel v, int mod, int jpx, int jpy, MouseEvent e){
+	lastJPX = jpx;
+	lastJPY = jpy;
+	if (inPortal){
+	    if (application.portal.coordInsideBar(jpx, jpy)){
+		portalStickedToMouse = true;
+	    }
+	    else {
+		pcamStickedToMouse = true;
+	    }
+	}
+	else {
+	    dcamStickedToMouse = true;
+	}
+    }
+
+    public void release1(ViewPanel v, int mod, int jpx, int jpy, MouseEvent e){
+	portalStickedToMouse = false;
+	dcamStickedToMouse = false;
+	pcamStickedToMouse = false;
+    }
+
+    public void click1(ViewPanel v,int mod,int jpx,int jpy,int clickNumber, MouseEvent e){
+	lastJPX = jpx;
+	lastJPY = jpy;
+    }
+
+    public void press2(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){}
+    public void release2(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){}
+    public void click2(ViewPanel v,int mod,int jpx,int jpy,int clickNumber, MouseEvent e){}
+
+    public void press3(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
+
+    }
+
+    public void release3(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
+
+    }
+
+
+    public void click3(ViewPanel v,int mod,int jpx,int jpy,int clickNumber, MouseEvent e){
+	application.switchPortal(jpx+PORTAL_INITIAL_X_OFFSET, jpy+PORTAL_INITIAL_Y_OFFSET);
+    }
+
+    public void mouseMoved(ViewPanel v,int jpx,int jpy, MouseEvent e){
+    }
+
+    public void mouseDragged(ViewPanel v,int mod,int buttonNumber,int jpx,int jpy, MouseEvent e){
+	if (inPortal){
+	    if (buttonNumber == 1){
+		if (portalStickedToMouse){
+		    application.portal.move(jpx-lastJPX, jpy-lastJPY);
+		    lastJPX = jpx;
+		    lastJPY = jpy;
+		    application.vsm.repaintNow();
+		}
+		else if (pcamStickedToMouse){
+		    float a = (application.portalCamera.focal+Math.abs(application.portalCamera.altitude))/application.portalCamera.focal;
+		    synchronized(application.portalCamera){
+			application.portalCamera.move(Math.round(a*(lastJPX-jpx)),
+						      Math.round(a*(jpy-lastJPY)));
+			lastJPX = jpx;
+			lastJPY = jpy;
+		    }
+		}
+	    }
+	}
+	else {
+	    if (buttonNumber == 1){
+		if (dcamStickedToMouse){
+		    float a = (application.demoCamera.focal+Math.abs(application.demoCamera.altitude))/application.demoCamera.focal;
+		    synchronized(application.demoCamera){
+			application.demoCamera.move(Math.round(a*(lastJPX-jpx)),
+						    Math.round(a*(jpy-lastJPY)));
+			lastJPX = jpx;
+			lastJPY = jpy;
+		    }
+		}
+	    }
+	}
+    }
+
+    public void mouseWheelMoved(ViewPanel v,short wheelDirection,int jpx,int jpy, MouseWheelEvent e){
+	Camera c = application.demoCamera;
+	float a = (c.focal+Math.abs(c.altitude)) / c.focal;
+	if (wheelDirection == WHEEL_UP){
+	    c.altitudeOffset(-a*5);
+	    application.vsm.repaintNow();
+	}
+	else {//wheelDirection == WHEEL_DOWN
+	    c.altitudeOffset(a*5);
+	    application.vsm.repaintNow();
+	}
+    }
+
+    public void enterGlyph(Glyph g){}
+    
+    public void exitGlyph(Glyph g){}
+
+    public void Ktype(ViewPanel v,char c,int code,int mod, KeyEvent e){}
+
+    public void Kpress(ViewPanel v,char c,int code,int mod, KeyEvent e){
+
+    }
+
+    public void Krelease(ViewPanel v,char c,int code,int mod, KeyEvent e){
+	if (code == KeyEvent.VK_PAGE_UP){application.getHigherView(mod == CTRL_MOD);}
+	else if (code == KeyEvent.VK_PAGE_DOWN){application.getLowerView(mod == CTRL_MOD);}
+	else if (code == KeyEvent.VK_HOME){application.getGlobalView(mod == CTRL_MOD);}
+	else if (code == KeyEvent.VK_UP){application.translateView(PortalWorldDemo.MOVE_UP, mod == CTRL_MOD);}
+	else if (code == KeyEvent.VK_DOWN){application.translateView(PortalWorldDemo.MOVE_DOWN, mod == CTRL_MOD);}
+	else if (code == KeyEvent.VK_LEFT){application.translateView(PortalWorldDemo.MOVE_LEFT, mod == CTRL_MOD);}
+	else if (code == KeyEvent.VK_RIGHT){application.translateView(PortalWorldDemo.MOVE_RIGHT, mod == CTRL_MOD);}
+    }
+
+    public void viewActivated(View v){}
+    
+    public void viewDeactivated(View v){}
+
+    public void viewIconified(View v){}
+
+    public void viewDeiconified(View v){}
+
+    public void viewClosing(View v){System.exit(0);}
+
+    /**cursor enters portal*/
+    public void enterPortal(Portal p){
+	inPortal = true;
+	((CameraPortal)p).setBorder(Color.WHITE);
+	application.vsm.repaintNow();
+    }
+
+    /**cursor exits portal*/
+    public void exitPortal(Portal p){
+	inPortal = false;
+	((CameraPortal)p).setBorder(Color.RED);
+	application.vsm.repaintNow();
+    }
+
+}
---
+++ net/claribole/zvtm/demo/DragMagDemo.java
@@ -0,0 +1,222 @@
+/*   FILE: DragMagDemo.java
+ *   DATE OF CREATION:  Sat Jun 17 13:58:59 2006
+ *   AUTHOR :           Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   MODIF:             Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   Copyright (c) INRIA, 2004-2006. All Rights Reserved
+ *   Licensed under the GNU LGPL. For full terms see the file COPYING.
+ *
+ * $Id$
+ */ 
+
+package net.claribole.zvtm.demo;
+
+import java.awt.Toolkit;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.ImageIcon;
+
+import java.util.Vector;
+
+import com.xerox.VTM.engine.*;
+import com.xerox.VTM.glyphs.*;
+import net.claribole.zvtm.engine.*;
+
+public class DragMagDemo {
+
+    /* screen dimensions */
+    static int SCREEN_WIDTH =  Toolkit.getDefaultToolkit().getScreenSize().width;
+    static int SCREEN_HEIGHT =  Toolkit.getDefaultToolkit().getScreenSize().height;
+
+    /* max dimensions of ZVTM view */
+    static final int VIEW_MAX_W = 800;
+    static final int VIEW_MAX_H = 600;
+
+    /* actual dimensions of windows on screen */
+    int VIEW_W, VIEW_H;
+    int VIEW_X, VIEW_Y;
+
+    /* World map */
+    static final Double MN000factor = new Double(8.0);
+    static final String M1000 = "1000";
+    static final String M1000path = "images/world/0000.png";
+    static final long M1000x = 0;
+    static final long M1000y = 0;
+    VImage mainMap;
+    static final int MAIN_MAP_WIDTH = 8000;
+    static final int MAIN_MAP_HEIGHT = 4000;
+    static final long MAP_WIDTH = Math.round(MAIN_MAP_WIDTH * MN000factor.doubleValue());
+    static final long MAP_HEIGHT = Math.round(MAIN_MAP_HEIGHT * MN000factor.doubleValue());
+    static final long HALF_MAP_WIDTH = Math.round(MAP_WIDTH/2.0);
+    static final long HALF_MAP_HEIGHT = Math.round(MAP_HEIGHT/2.0);
+
+    /* ZVTM */
+    VirtualSpaceManager vsm;
+
+    /* main view event handler */
+    DMDEventHandler eh;
+
+    /* main view*/
+    View demoView;
+    Camera demoCamera;
+    VirtualSpace mainVS;
+    static String mainVSname = "mainSpace";
+
+    static final Color HCURSOR_COLOR = new Color(200,48,48);
+
+    /*translation constants*/
+    static final short MOVE_UP = 0;
+    static final short MOVE_DOWN = 1;
+    static final short MOVE_LEFT = 2;
+    static final short MOVE_RIGHT = 3;
+
+    /*animation timing*/
+    static int ANIM_MOVE_LENGTH = 500;
+
+    /* Portal */
+    static int PORTAL_WIDTH = 200;
+    static int PORTAL_HEIGHT = 200;
+    DraggableCameraPortal portal;
+    Camera portalCamera;
+
+    DragMagDemo(){
+	vsm = new VirtualSpaceManager();
+ 	vsm.setDebug(true);
+	init();
+    }
+
+    public void init(){
+	eh = new DMDEventHandler(this);
+	windowLayout();
+	mainVS = vsm.addVirtualSpace(mainVSname);
+	vsm.setZoomLimit(0);
+	demoCamera = vsm.addCamera(mainVSname);
+	Vector cameras=new Vector();
+	cameras.add(demoCamera);
+	demoView = vsm.addExternalView(cameras, "Portal World Demo", View.STD_VIEW, VIEW_W, VIEW_H, false, true, true, null);
+	demoView.mouse.setHintColor(HCURSOR_COLOR);
+	demoView.setLocation(VIEW_X, VIEW_Y);
+	demoView.setEventHandler(eh);
+	demoView.setNotifyMouseMoved(true);
+	portalCamera = vsm.addCamera(mainVSname);
+	initMap();
+	getGlobalView(false);
+	System.gc();
+    }
+
+    void initMap(){
+	System.out.print("Loading World Map...");
+	mainMap = new VImage(M1000x, M1000y, 0,
+			     (new ImageIcon(M1000path)).getImage(),
+			     MN000factor.doubleValue());
+	mainMap.setDrawBorderPolicy(VImage.DRAW_BORDER_NEVER);
+	vsm.addGlyph(mainMap, mainVS);
+	mainVS.atBottom(mainMap);
+	vsm.addGlyph(new VCircle(0,0,0,200,Color.RED), mainVS);
+	System.out.println("OK");
+    }
+
+    void windowLayout(){
+	if (Utilities.osIsWindows()){
+	    VIEW_X = VIEW_Y = 0;
+	    SCREEN_HEIGHT -= 30;
+	}
+	else if (Utilities.osIsMacOS()){
+	    VIEW_X = 80;
+	    SCREEN_WIDTH -= 80;
+	}
+	VIEW_W = (SCREEN_WIDTH <= VIEW_MAX_W) ? SCREEN_WIDTH : VIEW_MAX_W;
+	VIEW_H = (SCREEN_HEIGHT <= VIEW_MAX_H) ? SCREEN_HEIGHT : VIEW_MAX_H;
+    }
+
+    void switchPortal(int x, int y){
+	if (portal != null){// portal is active, destroy it it
+	    //XXX:animate its disappearance (use a postanimaction to call following lines)
+	    vsm.destroyPortal(portal);
+	    portal = null;
+	    vsm.repaintNow();
+	}
+	else {// portal not active, create it
+	    portal = new DraggableCameraPortal(x-PORTAL_WIDTH/2, y-PORTAL_HEIGHT/2, PORTAL_WIDTH, PORTAL_HEIGHT, portalCamera);
+	    portal.setPortalEventHandler(eh);
+	    vsm.addPortal(portal, demoView);
+ 	    portal.setBorder(Color.RED);
+	    Location l = portal.getSeamlessView(demoCamera);
+	    portalCamera.moveTo(l.vx, l.vy);
+	    portalCamera.setAltitude(l.alt);
+	    Float alt=new Float(-2*(portalCamera.getAltitude()+portalCamera.getFocal())/3.0f);
+	    vsm.animator.createCameraAnimation(ANIM_MOVE_LENGTH,AnimManager.CA_ALT_SIG,alt,portalCamera.getID());
+	}
+    }
+
+    void getGlobalView(boolean inPortal){
+	if (inPortal){
+	    portal.getGlobalView(ANIM_MOVE_LENGTH, vsm);
+	}
+	else {
+	    vsm.getGlobalView(demoCamera, ANIM_MOVE_LENGTH);
+	}
+    }
+
+    /*higher view (multiply altitude by altitudeFactor)*/
+    void getHigherView(boolean inPortal){
+	Camera c;
+	if (inPortal){
+	    c = portalCamera;
+	}
+	else {
+	    c = demoCamera;
+	}
+	Float alt=new Float(c.getAltitude()+c.getFocal());
+	vsm.animator.createCameraAnimation(ANIM_MOVE_LENGTH,AnimManager.CA_ALT_SIG,alt,c.getID());
+    }
+
+    /*higher view (multiply altitude by altitudeFactor)*/
+    void getLowerView(boolean inPortal){
+	Camera c;
+	if (inPortal){
+	    c = portalCamera;
+	}
+	else {
+	    c = demoCamera;
+	}
+	Float alt=new Float(-(c.getAltitude()+c.getFocal())/2.0f);
+	vsm.animator.createCameraAnimation(ANIM_MOVE_LENGTH,AnimManager.CA_ALT_SIG,alt,c.getID());
+    }
+
+    /*direction should be one of ZGRViewer.MOVE_* */
+    void translateView(short direction, boolean inPortal){
+	Camera c;
+	LongPoint trans;
+	long[] rb;
+	if (inPortal){
+	    c = portalCamera;
+	    rb = portal.getVisibleRegion();
+	}
+	else {
+	    c = demoCamera;
+	    rb = demoView.getVisibleRegion(c);
+	}
+	if (direction==MOVE_UP){
+	    long qt=Math.round((rb[1]-rb[3])/2.4);
+	    trans=new LongPoint(0,qt);
+	}
+	else if (direction==MOVE_DOWN){
+	    long qt=Math.round((rb[3]-rb[1])/2.4);
+	    trans=new LongPoint(0,qt);
+	}
+	else if (direction==MOVE_RIGHT){
+	    long qt=Math.round((rb[2]-rb[0])/2.4);
+	    trans=new LongPoint(qt,0);
+	}
+	else {// direction==MOVE_LEFT
+	    long qt=Math.round((rb[0]-rb[2])/2.4);
+	    trans=new LongPoint(qt,0);
+	}
+	vsm.animator.createCameraAnimation(ANIM_MOVE_LENGTH,AnimManager.CA_TRANS_SIG,trans,c.getID());
+    }
+
+    public static void main(String[] args){
+ 	new DragMagDemo();
+    }
+
+}
