0710738958c379459efc1666f755d0adcf12a197
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2006-06-23 07:16:44 +0000

    new Java2DPainter layer: after distortion but before portals
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@62 74e165f3-c697-40a6-91af-552c8936d7fe

--- net/claribole/zvtm/engine/Java2DPainter.java
+++ net/claribole/zvtm/engine/Java2DPainter.java
@@ -22,8 +22,9 @@ public interface Java2DPainter {
     public static final short BACKGROUND = 0;
     public static final short FOREGROUND = 1;
     public static final short AFTER_DISTORTION = 2;
+    public static final short AFTER_PORTALS = 3;
     
-    /**painting instructions (called by the associated view at each repaint)<br>This method is called at one of the following two times depending on how this painter was registered with the View: before ZVTM glyphs are painted (BACKGROUND), after ZVTM glyphs have been painted (FOREGROUND), after a distortion lens has been applied (ABOVE_LENS).
+    /**painting instructions (called by the associated view at each repaint)<br>This method is called at one of the following two times depending on how this painter was registered with the View: before ZVTM glyphs are painted (BACKGROUND), after ZVTM glyphs have been painted (FOREGROUND), after a distortion lens has been applied (AFTER_DISTORTION), after portals have been painted (AFTER_PORTALS).
      *@param g2d the Graphics context on which to paint
      *@param viewWidth the associated View's width
      *@param viewHeight the associated View's height
