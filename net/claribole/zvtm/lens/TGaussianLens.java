5ab1f4d3391753d5e4a87d6beb25e091a825c713
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2006-10-06 06:54:24 +0000

    TLens, L(2), gaussian drop off
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@216 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ net/claribole/zvtm/lens/TGaussianLens.java
@@ -0,0 +1,200 @@
+/*   FILE: TGaussianLens.java
+ *   DATE OF CREATION:  Fri Oct 06 08:41:04 2006
+ *   AUTHOR :           Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   MODIF:             Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   Copyright (c) INRIA, 2004-2006. All Rights Reserved
+ *   Licensed under the GNU LGPL. For full terms see the file COPYING.
+ *
+ * $Id: $
+ */ 
+
+
+package net.claribole.zvtm.lens;
+
+import java.awt.image.SinglePixelPackedSampleModel;
+import java.awt.image.WritableRaster;
+
+/**Translucent lens. Profile: inverse cosine - Distance metric: L(2) (circular shape)<br>Size expressed as an absolute value in pixels*/
+
+public class TGaussianLens extends TLens {
+
+    /* gain function parameters (transition in translucence space) */
+    protected double aT = 0;
+    protected double bT = 0;
+    protected double cT = 0;
+    protected double eT = 0;
+
+    /**
+     * create a lens with a maximum magnification factor of 2.0
+     */
+    public TGaussianLens(){
+	this.MM = 2.0f;
+	updateMagBufferWorkingDimensions();
+	aT = Math.PI/(LR1-LR2);
+	bT = - Math.PI*LR2/(LR1-LR2);
+	cT = (MMTf-MMTc)/2;
+	eT = (MMTf+MMTc)/2;
+    }
+
+    /**
+     * create a lens with a given maximum magnification factor
+     *
+     *@param mm magnification factor, mm in [0,+inf[
+     */
+    public TGaussianLens(float mm){
+	this.MM = mm;
+	updateMagBufferWorkingDimensions();
+	aT = Math.PI/(LR1-LR2);
+	bT = - Math.PI*LR2/(LR1-LR2);
+	cT = (MMTf-MMTc)/2;
+	eT = (MMTf+MMTc)/2;
+    }
+
+    /**
+     * create a lens with a given maximum magnification factor, inner and outer radii
+     *
+     *@param mm magnification factor, mm in [0,+inf[
+     *@param tc translucency value (at junction between transition and context), tc in [0,1.0]
+     *@param tf translucency value (at junction between transition and focus), tf in [0,1.0]
+     *@param outerRadius outer radius (beyond which no magnification is applied - outward)
+     *@param innerRadius inner radius (beyond which maximum magnification is applied - inward)
+     */
+    public TGaussianLens(float mm, float tc, float tf, int outerRadius, int innerRadius){
+	this.MM = mm;
+	this.LR1 = outerRadius;
+	this.LR2 = innerRadius;
+	this.MMTc = tc;
+	this.MMTf = tf;
+	updateMagBufferWorkingDimensions();
+	aT = Math.PI/(LR1-LR2);
+	bT = - Math.PI*LR2/(LR1-LR2);
+	cT = (MMTf-MMTc)/2;
+	eT = (MMTf+MMTc)/2;
+    }
+
+    /**
+     * create a lens with a given maximum magnification factor, inner and outer radii
+     *
+     *@param mm magnification factor, mm in [0,+inf[
+     *@param tc translucency value (at junction between transition and context), tc in [0,1.0]
+     *@param tf translucency value (at junction between transition and focus), tf in [0,1.0]
+     *@param outerRadius outer radius (beyond which no magnification is applied - outward)
+     *@param innerRadius inner radius (beyond which maximum magnification is applied - inward)
+     *@param x horizontal coordinate of the lens' center (as an offset w.r.t the view's center coordinates)
+     *@param y vertical coordinate of the lens' center (as an offset w.r.t the view's center coordinates)
+     */
+    public TGaussianLens(float mm, float tc, float tf, int outerRadius, int innerRadius, int x, int y){
+	this.MM = mm;
+	this.LR1 = outerRadius;
+	this.LR2 = innerRadius;
+	this.MMTc = tc;
+	this.MMTf = tf;
+	updateMagBufferWorkingDimensions();
+	aT = Math.PI/(LR1-LR2);
+	bT = - Math.PI*LR2/(LR1-LR2);
+	cT = (MMTf-MMTc)/2;
+	eT = (MMTf+MMTc)/2;
+	lx = x;
+	ly = y;
+    }
+
+    /**
+     * set the lens' outer radius (beyond which no magnification is applied - outward)
+     *
+     *@param r radius in pixels
+     */
+    public void setOuterRadius(int r){
+	super.setOuterRadius(r);
+	aT = Math.PI/(LR1-LR2);
+	bT = - Math.PI*LR2/(LR1-LR2);
+	cT = (MMTf-MMTc)/2;
+	eT = (MMTf+MMTc)/2;
+    }
+
+    /**
+     * set the lens' inner radius (beyond which maximum magnification is applied - inward)
+     *
+     *@param r radius in pixels
+     */
+    public void setInnerRadius(int r){
+	super.setInnerRadius(r);
+	aT = Math.PI/(LR1-LR2);
+	bT = - Math.PI*LR2/(LR1-LR2);
+	cT = (MMTf-MMTc)/2;
+	eT = (MMTf+MMTc)/2;
+    }
+
+    /**
+     * set the lens' radii
+     *
+     *@param outerRadius outer radius (beyond which no magnification is applied - outward)
+     *@param innerRadius inner radius (beyond which maximum magnification is applied - inward)
+     */
+    public void setRadii(int outerRadius, int innerRadius){
+	this.setRadii(outerRadius, innerRadius, true);
+    }
+
+    /**
+     * set the lens' radii
+     *
+     *@param outerRadius outer radius (beyond which no magnification is applied - outward)
+     *@param innerRadius inner radius (beyond which maximum magnification is applied - inward)
+     */
+    public void setRadii(int outerRadius, int innerRadius, boolean forceRaster){
+	super.setRadii(outerRadius, innerRadius, forceRaster);
+	aT = Math.PI/(LR1-LR2);
+	bT = - Math.PI*LR2/(LR1-LR2);
+	cT = (MMTf-MMTc)/2;
+	eT = (MMTf+MMTc)/2;
+    }
+
+
+    /**
+     * set the lens' radii and maximum magnification
+     *
+     *@param mm maximum magnification factor, mm in [0,+inf[
+     *@param outerRadius outer radius (beyond which no magnification is applied - outward)
+     *@param innerRadius inner radius (beyond which maximum magnification is applied - inward)
+     */
+    public void setMMandRadii(float mm, int outerRadius, int innerRadius){
+	this.setMMandRadii(mm, outerRadius, innerRadius, true);
+    }
+
+    /**
+     * set the lens' radii and maximum magnification
+     *
+     *@param mm maximum magnification factor, mm in [0,+inf[
+     *@param outerRadius outer radius (beyond which no magnification is applied - outward)
+     *@param innerRadius inner radius (beyond which maximum magnification is applied - inward)
+     */
+    public void setMMandRadii(float mm, int outerRadius, int innerRadius, boolean forceRaster){
+	super.setMMandRadii(mm, outerRadius, innerRadius, forceRaster);
+	aT = Math.PI/(LR1-LR2);
+	bT = - Math.PI*LR2/(LR1-LR2);
+	cT = (MMTf-MMTc)/2;
+	eT = (MMTf+MMTc)/2;
+    }
+
+    public void setMaximumMagnification(float mm){
+	this.setMaximumMagnification(mm, true);
+    }
+
+    public void setMaximumMagnification(float mm, boolean forceRaster){
+	super.setMaximumMagnification(mm, forceRaster);
+	aT = Math.PI/(LR1-LR2);
+	bT = - Math.PI*LR2/(LR1-LR2);
+	cT = (MMTf-MMTc)/2;
+	eT = (MMTf+MMTc)/2;
+    }
+
+    public void gfT(float x, float y, float[] g){
+	d = Math.sqrt(Math.pow(x-sw-lx,2) + Math.pow(y-sh-ly,2));
+	if (d <= LR2)
+	    g[0] = MMTf;
+	else if (d <= LR1)
+	    g[0] = (float)(cT * Math.cos(aT*d+bT) + eT);
+	else
+	    g[0] = MMTc;
+    }
+
+}
