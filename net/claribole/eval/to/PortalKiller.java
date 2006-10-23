b7fde5947275c8d4dd19d2d909c6f1d2e7e13e75
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2006-10-23 06:12:32 +0000

    forgot portal killer
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@244 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ net/claribole/eval/to/PortalKiller.java
@@ -0,0 +1,28 @@
+/*   FILE: Eval.java
+ *   DATE OF CREATION:  Fri Oct 20 10:28:06 2006
+ *   AUTHOR :           Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   MODIF:             Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   Copyright (c) INRIA, 2006. All Rights Reserved
+ *
+ * $Id:  $
+ */
+
+package net.claribole.eval.to;
+
+import net.claribole.zvtm.engine.PostAnimationAction;
+
+public class PortalKiller implements PostAnimationAction {
+
+    Eval application;
+    
+    public PortalKiller(Eval application){
+	this.application = application;
+    }
+    
+    public void animationEnded(Object target, short type, String dimension){
+	if (type == PostAnimationAction.PORTAL){
+	    application.killPortal();
+	}
+    }
+    
+}
