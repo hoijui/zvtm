3beb5afafc6b209c4476cd3456de55cdecdf21a5
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2006-12-15 09:54:26 +0000

    load SVG graph
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@358 74e165f3-c697-40a6-91af-552c8936d7fe

--- net/claribole/eval/alphalens/EvalGraphPointing.java
+++ net/claribole/eval/alphalens/EvalGraphPointing.java
@@ -8,11 +8,26 @@ import java.awt.Graphics2D;
 
 import java.util.Vector;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
 import com.xerox.VTM.engine.*;
 import com.xerox.VTM.glyphs.*;
+import com.xerox.VTM.svg.SVGReader;
 import net.claribole.zvtm.engine.*;
 
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.FactoryConfigurationError;
+import javax.xml.parsers.ParserConfigurationException;
+
 public class EvalGraphPointing extends EvalPointing {
+
+    static final String GRAPH_PATH = "images/graphs/jcl.svg";
+    static final File GRAPH_FILE = new File(GRAPH_PATH);
     
     public EvalGraphPointing(short t){
 	initGUI();
@@ -21,28 +36,47 @@ public class EvalGraphPointing extends EvalPointing {
 	eh = new BaseEventHandlerPointing(this);
 	mView.setEventHandler(eh);
 	initScene();
-	Location l = vsm.getGlobalView(mCamera);
-	mCamera.moveTo(l.vx, l.vy);
-	mCamera.setAltitude(l.alt);
+// 	mCamera.moveTo(0, 0);
+// 	mCamera.setAltitude(100.0f);
+	vsm.getGlobalView(mSpace.getCamera(0),100);
     }
 
     void initScene(){
 	mView.setBackgroundColor(EvalPointing.BACKGROUND_COLOR);
-	
+	try {
+	SVGReader.load(parse(GRAPH_FILE), vsm, mSpaceName, false, GRAPH_FILE.toURL().toString());
+	}
+	catch (MalformedURLException ex){ex.printStackTrace();}
+    }
+
+    static Document parse(File f){ 
+	try {
+	    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	    factory.setValidating(false);
+	    factory.setAttribute("http://apache.org/xml/features/nonvalidating/load-external-dtd", new Boolean(false));
+	    factory.setNamespaceAware(true);
+	    DocumentBuilder builder = factory.newDocumentBuilder();
+	    Document res = builder.parse(f);
+	    return res;
+	}
+	catch (FactoryConfigurationError e){e.printStackTrace();return null;} 
+	catch (ParserConfigurationException e){e.printStackTrace();return null;}
+	catch (SAXException e){e.printStackTrace();return null;}
+	catch (IOException e){e.printStackTrace();return null;}
     }
     
     public static void main(String[] args){
 	try {
 	    if (args.length >= 3){
-		EvalWorldPointing.VIEW_MAX_W = Integer.parseInt(args[1]);
-		EvalWorldPointing.VIEW_MAX_H = Integer.parseInt(args[2]);
+		EvalGraphPointing.VIEW_MAX_W = Integer.parseInt(args[1]);
+		EvalGraphPointing.VIEW_MAX_H = Integer.parseInt(args[2]);
 	    }
-	    new EvalWorldPointing(Short.parseShort(args[0]));
+	    new EvalGraphPointing(Short.parseShort(args[0]));
 	}
 	catch (Exception ex){
 	    ex.printStackTrace();
 	    System.err.println("No cmd line parameter to indicate technique, defaulting to Fading Lens");
-	    new EvalWorldPointing(EvalPointing.TECHNIQUE_FL);
+	    new EvalGraphPointing(EvalPointing.TECHNIQUE_FL);
 	}
     }
 
