8541bfb4e8f366cab1268c8e7c61ee450b15c954
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2007-01-24 13:10:29 +0000

    circle whose min projected size can be customized
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@384 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ com/xerox/VTM/glyphs/ZCircle.java
@@ -0,0 +1,129 @@
+/*   FILE: ZCircle.java
+ *   DATE OF CREATION:  Wed Jan 24 10:45:06 2007
+ *   AUTHOR :            Emmanuel Pietriga (emmanuel.pietriga@xrce.xerox.com)
+ *   MODIF:              Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   Copyright (c) INRIA, 2007. All Rights Reserved
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * For full terms see the file COPYING.
+ *
+ * $Id:  $
+ */
+
+package com.xerox.VTM.glyphs;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.awt.Dimension;
+import java.awt.geom.AffineTransform;
+
+import com.xerox.VTM.engine.Camera;
+import net.claribole.zvtm.lens.Lens;
+
+/**
+ * Circle - cannot be reoriented - it makes no sense anyway
+ * @author Emmanuel Pietriga
+ **/
+
+public class ZCircle extends VCircle {
+
+    public ZCircle(){
+	super();
+    }
+
+    /**
+     *@param x coordinate in virtual space
+     *@param y coordinate in virtual space
+     *@param z altitude
+     *@param r radius in virtual space
+     *@param c fill color
+     */
+    public ZCircle(long x,long y,float z,long r,Color c){
+	super(x, y, z, r, c);
+    }
+
+    int minProjSize = 1;
+
+    public void setMinimumProjectedSize(int s){
+	minProjSize = s;
+    }
+
+    public int getMimimumProjectedSize(){
+	return minProjSize;
+    }
+
+    /**draw glyph 
+     *@param i camera index in the virtual space
+     */
+    public void draw(Graphics2D g,int vW,int vH,int i,Stroke stdS,AffineTransform stdT, int dx, int dy){
+	if (pc[i].cr >= minProjSize){
+	    if (filled){
+		g.setColor(this.color);
+		g.fillOval(dx+pc[i].cx-pc[i].cr,dy+pc[i].cy-pc[i].cr,2*pc[i].cr,2*pc[i].cr);
+	    }
+	    g.setColor(borderColor);
+	    if (paintBorder){
+		if (stroke!=null) {
+		    g.setStroke(stroke);
+		    g.drawOval(dx+pc[i].cx-pc[i].cr,dy+pc[i].cy-pc[i].cr,2*pc[i].cr,2*pc[i].cr);
+		    g.setStroke(stdS);
+		}
+		else {
+		    g.drawOval(dx+pc[i].cx-pc[i].cr,dy+pc[i].cy-pc[i].cr,2*pc[i].cr,2*pc[i].cr);
+		}
+	    }
+	}
+	else {
+	    g.setColor(this.color);
+	    g.fillRect(dx+pc[i].cx, dy+pc[i].cy, minProjSize, minProjSize);
+	}
+    }
+
+    public void drawForLens(Graphics2D g,int vW,int vH,int i,Stroke stdS,AffineTransform stdT, int dx, int dy){
+	if (pc[i].lcr >= minProjSize){
+	    if (filled){
+		g.setColor(this.color);
+		g.fillOval(dx+pc[i].lcx-pc[i].lcr,dy+pc[i].lcy-pc[i].lcr,2*pc[i].lcr,2*pc[i].lcr);
+	    }
+	    g.setColor(borderColor);
+	    if (paintBorder){
+		if (stroke!=null) {
+		    g.setStroke(stroke);
+		    g.drawOval(dx+pc[i].lcx-pc[i].lcr,dy+pc[i].lcy-pc[i].lcr,2*pc[i].lcr,2*pc[i].lcr);
+		    g.setStroke(stdS);
+		}
+		else {
+		    g.drawOval(dx+pc[i].lcx-pc[i].lcr,dy+pc[i].lcy-pc[i].lcr,2*pc[i].lcr,2*pc[i].lcr);
+		}
+	    }
+	}
+	else {
+	    g.setColor(this.color);
+	    g.fillRect(dx+pc[i].lcx, dy+pc[i].lcy, minProjSize, minProjSize);
+	}
+    }
+
+
+    /**returns a clone of this object (only basic information is cloned for now: shape, orientation, position, size)*/
+    public Object clone(){
+	ZCircle res = new ZCircle(vx,vy,0,vr,color);
+	res.borderColor = this.borderColor;
+	res.selectedColor = this.selectedColor;
+	res.mouseInsideColor = this.mouseInsideColor;
+	res.bColor = this.bColor;
+	res.setMinimumProjectedSize(minProjSize);
+	return res;
+    }
+
+}
