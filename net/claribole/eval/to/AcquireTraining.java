90d237d673b58a45d20d34d9e9a9b6173895430d
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2007-01-26 09:53:51 +0000

    training environment (practice tow acquisition)
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@401 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ net/claribole/eval/to/AcquireTraining.java
@@ -0,0 +1,172 @@
+/*   FILE: AcquireTraining.java
+ *   DATE OF CREATION:  Fri Jan 26 10:36:06 2007
+ *   AUTHOR :           Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   MODIF:             Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   Copyright (c) INRIA, 2007. All Rights Reserved
+ *
+ * $Id:  $
+ */
+
+package net.claribole.eval.to;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import java.util.Vector;
+
+import com.xerox.VTM.engine.*;
+import com.xerox.VTM.glyphs.*;
+import net.claribole.zvtm.engine.*;
+
+public class AcquireTraining implements TOWApplication {
+
+    /* screen dimensions, actual dimensions of windows */
+    static int SCREEN_WIDTH =  Toolkit.getDefaultToolkit().getScreenSize().width;
+    static int SCREEN_HEIGHT =  Toolkit.getDefaultToolkit().getScreenSize().height;
+    static int VIEW_MAX_W = 1600;
+    static int VIEW_MAX_H = 1200;
+    int VIEW_W, VIEW_H;
+    int VIEW_X, VIEW_Y;
+    /* dimensions of zoomable panel */
+    int panelWidth, panelHeight;
+
+    /* ZVTM components */
+    static final Color BACKGROUND_COLOR = Color.LIGHT_GRAY;
+    VirtualSpaceManager vsm;
+    VirtualSpace mSpace;
+    static final String mSpaceName = "mainSpace";
+    View mView;
+    String mViewName = "Training";
+    Camera mCamera, oCamera;
+
+    AcquireTrainingEventHandler eh;
+
+    /* generic overview settings */
+    static int OVERVIEW_WIDTH = 150;
+    static int OVERVIEW_HEIGHT = 150;
+    static final Color DEFAULT_PORTAL_BORDER_COLOR = Color.BLACK;
+    static final Color INSIDE_PORTAL_BORDER_COLOR = Color.RED;
+    static final float OVERVIEW_CAMERA_ALTITUDE_FACTOR = 36.0f;
+    static final int OVERVIEW_CENTERING_TRANSLATE_TIME = 300;
+    
+    /* trailing overview settings */
+    static final int TOW_SWITCH_ANIM_TIME = 500;
+    static final int TOW_CONTRACTED_WIDTH = 50;
+    static final int TOW_CONTRACTED_HEIGHT = 50;
+    static final int TOW_HORIZONTAL_EXPANSION_OFFSET = OVERVIEW_WIDTH - TOW_CONTRACTED_WIDTH;
+    static final int TOW_VERTICAL_EXPANSION_OFFSET = OVERVIEW_HEIGHT - TOW_CONTRACTED_HEIGHT;
+    static final int TOW_PORTAL_X_OFFSET = -120;
+    static final int TOW_PORTAL_Y_OFFSET = 120;
+    TrailingOverview to;
+
+    public AcquireTraining(){
+	initGUI();
+	eh = new AcquireTrainingEventHandler(this);
+	mView.setEventHandler(eh);
+	mCamera.moveTo(0, 0);
+	mCamera.setAltitude(0);
+	initWorld();
+	centerOverview(false);
+	updateOverview();
+	vsm.repaintNow(mView);
+    }
+
+    void initGUI(){
+	windowLayout();
+	vsm = new VirtualSpaceManager();
+	mSpace = vsm.addVirtualSpace(mSpaceName);
+	mCamera = vsm.addCamera(mSpaceName);
+	oCamera = vsm.addCamera(mSpaceName);
+	Vector v = new Vector();
+	v.add(mCamera);
+	mView = vsm.addExternalView(v, mViewName, View.STD_VIEW, VIEW_W, VIEW_H, false, true, false, null);
+	mView.getPanel().addComponentListener(eh);
+	mView.setNotifyMouseMoved(true);
+	mView.setBackgroundColor(AcquireTraining.BACKGROUND_COLOR);
+	mView.setAntialiasing(true);
+	updatePanelSize();
+    }
+
+    void initWorld(){
+	vsm.addGlyph(new ZCircle(100, 100, 0, 40, Color.BLUE), mSpace);
+    }
+
+    void windowLayout(){
+	if (Utilities.osIsWindows()){
+	    VIEW_X = VIEW_Y = 0;
+	}
+	else if (Utilities.osIsMacOS()){
+	    VIEW_X = 80;
+	    SCREEN_WIDTH -= 80;
+	}
+	VIEW_W = (SCREEN_WIDTH <= VIEW_MAX_W) ? SCREEN_WIDTH : VIEW_MAX_W;
+	VIEW_H = (SCREEN_HEIGHT <= VIEW_MAX_H) ? SCREEN_HEIGHT : VIEW_MAX_H;
+    }
+
+    void centerOverview(boolean animate){
+	if (animate){
+	    vsm.animator.createCameraAnimation(OVERVIEW_CENTERING_TRANSLATE_TIME, AnimManager.CA_TRANS_SIG,
+					       new LongPoint(mCamera.posx-oCamera.posx, mCamera.posy-oCamera.posy),
+					       oCamera.getID(), null);
+	}
+	else {
+	    oCamera.moveTo(mCamera.posx, mCamera.posy);
+	}
+    }
+    
+    void updateOverview(){// update overview camera's altitude
+	oCamera.setAltitude((float)((mCamera.getAltitude()+mCamera.getFocal())*OVERVIEW_CAMERA_ALTITUDE_FACTOR-mCamera.getFocal()));
+    }
+
+    void switchPortal(int x, int y){
+	if (to != null){// portal is active, destroy it it
+	    vsm.animator.createPortalAnimation(TOW_SWITCH_ANIM_TIME, AnimManager.PT_ALPHA_LIN, new Float(-0.5f),
+					       to.getID(), new PortalKiller(this));
+	}
+	else {// portal not active, create it
+	    to = getPortal(x, y);
+	    to.setBackgroundColor(BACKGROUND_COLOR);
+	    to.setPortalEventHandler((PortalEventHandler)eh);
+	    vsm.addPortal(to, mView);
+ 	    to.setBorder(DEFAULT_PORTAL_BORDER_COLOR);
+	    vsm.animator.createPortalAnimation(TOW_SWITCH_ANIM_TIME, AnimManager.PT_ALPHA_LIN, new Float(0.5f),
+					       to.getID(), null);
+	    oCamera.moveTo(0, 0);
+	    centerOverview(false);
+	}
+    }
+
+    TrailingOverview getPortal(int x, int y){
+	return new TrailingOverview(x-TOW_CONTRACTED_WIDTH/2, y-TOW_CONTRACTED_HEIGHT/2,
+				    TOW_CONTRACTED_WIDTH, TOW_CONTRACTED_HEIGHT,
+				    oCamera, mCamera, 0.0f, TOW_PORTAL_X_OFFSET, TOW_PORTAL_Y_OFFSET);
+    }
+
+    public void killPortal(){
+	vsm.destroyPortal(to);
+	to.dispose();
+	to = null;
+	vsm.repaintNow();
+    }
+
+    void updatePanelSize(){
+	Dimension d = mView.getPanel().getSize();
+	panelWidth = d.width;
+	panelHeight = d.height;
+    }
+
+    void exit(){
+	if (to != null){to.dispose();}
+	System.exit(0);
+    }
+    
+    public static void main(String[] args){
+	if (args.length >= 2){
+	    AcquireTraining.VIEW_MAX_W = Integer.parseInt(args[0]);
+	    AcquireTraining.VIEW_MAX_H = Integer.parseInt(args[1]);
+	}
+	new AcquireTraining();
+    }
+
+}
---
+++ net/claribole/eval/to/AcquireTrainingEventHandler.java
@@ -0,0 +1,231 @@
+/*   FILE: TOWApplication.java
+ *   DATE OF CREATION:  Fri Jan 19 15:35:06 2007
+ *   AUTHOR :           Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   MODIF:             Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   Copyright (c) INRIA, 2007. All Rights Reserved
+ *
+ * $Id:  $
+ */
+
+package net.claribole.eval.to;
+
+import java.awt.Toolkit;
+import java.awt.event.ComponentListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.KeyEvent;
+import java.util.Vector;
+
+import com.xerox.VTM.engine.*;
+import com.xerox.VTM.glyphs.Glyph;
+import net.claribole.zvtm.engine.*;
+
+
+class AcquireTrainingEventHandler implements ViewEventHandler, ComponentListener, PortalEventHandler {
+
+    static final float MAIN_SPEED_FACTOR = 50.0f;
+    static final float LENS_SPEED_FACTOR = 5.0f;
+
+    static final float WHEEL_ZOOMIN_FACTOR = 21.0f;
+    static final float WHEEL_ZOOMOUT_FACTOR = 22.0f;
+
+    AcquireTraining application;
+
+    boolean mouseInsideOverview = false;
+
+    static final int PORTAL_EXPANSION_TIME = 200;
+
+    int currentJPX, currentJPY;
+
+    boolean delayedTOWExit = false;
+
+    int lastJPX,lastJPY;    //remember last mouse coords to compute translation  (dragging)
+    long lastVX, lastVY;
+
+    float projCoef, alt, oldCameraAltitude; // for efficiency
+
+    /* main camera is being dragged */
+    boolean mCameraStickedToMouse = false;
+    /* overview camera is being dragged */
+    boolean oCameraStickedToMouse = false;
+    /* observed region in overview is being dragged */
+    boolean orStickedToMouse = false;
+
+    AcquireTrainingEventHandler(AcquireTraining app){
+	this.application = app;
+    }
+
+    public void press1(ViewPanel v, int mod, int jpx, int jpy, MouseEvent e){
+	lastJPX = jpx;
+	lastJPY = jpy;
+	if (mouseInsideOverview){
+	    if (application.to.coordInsideObservedRegion(jpx, jpy)){
+		orStickedToMouse = true;
+	    }
+	}
+	else {
+	    mCameraStickedToMouse = true;
+	}
+    }
+
+    public void release1(ViewPanel v, int mod, int jpx, int jpy, MouseEvent e){
+	if (delayedTOWExit){
+	    portalExitActions();
+	    delayedTOWExit = false;
+	}
+	if (mCameraStickedToMouse){
+	    application.centerOverview(false);
+	    mCameraStickedToMouse = false;
+	}
+	orStickedToMouse = false;
+    }
+
+    public void click1(ViewPanel v, int mod, int jpx, int jpy, int clickNumber, MouseEvent e){}
+
+    public void press2(ViewPanel v, int mod, int jpx, int jpy, MouseEvent e){}
+    public void release2(ViewPanel v, int mod, int jpx, int jpy, MouseEvent e){}
+    public void click2(ViewPanel v, int mod, int jpx, int jpy, int clickNumber, MouseEvent e){}
+
+    public void press3(ViewPanel v, int mod, int jpx, int jpy, MouseEvent e){}
+    public void release3(ViewPanel v, int mod, int jpx, int jpy, MouseEvent e){}
+    public void click3(ViewPanel v, int mod, int jpx, int jpy, int clickNumber, MouseEvent e){}
+
+    public void mouseMoved(ViewPanel v, int jpx, int jpy, MouseEvent e){
+	if (!mouseInsideOverview && application.to != null){
+	    application.to.updateFrequency(e.getWhen());
+	    application.to.updateWidgetLocation(jpx, jpy);
+	}
+	currentJPX = jpx;
+	currentJPY = jpy;
+    }
+
+    public void mouseDragged(ViewPanel v, int mod, int buttonNumber, int jpx, int jpy, MouseEvent e){
+	if (!mouseInsideOverview && application.to != null){
+	    application.to.updateFrequency(e.getWhen());
+	    application.to.updateWidgetLocation(jpx, jpy);
+	}
+	currentJPX = jpx;
+	currentJPY = jpy;
+	if (buttonNumber == 1){
+	    if (mCameraStickedToMouse){
+		synchronized(application.mCamera){
+		    projCoef = (application.mCamera.focal+Math.abs(application.mCamera.altitude))/application.mCamera.focal;
+		    application.mCamera.move(Math.round(projCoef*(lastJPX-jpx)),
+					     Math.round(projCoef*(jpy-lastJPY)));
+		    lastJPX = jpx;
+		    lastJPY = jpy;
+		}
+	    }
+	    else if (orStickedToMouse){
+		synchronized(application.oCamera){
+		    projCoef = (application.oCamera.focal+Math.abs(application.oCamera.altitude))/application.oCamera.focal;
+		    application.mCamera.move(Math.round(projCoef*(jpx-lastJPX)),
+					     Math.round(projCoef*(lastJPY-jpy)));
+		    lastJPX = jpx;
+		    lastJPY = jpy;
+		}
+	    }
+ 	}
+    }
+
+    public void mouseWheelMoved(ViewPanel v, short wheelDirection, int jpx, int jpy, MouseWheelEvent e){}
+
+    public void enterGlyph(Glyph g){
+	if (g.mouseInsideFColor != null){g.color = g.mouseInsideFColor;}
+	if (g.mouseInsideColor != null){g.borderColor = g.mouseInsideColor;}
+    }
+
+    public void exitGlyph(Glyph g){
+	if (g.isSelected()){
+	    g.borderColor = (g.selectedColor != null) ? g.selectedColor : g.bColor;
+	}
+	else {
+	    if (g.mouseInsideFColor != null){g.color = g.fColor;}
+	    if (g.mouseInsideColor != null){g.borderColor = g.bColor;}
+	}
+    }
+
+    public void Kpress(ViewPanel v, char c, int code, int mod, KeyEvent e){
+	if (code == KeyEvent.VK_SPACE){
+	    application.switchPortal(currentJPX, currentJPY);
+	    application.to.updateFrequency(e.getWhen());
+	    application.to.updateWidgetLocation(currentJPX, currentJPY);
+	}
+    }
+           
+    public void Krelease(ViewPanel v, char c, int code, int mod, KeyEvent e){
+	if (mod == CTRL_MOD && code == KeyEvent.VK_Q){application.exit();}
+    }
+           
+    public void Ktype(ViewPanel v, char c, int code, int mod, KeyEvent e){}
+    
+    public void viewActivated(View v){}
+    
+    public void viewClosing(View v){}
+    
+    public void viewDeactivated(View v){}
+    
+    public void viewDeiconified(View v){}
+    
+    public void viewIconified(View v){}
+
+    /* ComponentListener */
+    public void componentHidden(ComponentEvent e){}
+
+    public void componentMoved(ComponentEvent e){}
+
+    public void componentResized(ComponentEvent e){
+	application.updatePanelSize();
+    }
+
+    public void componentShown(ComponentEvent e){}
+
+    /**cursor enters portal*/
+    public void enterPortal(Portal p){
+	if (mCameraStickedToMouse){// do not exec actions associated with entering portal when the user is
+	    return;                // panning the main viewport (most likely entered portal by accident)
+	}
+	if (delayedTOWExit){
+	    delayedTOWExit = false;
+	    return;
+	}
+	mouseInsideOverview = true;
+ 	stickPortal();
+	application.vsm.repaintNow();
+    }
+
+    /**cursor exits portal*/
+    public void exitPortal(Portal p){
+	if (!mouseInsideOverview){// do not exec exit actions if enter actions
+	    return;          // were not executed at entry time
+	}
+	if (orStickedToMouse){
+	    delayedTOWExit = true;
+	}
+	else {
+	    portalExitActions();
+	}
+    }
+
+    void portalExitActions(){
+	mouseInsideOverview = false;
+	delayedTOWExit = false;
+ 	unstickPortal();
+	application.centerOverview(false);
+	application.vsm.repaintNow();
+    }
+
+    void stickPortal(){
+	application.to.setNoUpdateWhenMouseStill(true); // prevent tow from moving
+	application.to.resize(Eval.TOW_HORIZONTAL_EXPANSION_OFFSET, Eval.TOW_VERTICAL_EXPANSION_OFFSET);
+	application.to.move(-Eval.TOW_HORIZONTAL_EXPANSION_OFFSET/2, -Eval.TOW_VERTICAL_EXPANSION_OFFSET/2);
+	application.to.setTransparencyValue(1.0f);
+    }
+
+    void unstickPortal(){
+	application.to.setNoUpdateWhenMouseStill(false);
+	application.to.resize(-Eval.TOW_HORIZONTAL_EXPANSION_OFFSET, -Eval.TOW_VERTICAL_EXPANSION_OFFSET);
+    }
+
+}
