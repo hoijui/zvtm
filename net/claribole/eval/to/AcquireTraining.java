0513fad7b7203ff49de4a488cf00a5b229ab4eb1
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2007-02-05 15:33:59 +0000

    use new trailing overview
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@418 74e165f3-c697-40a6-91af-552c8936d7fe

--- net/claribole/eval/to/AcquireTraining.java
+++ net/claribole/eval/to/AcquireTraining.java
@@ -146,9 +146,9 @@ public class AcquireTraining implements TOWApplication, Java2DPainter {
     }
 
     TrailingOverview getPortal(int x, int y){
-	return new TrailingOverview(x-TOW_CONTRACTED_WIDTH/2, y-TOW_CONTRACTED_HEIGHT/2,
-				    TOW_CONTRACTED_WIDTH, TOW_CONTRACTED_HEIGHT,
-				    oCamera, mCamera, 0.0f, TOW_PORTAL_X_OFFSET, TOW_PORTAL_Y_OFFSET);
+	return new TrailingOverviewInv(x-TOW_CONTRACTED_WIDTH/2, y-TOW_CONTRACTED_HEIGHT/2,
+				       TOW_CONTRACTED_WIDTH, TOW_CONTRACTED_HEIGHT,
+				       oCamera, mCamera, 0.0f, TOW_PORTAL_X_OFFSET, TOW_PORTAL_Y_OFFSET);
     }
 
     public void killPortal(){
