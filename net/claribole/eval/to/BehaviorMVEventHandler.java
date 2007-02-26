da51db7356657093e608c1212b6087f70126da4d
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2007-02-26 10:15:24 +0000

    forgot to add two classes
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@440 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ net/claribole/eval/to/BehaviorMVEventHandler.java
@@ -0,0 +1,58 @@
+/*   DATE OF CREATION:  Fri Jan 19 15:35:06 2007
+ *   AUTHOR :           Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   MODIF:             Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   Copyright (c) INRIA, 2007. All Rights Reserved
+ *
+ * $Id:  $
+ */
+
+package net.claribole.eval.to;
+
+import java.awt.Toolkit;
+import java.awt.event.ComponentListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.KeyEvent;
+import java.util.Vector;
+
+import com.xerox.VTM.engine.*;
+import com.xerox.VTM.glyphs.*;
+import net.claribole.zvtm.engine.*;
+
+
+class BehaviorMVEventHandler extends BehaviorEventHandler {
+
+    BehaviorMVEventHandler(BehaviorEval app){
+	super(app);
+    }
+
+    public void press1(ViewPanel v, int mod, int jpx, int jpy, MouseEvent e){
+	lastJPX = jpx;
+	lastJPY = jpy;
+	if (!application.blm.trialStarted){
+	    if (application.blm.sessionStarted && application.blm.im.clickOnStartButton(jpx, jpy)){
+		application.blm.startTrial();
+		return;
+	    }
+	    else {
+		return;
+	    }
+	}
+	if (mouseInsideOverview){
+	    if (application.to.coordInsideObservedRegion(jpx, jpy)){
+ 		orStickedToMouse = true;
+	    }
+	}
+	else {
+	    mCameraStickedToMouse = true;
+	    if (v.lastGlyphEntered() == application.blm.target){
+		application.blm.endTrial();
+	    }
+	    else {
+		application.blm.error();
+	    }
+	}
+    }
+   
+}
---
+++ net/claribole/eval/to/BehaviorTWEventHandler.java
@@ -0,0 +1,55 @@
+/*   DATE OF CREATION:  Fri Jan 19 15:35:06 2007
+ *   AUTHOR :           Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   MODIF:             Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   Copyright (c) INRIA, 2007. All Rights Reserved
+ *
+ * $Id:  $
+ */
+
+package net.claribole.eval.to;
+
+import java.awt.Toolkit;
+import java.awt.event.ComponentListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.KeyEvent;
+import java.util.Vector;
+
+import com.xerox.VTM.engine.*;
+import com.xerox.VTM.glyphs.*;
+import net.claribole.zvtm.engine.*;
+
+
+class BehaviorTWEventHandler extends BehaviorEventHandler {
+
+    BehaviorTWEventHandler(BehaviorEval app){
+	super(app);
+    }
+
+    public void press1(ViewPanel v, int mod, int jpx, int jpy, MouseEvent e){
+	lastJPX = jpx;
+	lastJPY = jpy;
+	if (!application.blm.trialStarted){
+	    if (application.blm.sessionStarted && application.blm.im.clickOnStartButton(jpx, jpy)){
+		application.blm.startTrial();
+		return;
+	    }
+	    else {
+		return;
+	    }
+	}
+	if (mouseInsideOverview){
+	    if (application.to.coordInsideObservedRegion(jpx, jpy)){
+		application.blm.endTrial();
+	    }
+	    else {
+		application.blm.error();
+	    }
+	}
+	else {
+	    mCameraStickedToMouse = true;
+	}
+    }
+   
+}
