5b7170045e83164690c3bdc6db873835dfd98755
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2007-03-21 16:23:12 +0000

    moved projected coordinates from com.xerox.VTM.glyphs into their own package
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@520 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ net/claribole/zvtm/glyphs/projection/ProjCbCurve.java
@@ -0,0 +1,55 @@
+/*   FILE: ProjCbCurve.java
+ *   DATE OF CREATION:   Oct 03 2001
+ *   AUTHOR :            Emmanuel Pietriga (emmanuel.pietriga@xrce.xerox.com)
+ *   MODIF:              Thu Jan 24 10:29:57 2002 by Emmanuel Pietriga
+ *   Copyright (c) Xerox Corporation, XRCE/Contextual Computing, 2002. All Rights Reserved
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * For full terms see the file COPYING.
+ *
+ * $Id: ProjCbCurve.java,v 1.4 2005/12/08 09:08:21 epietrig Exp $
+ */
+
+package net.claribole.zvtm.glyphs.projection;
+
+import java.awt.geom.CubicCurve2D;
+import java.awt.geom.Point2D;
+
+/**project coordinates of a quadratic curve
+ * @author Emmanuel Pietriga
+ */
+
+public class ProjCbCurve extends BProjectedCoords {
+
+    /**curve*/
+    public CubicCurve2D.Double quad=new CubicCurve2D.Double();
+    /**start point*/
+    public Point2D.Double start=new Point2D.Double();
+    /**end point*/
+    public Point2D.Double end=new Point2D.Double();
+    /**control point 1 (controls start point tangent vector)*/
+    public Point2D.Double ctrlStart=new Point2D.Double();
+    /**control point 2 (controls end point tangent vector)*/
+    public Point2D.Double ctrlEnd=new Point2D.Double();
+
+    /**curve*/
+    public CubicCurve2D.Double lquad=new CubicCurve2D.Double();
+    /**start point*/
+    public Point2D.Double lstart=new Point2D.Double();
+    /**end point*/
+    public Point2D.Double lend=new Point2D.Double();
+    /**control point 1 (controls start point tangent vector)*/
+    public Point2D.Double lctrlStart=new Point2D.Double();
+    /**control point 2 (controls end point tangent vector)*/
+    public Point2D.Double lctrlEnd=new Point2D.Double();
+
+}
---
+++ net/claribole/zvtm/glyphs/projection/ProjQdCurve.java
@@ -0,0 +1,50 @@
+/*   FILE: ProjQdCurve.java
+ *   DATE OF CREATION:   Oct 02 2001
+ *   AUTHOR :            Emmanuel Pietriga (emmanuel.pietriga@xrce.xerox.com)
+ *   MODIF:              Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   Copyright (c) Xerox Corporation, XRCE/Contextual Computing, 2002. All Rights Reserved
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * For full terms see the file COPYING.
+ *
+ * $Id: ProjQdCurve.java,v 1.4 2005/12/08 09:08:21 epietrig Exp $
+ */
+
+package net.claribole.zvtm.glyphs.projection;
+
+import java.awt.geom.Point2D;
+import java.awt.geom.QuadCurve2D;
+
+/**project coordinates of a quadratic curve
+ * @author Emmanuel Pietriga
+ */
+
+public class ProjQdCurve extends BProjectedCoords {
+
+    /**curve*/
+    public QuadCurve2D.Double quad=new QuadCurve2D.Double();
+    /**start point*/
+    public Point2D.Double start=new Point2D.Double();
+    /**end point*/
+    public Point2D.Double end=new Point2D.Double();
+    /**control point (controls both start and end points tangent vectors)*/
+    public Point2D.Double ctrl=new Point2D.Double();
+
+    /**curve*/
+    public QuadCurve2D.Double lquad=new QuadCurve2D.Double();
+    /**start point*/
+    public Point2D.Double lstart=new Point2D.Double();
+    /**end point*/
+    public Point2D.Double lend=new Point2D.Double();
+    /**control point (controls both start and end points tangent vectors)*/
+    public Point2D.Double lctrl=new Point2D.Double();
+}
