81f6c97afb404efc2d36e44b09b12234b5320c9c
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2007-03-22 15:50:38 +0000

    made FadeIn and FadeOut public classes so as to allow them to be subclassed, in case more post fade animation actions are to be executed
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@530 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ net/claribole/zvtm/engine/FadeOut.java
@@ -0,0 +1,41 @@
+/*   FILE: TransitionManager.java
+ *   DATE OF CREATION:  Sun Mar 04 10:59:11 2007
+ *   AUTHOR :           Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   MODIF:             Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   Copyright (c) INRIA, 2007. All Rights Reserved
+ *   Licensed under the GNU LGPL. For full terms see the file COPYING.
+ *
+ * $Id: $
+ */ 
+
+package net.claribole.zvtm.engine;
+
+import java.awt.Color;
+
+import com.xerox.VTM.engine.VirtualSpace;
+import com.xerox.VTM.engine.View;
+import com.xerox.VTM.glyphs.Glyph;
+
+/** Fade Out transition for Views.
+ * Subclasses must call super(v, c, vs); in their constructor, and super.animationEnded(target, type, dimension); at the beginning of their implementation of animationEnded(Object target, short type, String dimension).
+ * @author Emmanuel Pietriga
+ */
+
+public class FadeOut implements PostAnimationAction {
+    
+    View view;
+    Color blankColor;
+    VirtualSpace spaceOwningFadeRect;
+
+    public FadeOut(View v, Color c, VirtualSpace vs){
+	this.view = v;
+	this.blankColor = c;
+	this.spaceOwningFadeRect = vs;
+    }
+
+    public void animationEnded(Object target, short type, String dimension){
+	view.setBlank(blankColor);
+	spaceOwningFadeRect.destroyGlyph((Glyph)target);
+    }
+
+}
--- net/claribole/zvtm/engine/TransitionManager.java
+++ net/claribole/zvtm/engine/TransitionManager.java
@@ -62,8 +62,23 @@ public class TransitionManager {
      *@param duration duration of the fade out transition in milliseconds
      *@param fadeColor target color for the fade out
      *@param vsm hook to virtual space manager
+     *@see #fadeOut(View v, int duration, Color fadeColor, VirtualSpaceManager vsm, FadeOut actions)
      */
     public static void fadeOut(View v, int duration, Color fadeColor, VirtualSpaceManager vsm){
+	TransitionManager.fadeOut(v, duration, fadeColor, vsm, null);
+    }
+
+    /** Make a view fade out, and eventually be painted blank.
+     * The view must not be blank for the fade out to work.
+     *@param v the view whose content will fade out
+     *@param duration duration of the fade out transition in milliseconds
+     *@param fadeColor target color for the fade out
+     *@param vsm hook to virtual space manager
+     *@param actions fade out actions. Specify a custom subclass of FadeOut if you need to execute specific actions after the fade out itself. See FadeOut to learn how to properly subclass it.
+     *@see #fadeOut(View v, int duration, Color fadeColor, VirtualSpaceManager vsm)
+     *@see net.claribole.zvtm.engine.FadeOut
+     */
+    public static void fadeOut(View v, int duration, Color fadeColor, VirtualSpaceManager vsm, FadeOut actions){
 	// do not fade out is view is already blank
 	if (v.isBlank() != null){return;}
 	// get the region of virtual space seen through the
@@ -77,7 +92,8 @@ public class TransitionManager {
 	fadeRect.setDrawBorder(false);
 	vsm.addGlyph(fadeRect, c.getOwningSpace());
 	vsm.animator.createGlyphAnimation(duration, AnimManager.GL_COLOR_LIN, fadeOutData,
-					  fadeRect.getID(), new FadeOut(v, fadeColor, c.getOwningSpace()));
+					  fadeRect.getID(),
+					  (actions != null) ? actions : new FadeOut(v, fadeColor, c.getOwningSpace()));
     }
 
     /** Make a view (originally blank) fade in.
@@ -85,8 +101,23 @@ public class TransitionManager {
      *@param v the view whose content will fade in
      *@param duration duration of the fade in transition in milliseconds
      *@param vsm hook to virtual space manager
+     *@see #fadeIn(View v, int duration, VirtualSpaceManager vsm, FadeIn actions)
      */
     public static void fadeIn(View v, int duration, VirtualSpaceManager vsm){
+	TransitionManager.fadeIn(v, duration, vsm, null);
+    }
+    
+
+    /** Make a view (originally blank) fade in.
+     * The view must be blank for the fade in to work.
+     *@param v the view whose content will fade in
+     *@param duration duration of the fade in transition in milliseconds
+     *@param vsm hook to virtual space manager
+     *@param actions fade out actions. Specify a custom subclass of FadeOut if you need to execute specific actions after the fade out itself. See FadeOut to learn how to properly subclass it.
+     *@see #fadeIn(View v, int duration, VirtualSpaceManager vsm)
+     *@see net.claribole.zvtm.engine.FadeIn
+     */
+    public static void fadeIn(View v, int duration, VirtualSpaceManager vsm, FadeIn actions){
 	// do not fade in if view is not blank
 	Color fadeColor = v.isBlank();
 	if (fadeColor == null){return;}
@@ -103,42 +134,8 @@ public class TransitionManager {
 	v.setBlank(null);
 	vsm.repaintNow();
 	vsm.animator.createGlyphAnimation(duration, AnimManager.GL_COLOR_LIN, fadeInData,
-					  fadeRect.getID(), new FadeIn(v, c.getOwningSpace()));
-    }
-
-}
-
-class FadeOut implements PostAnimationAction {
-    
-    View view;
-    Color blankColor;
-    VirtualSpace spaceOwningFadeRect;
-    
-    FadeOut(View v, Color c, VirtualSpace vs){
-	this.view = v;
-	this.blankColor = c;
-	this.spaceOwningFadeRect = vs;
-    }
-
-    public void animationEnded(Object target, short type, String dimension){
-	view.setBlank(blankColor);
-	spaceOwningFadeRect.destroyGlyph((Glyph)target);
-    }
-
-}
-
-class FadeIn implements PostAnimationAction {
-    
-    View view;
-    VirtualSpace spaceOwningFadeRect;
-    
-    FadeIn(View v, VirtualSpace vs){
-	this.view = v;
-	this.spaceOwningFadeRect = vs;
-    }
-
-    public void animationEnded(Object target, short type, String dimension){
-	spaceOwningFadeRect.destroyGlyph((Glyph)target);
+					  fadeRect.getID(),
+					  (actions != null) ? actions : new FadeIn(v, c.getOwningSpace()));
     }
 
 }
