691c4af2a3e742ec3c00322a5a6e85e6ab2bd118
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2007-03-27 09:04:58 +0000

    fixed bug in endpoints constructor (appeared after orient bug fix)
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@556 74e165f3-c697-40a6-91af-552c8936d7fe

--- com/xerox/VTM/glyphs/VSegment.java
+++ com/xerox/VTM/glyphs/VSegment.java
@@ -91,7 +91,7 @@ public class VSegment extends Glyph implements RectangularShape {
 	vx = (x1 + x2) / 2;
 	vy = (y1 + y2) / 2;
 	vz = z;
-	vw = (x1 - x2) / 2;
+	vw = (x2 - x1) / 2;
 	vh = (y2 - y1) / 2;
 	computeSize();
 	setColor(c);
@@ -172,7 +172,7 @@ public class VSegment extends Glyph implements RectangularShape {
     public void setEndPoints(long x1, long y1, long x2, long y2){
 	vx = (x1 + x2) / 2;
 	vy = (y1 + y2) / 2;
-	vw = (x1 - x2) / 2;
+	vw = (x2 - x1) / 2;
 	vh = (y2 - y1) / 2;
 	computeSize();
 	try{vsm.repaintNow();}catch(NullPointerException e){}
