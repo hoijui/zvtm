3ebddec49dd330ab0bae106fd95b7429fd1b7655
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2007-03-29 18:40:14 +0000

    prepared skeleton for dynamic path
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@577 74e165f3-c697-40a6-91af-552c8936d7fe

--- net/claribole/zvtm/glyphs/DPath.java
+++ net/claribole/zvtm/glyphs/DPath.java
@@ -21,7 +21,9 @@ import java.awt.AlphaComposite;
 import com.xerox.VTM.engine.Camera;
 import com.xerox.VTM.engine.LongPoint;
 import com.xerox.VTM.engine.VirtualSpaceManager;
+import com.xerox.VTM.glyphs.Glyph;
 import com.xerox.VTM.glyphs.VPath;
+import net.claribole.zvtm.glyphs.projection.ProjectedCoords;
 import net.claribole.zvtm.lens.Lens;
 
 /**
@@ -37,5 +39,103 @@ import net.claribole.zvtm.lens.Lens;
  */
 
 public class DPath extends Glyph {
+
+    /** For internal use. Dot not tamper with. Made public for outside package subclassing. */
+    public ProjectedCoords[] pc;
+
+    public void initCams(int nbCam){
+	pc = new ProjectedCoords[nbCam];
+	for (int i=0;i<nbCam;i++){
+	    pc[i] = new ProjectedCoords();
+	}
+    }
+
+    public void addCamera(int verifIndex){
+	if (pc!=null){
+	    if (verifIndex==pc.length){
+		ProjectedCoords[] ta = pc;
+		pc = new ProjectedCoords[ta.length+1];
+		for (int i=0;i<ta.length;i++){
+		    pc[i] = ta[i];
+		}
+		pc[pc.length-1] = new ProjectedCoords();
+	    }
+	    else {System.err.println("VPath:Error while adding camera "+verifIndex);}
+	}
+	else {
+	    if (verifIndex==0){
+		pc = new ProjectedCoords[1];
+		pc[0] = new ProjectedCoords();
+	    }
+	    else {System.err.println("VPath:Error while adding camera "+verifIndex);}
+	}
+    }
+
+    public void removeCamera(int index){
+	pc[index] = null;
+    }
+
+    public void resetMouseIn(){
+	for (int i=0;i<pc.length;i++){
+	    resetMouseIn(i);
+	}
+    }
+
+    public void resetMouseIn(int i){
+	if (pc[i]!=null){pc[i].prevMouseIn = false;}
+    }
+    
+    public void sizeTo(float factor){}
+
+    public void reSize(float factor){}
+
+    public void orientTo(float angle){}
+
+    public float getSize(){
+	return size;
+    }
+
+    public float getOrient(){return orient;}
+
+    public boolean fillsView(long w,long h,int camIndex){
+	return false;
+    }
+
+    public boolean coordInside(int x,int y,int camIndex){
+	return false;
+    }
+
+    public short mouseInOut(int x,int y,int camIndex){
+	return Glyph.NO_CURSOR_EVENT;
+    }
+
+    public void project(Camera c, Dimension d){
+	int i = c.getIndex();
+	coef = (float)(c.focal / (c.focal+c.altitude));
+	pc[i].cx = (d.width/2) + Math.round((-c.posx)*coef);
+	pc[i].cy = (d.height/2) - Math.round((-c.posy)*coef);
+    }
+
+    public void projectForLens(Camera c, int lensWidth, int lensHeight, float lensMag, long lensx, long lensy){
+	int i = c.getIndex();
+	coef = (float)(c.focal / (c.focal+c.altitude)) * lensMag;
+	pc[i].lcx = (lensWidth/2) + Math.round((-lensx)*coef);
+	pc[i].lcy = (lensHeight/2) - Math.round((-lensy)*coef);
+    }
+
+    public void draw(Graphics2D g,int vW,int vH,int i,Stroke stdS,AffineTransform stdT, int dx, int dy){
+
+    }
+
+    public void drawForLens(Graphics2D g,int vW,int vH,int i,Stroke stdS,AffineTransform stdT, int dx, int dy){
+
+    }
+
+    /** Not implemented yet. */
+    public Object clone(){
+	return new DPath();
+    }
+
+    public void highlight(boolean b, Color selectedColor){}
 
 }
