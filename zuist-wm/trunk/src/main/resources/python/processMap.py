5e7b40c2fbb7be7fce10226475907be5ff2be61f
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2007-11-22 09:07:45 +0000

    skeleton file
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@968 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ zuist-wm/trunk/src/main/resources/python/processMap.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# $Id$
+
+from PIL import Image
+
+TRACE_LEVEL = 1
+
+################################################################################
+# Trace exec on std output
+################################################################################
+def log(msg, level=0):
+    if level <= TRACE_LEVEL:
+        print msg
+
+################################################################################
+# main
+################################################################################
+if len(sys.argv) > 1:
+    SRC_DIR = os.path.realpath(sys.argv[1])
+    if len(sys.argv) > 2:
+        TRACE_LEVEL = int(sys.argv[2])
+else:
+    sys.exit(0)
+
