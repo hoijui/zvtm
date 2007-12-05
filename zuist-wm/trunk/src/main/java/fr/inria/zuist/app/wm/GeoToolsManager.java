f571fcefc0c5e4707680689e0867b14969d9852c
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2007-12-05 16:08:58 +0000

    more debugging, support for multiple shapefiles
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1018 74e165f3-c697-40a6-91af-552c8936d7fe

--- zuist-wm/trunk/src/main/java/fr/inria/zuist/app/wm/GeoToolsManager.java
+++ zuist-wm/trunk/src/main/java/fr/inria/zuist/app/wm/GeoToolsManager.java
@@ -25,7 +25,9 @@ import org.geotools.data.shapefile.shp.JTSUtilities;
 
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jts.simplify.DouglasPeuckerSimplifier;
+import com.vividsolutions.jts.geom.util.PolygonExtracter;
 
 import com.xerox.VTM.glyphs.VSegment;
 import com.xerox.VTM.glyphs.VPolygon;
@@ -47,25 +49,31 @@ class GeoToolsManager {
     PolygonDescription[] countryBoundaries;
     
     static final String[] transitions = {Region.APPEAR_STR, Region.APPEAR_STR, Region.DISAPPEAR_STR, Region.DISAPPEAR_STR};
-    
-    // polygon 968 is Canada, and contains an error
-    // polygon 1182 is Chile, and contains an error
+
+    int polygonID = 0;
     
     GeoToolsManager(WorldExplorer app){
         this.application = app;
-        init();
+        Region region = application.sm.createRegion(0, 0, 84600, 43200, 3, "BR0", 1,
+                                                    transitions, Region.ORDERING_DISTANCE_STR,
+                                                    false, "Boundaries", null, null);
+
+        load(new File("data/shapefiles/misc/countries.shp"), "Loading countries...", region);
+//        load(new File("data/shapefiles/ca_provinces/province.shp"), "Loading Canadian provinces...", region);
+//        load(new File("data/shapefiles/us_states/statesp020.shp"), "Loading US states...", region);
+//        load(new File("data/shapefiles/mx_states/mx_state.shp"), "Loading Mexican states...", region);
+//        load(new File("data/shapefiles/russia/RUS1.shp"), "Loading Russian administrative divisions...", region);
+//        load(new File("data/shapefiles/china/CHN0.shp"), "Loading Chinese administrative divisions...", region);
     }
 
-    void init(){
+    void load(File shapeFile, String msg, Region region){
         int progress = 0;
         application.gp.setValue(0);
-        application.gp.setLabel("Loading country boundaries...");
-        File file = new File("data/shapefiles/world_borders.shp");
-        //File file = new File("data/shapefiles2/countries.shp");
+        application.gp.setLabel(msg);
         Map connect = new HashMap();
         int newProgress = 0;
         try {
-            connect.put("url", file.toURL());
+            connect.put("url", shapeFile.toURL());
             try {
                 DataStore dataStore = DataStoreFinder.getDataStore(connect);
                 String[] typeNames = dataStore.getTypeNames();
@@ -75,24 +83,26 @@ class GeoToolsManager {
                 LongPoint[] zvtmCoords;
                 Vector points = new Vector();
                 countryBoundaries = new PolygonDescription[features.length];
-                Region region = application.sm.createRegion(0, 0, 84600, 43200, 0, "BR0", 1,
-                                                            transitions, Region.ORDERING_DISTANCE_STR,
-                                                            false, "Boundaries", null, null);
                 for (int i=0;i<features.length;i++){
                     Feature feature = features[i];
-                    //Geometry geometry = DouglasPeuckerSimplifier.simplify(feature.getDefaultGeometry(), 0.01);
-                    Geometry geometry = feature.getDefaultGeometry();
-                    Coordinate[] coords = geometry.getCoordinates();
-                    points.clear();
-                    for (int j=0;j<coords.length;j+=1){
-                        points.add(new LongPoint(Math.round(coords[j].x*CC), Math.round(coords[j].y*CC)));
+                    Geometry geometry = feature.getPrimaryGeometry();                    
+                    Object[] polygons = PolygonExtracter.getPolygons(geometry).toArray();
+                    for (int k=0;k<polygons.length;k++){
+                    
+                        Geometry simplifiedPolygon = DouglasPeuckerSimplifier.simplify((Geometry)polygons[k], 0.00001);
+                        //Geometry simplifiedPolygon = (Geometry)polygons[k];
+                        Coordinate[] coords = simplifiedPolygon.getCoordinates();
+                        points.clear();
+                        for (int j=0;j<coords.length;j+=1){
+                            points.add(new LongPoint(Math.round(coords[j].x*CC), Math.round(coords[j].y*CC)));
+                        }
+                        zvtmCoords = new LongPoint[points.size()];
+                        for (int j=0;j<zvtmCoords.length;j++){
+                            zvtmCoords[j] = (LongPoint)points.elementAt(j);
+                        }
+                        application.sm.createPolygon(zvtmCoords, "B"+Integer.toString(polygonID++), region,
+                            false, null, Color.YELLOW);
                     }
-                    zvtmCoords = new LongPoint[points.size()];
-                    for (int j=0;j<zvtmCoords.length;j++){
-                        zvtmCoords[j] = (LongPoint)points.elementAt(j);
-                    }
-                    application.sm.createPolygon(zvtmCoords, "B"+Integer.toString(i), region,
-                                                 false, null, Color.YELLOW);
                     newProgress = i *100 / features.length;
                     if (newProgress > progress){
                         progress = newProgress;
