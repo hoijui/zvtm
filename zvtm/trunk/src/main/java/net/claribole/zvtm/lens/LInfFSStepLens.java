9ccd60100ce8aba7a25cbecf422148de67749c6c
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2007-12-19 09:28:48 +0000

    new step lenses
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1038 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ zvtm/trunk/src/main/java/net/claribole/zvtm/lens/LInfFSStepLens.java
@@ -0,0 +1,84 @@
+/*   AUTHOR :           Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   Copyright (c) INRIA, 2007. All Rights Reserved
+ *   Licensed under the GNU LGPL. For full terms see the file COPYING.
+ *
+ * $Id$
+ */
+
+package net.claribole.zvtm.lens;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+/**Profile: Step-like (flat steps) - Distance metric: L(Inf) (square shape)<br>Size expressed as an absolute value in pixels*/
+
+public class LInfFSStepLens extends FSStepLens {
+
+    /**
+        * create a lens with a maximum magnification factor of 2.0
+        */
+    public LInfFSStepLens(){
+        super();
+    }
+
+    /**
+        * create a lens with a given maximum magnification factor
+        *
+        *@param mm maximum magnification factor, mm in [0,+inf[
+        *@param ns number of flat steps between context and focus
+        */
+    public LInfFSStepLens(float mm, int ns){
+        super(mm, ns);
+    }
+
+    /**
+        * create a lens with a given maximum magnification factor, inner and outer radii
+        *
+        *@param mm maximum magnification factor, mm in [0,+inf[
+        *@param outerRadius outer radius (beyond which no magnification is applied - outward)
+        *@param innerRadius inner radius (beyond which maximum magnification is applied - inward)
+        *@param ns number of flat steps between context and focus
+        */
+    public LInfFSStepLens(float mm, int outerRadius, int innerRadius, int ns){
+        super(mm, outerRadius, innerRadius, ns);
+    }
+
+    /**
+        * create a lens with a given maximum magnification factor, inner and outer radii
+        *
+        *@param mm maximum magnification factor, mm in [0,+inf[
+        *@param outerRadius outer radius (beyond which no magnification is applied - outward)
+        *@param innerRadius inner radius (beyond which maximum magnification is applied - inward)
+        *@param ns number of flat steps between context and focus
+        *@param x horizontal coordinate of the lens' center (as an offset w.r.t the view's center coordinates)
+        *@param y vertical coordinate of the lens' center (as an offset w.r.t the view's center coordinates)
+        */
+    public LInfFSStepLens(float mm, int outerRadius, int innerRadius, int ns, int x, int y){
+        super(mm, outerRadius, innerRadius, ns, x, y);
+    }
+
+    public void gf(float x, float y, float[] g){
+        d = Math.max(Math.abs(x-sw-lx), Math.abs(y-sh-ly));
+        if (d <= LR2)
+            g[0] = g[1] = MM;
+        else if (d <= LR1){
+            for (int i=0;i<Ri.length;i++){
+                if (d <= Ri[i]){
+                    g[0] = g[1] = Ai[i]*(float)d + Bi[i];
+                    break;
+                }
+            }
+        }
+        else
+            g[0] = g[1] = 1;
+    }
+    
+    public void drawBoundary(Graphics2D g2d){
+	    g2d.setColor(java.awt.Color.RED);
+	    for (int i=0;i<nbSteps;i++){
+    	    g2d.drawRect(Math.round(lx+w/2-lensWidth/2/((float)(nbSteps*i))), Math.round(ly+h/2-lensHeight/2/((float)(nbSteps*i))),
+    	                 Math.round(lensWidth/((float)(nbSteps*i))), Math.round(lensHeight/((float)(nbSteps*i))));
+	    }
+    }
+
+}
