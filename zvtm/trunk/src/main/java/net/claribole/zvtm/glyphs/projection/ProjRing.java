1c88a41c12f08f6d2561bd486d778eff2efab44c
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2008-06-06 10:41:08 +0000

    new Glyph: slice of a ring
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1078 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ zvtm/trunk/src/main/java/net/claribole/zvtm/glyphs/projection/ProjRing.java
@@ -0,0 +1,20 @@
+/*   AUTHOR :           Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   Copyright (c) INRIA, 2008. All Rights Reserved
+ *   Licensed under the GNU LGPL. For full terms see the file COPYING.
+ *
+ * $Id: $
+ */
+
+package net.claribole.zvtm.glyphs.projection;
+
+/**project coordinates of a ring slice
+ * @author Emmanuel Pietriga
+ */
+
+public class ProjRing extends ProjSlice {
+
+    public int innerRingRadius;
+
+    public int linnerRingRadius;
+    
+}
