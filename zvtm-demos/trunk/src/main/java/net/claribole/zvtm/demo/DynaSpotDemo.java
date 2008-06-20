11f24e260fb14d02d2ad0e62df7c6ae3d245dd79
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2008-06-20 14:04:51 +0000

    added a control panel for dynaspot parameters
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1114 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-demos/trunk/src/main/java/net/claribole/zvtm/demo/DynaSpotDemo.java
+++ zvtm-demos/trunk/src/main/java/net/claribole/zvtm/demo/DynaSpotDemo.java
@@ -10,6 +10,17 @@ package net.claribole.zvtm.demo;
 
 import java.awt.Color;
 import java.util.Vector;
+import javax.swing.JFrame;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.JSpinner;
+import javax.swing.JLabel;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowListener;
+import java.awt.event.WindowEvent;
+import java.awt.Container;
+import java.awt.GridLayout;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import com.xerox.VTM.engine.*;
 import com.xerox.VTM.glyphs.*;
@@ -24,10 +35,13 @@ public class DynaSpotDemo {
 
     View demoView;
 
+	DynaSpotDemoControlPanel cp;
+
     DynaSpotDemo(short ogl){
         vsm=new VirtualSpaceManager();
         vsm.setDebug(true);
         initTest(ogl);
+		cp = new DynaSpotDemoControlPanel(this);
     }
 
     public void initTest(short ogl){
@@ -50,8 +64,9 @@ public class DynaSpotDemo {
 		vsm.addGlyph(new VCircle(300,0,0,4,Color.BLACK), "src");
         vsm.repaintNow();
 		// DynaSpot setup and activation
-		demoView.getCursor().setDynaSpotMaxRadius(40);
-		demoView.getCursor().setCutoffFrequencyParameters(3, 0.1);
+		demoView.getCursor().setDynaSpotMaxRadius(20);
+		demoView.getCursor().setCutoffFrequencyParameters(1.3, 0.01);
+		demoView.getCursor().setOffsets(20, 20);
 		demoView.getCursor().activateDynaSpot(true);
     }
     
@@ -64,4 +79,53 @@ public class DynaSpotDemo {
         new DynaSpotDemo((args.length > 0) ? Short.parseShort(args[0]) : 0);
     }
     
+}
+
+class DynaSpotDemoControlPanel extends JFrame implements ChangeListener {
+	
+	DynaSpotDemo application;
+	JSpinner cutOffA, cutOffB, offset, maxRadius;
+	
+	DynaSpotDemoControlPanel(DynaSpotDemo app){
+		this.application = app;
+		Container cpane = getContentPane();
+		cpane.setLayout(new GridLayout(8, 1));
+		
+		cutOffA = new JSpinner(new SpinnerNumberModel(application.demoView.getCursor().getCutoffFrequencyParameterA(), 0.1, 5.0, 0.1));
+		cutOffB = new JSpinner(new SpinnerNumberModel(application.demoView.getCursor().getCutoffFrequencyParameterB(), 0.0001, 1, 0.001));
+		offset = new JSpinner(new SpinnerNumberModel(application.demoView.getCursor().getDynaSpotMaxRadius(), 0, 50, 1));
+		maxRadius = new JSpinner(new SpinnerNumberModel(application.demoView.getCursor().getOffsets().x, 1, 100, 1));
+		
+		cpane.add(new JLabel("Low-pass filter cutoff param A"));
+		cpane.add(cutOffA);
+		cpane.add(new JLabel("Low-pass filter cutoff param B"));
+		cpane.add(cutOffB);
+		cpane.add(new JLabel("Distance offset"));
+		cpane.add(offset);
+		cpane.add(new JLabel("DynaSpot maximum Radius"));
+		cpane.add(maxRadius);
+		cutOffA.addChangeListener(this);
+		cutOffB.addChangeListener(this);
+		offset.addChangeListener(this);
+		maxRadius.addChangeListener(this);
+		
+		WindowListener w0 = new WindowAdapter(){
+			public void windowClosing(WindowEvent e){System.exit(0);}
+		    };
+		this.addWindowListener(w0);
+		this.setLocation(900, 0);
+		this.setSize(400, 200);
+		this.setTitle("DynaSpot Parameters");
+		this.setVisible(true);
+		
+	}
+	
+	public void stateChanged(ChangeEvent e){
+		Object o = e.getSource();
+		if (o == cutOffA){application.demoView.getCursor().setCutoffFrequencyParameters(((Number)cutOffA.getValue()).doubleValue(), ((Number)cutOffB.getValue()).doubleValue());}
+		else if (o == cutOffB){application.demoView.getCursor().setCutoffFrequencyParameters(((Number)cutOffA.getValue()).doubleValue(), ((Number)cutOffB.getValue()).doubleValue());}
+		else if (o == offset){application.demoView.getCursor().setOffsets(((Number)offset.getValue()).intValue(), ((Number)offset.getValue()).intValue());}
+		else if (o == maxRadius){application.demoView.getCursor().setDynaSpotMaxRadius(((Number)maxRadius.getValue()).intValue());}
+	}
+	
 }
