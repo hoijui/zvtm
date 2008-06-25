100bdfc45992087b6b488c40256f589d3c9b90a9
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2008-06-25 14:16:57 +0000

    keyword ID
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1132 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-demos/trunk/src/main/java/net/claribole/zvtm/demo/DynaSpotDemoEvtHdlr.java
+++ zvtm-demos/trunk/src/main/java/net/claribole/zvtm/demo/DynaSpotDemoEvtHdlr.java
@@ -3,7 +3,7 @@
  *   Copyright (c) INRIA, 2007-2008. All Rights Reserved
  *   Licensed under the GNU LGPL. For full terms see the file COPYING.
  *
- * $Id:  $
+ * $Id$
  */
 
 package net.claribole.zvtm.demo;
