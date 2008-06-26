66500366acb2301449688a22bf93d5389661d7b2
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2008-06-26 14:20:54 +0000

    minor adjustment of pie menu labels
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1147 74e165f3-c697-40a6-91af-552c8936d7fe

--- zuist-engine/trunk/src/main/java/fr/inria/zuist/viewer/Messages.java
+++ zuist-engine/trunk/src/main/java/fr/inria/zuist/viewer/Messages.java
@@ -29,7 +29,7 @@ class Messages {
     static final String PM_RELOAD = "Reload";
 
     static final String[] mainMenuLabels = {PM_GLOBALVIEW, PM_OPEN, PM_BACK, PM_RELOAD};
-    static final LongPoint[] mainMenuLabelOffsets = {new LongPoint(0, 0), new LongPoint(0, 0),
-						     new LongPoint(0, 0), new LongPoint(0, -10)};    
+    static final LongPoint[] mainMenuLabelOffsets = {new LongPoint(10, 0), new LongPoint(0, 0),
+						     new LongPoint(-10, 0), new LongPoint(0, -10)};    
 
 }
