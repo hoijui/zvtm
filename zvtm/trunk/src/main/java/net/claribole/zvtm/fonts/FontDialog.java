631c56d6795d1cb939a5fa498843f4425831e291
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2008-08-13 16:03:07 +0000

    preamble
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1207 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm/trunk/src/main/java/net/claribole/zvtm/fonts/FontDialog.java
+++ zvtm/trunk/src/main/java/net/claribole/zvtm/fonts/FontDialog.java
@@ -3,7 +3,10 @@
  *   AUTHOR :            Emmanuel Pietriga (emmanuel@w3.org)
  *   MODIF:              Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
  *   Copyright (c) Emmanuel Pietriga, 2002. All Rights Reserved
+ *   Copyright (c) INRIA, 2008. All Rights Reserved
  *   Licensed under the GNU LGPL. For full terms see the file COPYING.
+ *
+ * $Id:$
  */
 
 package net.claribole.zvtm.fonts;
