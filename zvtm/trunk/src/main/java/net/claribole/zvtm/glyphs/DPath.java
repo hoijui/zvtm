1545aac8f2996b8be5879052df8daaba6f3eaada
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2008-08-14 10:55:04 +0000

    updated (c) statement
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1213 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm/trunk/src/main/java/net/claribole/zvtm/glyphs/DPath.java
+++ zvtm/trunk/src/main/java/net/claribole/zvtm/glyphs/DPath.java
@@ -1,7 +1,7 @@
 /*   FILE: DPath.java
  *   DATE OF CREATION:   Thu Mar 29 19:33 2007
  *   AUTHOR :            Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
- *   Copyright (c) INRIA, 2007. All Rights Reserved
+ *   Copyright (c) INRIA, 2007-2008. All Rights Reserved
  *   Licensed under the GNU LGPL. For full terms see the file COPYING.
  *
  * $Id$
--- zvtm/trunk/src/main/java/net/claribole/zvtm/glyphs/DPathST.java
+++ zvtm/trunk/src/main/java/net/claribole/zvtm/glyphs/DPathST.java
@@ -1,7 +1,7 @@
 /*   FILE: DPathST.java
  *   DATE OF CREATION:   Sat Apr 14 10:16 2007
  *   AUTHOR :            Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
- *   Copyright (c) INRIA, 2007. All Rights Reserved
+ *   Copyright (c) INRIA, 2007-2008. All Rights Reserved
  *   Licensed under the GNU LGPL. For full terms see the file COPYING.
  *
  * $Id$
