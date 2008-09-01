86c3ba95993bacdbc84f30997f8121820666c259
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2008-09-01 06:52:43 +0000

    upd (c) statement
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1268 74e165f3-c697-40a6-91af-552c8936d7fe

--- zuist-engine/trunk/src/main/java/fr/inria/zuist/engine/Region.java
+++ zuist-engine/trunk/src/main/java/fr/inria/zuist/engine/Region.java
@@ -1,5 +1,5 @@
 /*   AUTHOR :           Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
- *   Copyright (c) INRIA, 2007. All Rights Reserved
+ *   Copyright (c) INRIA, 2007-2008. All Rights Reserved
  *   Licensed under the GNU LGPL. For full terms see the file COPYING.
  *
  * $Id: Region.java,v 1.12 2007/10/02 09:34:10 pietriga Exp $
