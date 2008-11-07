83317d69ce91f65419ba6f2f64b904d2883654f4
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2008-11-07 08:45:37 +0000

    ZVTM 0.9.8-SNAPSHOT
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1335 74e165f3-c697-40a6-91af-552c8936d7fe

--- eval/trunk/alphalb.sh
+++ eval/trunk/alphalb.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xmx1024M -Xms512M -classpath "target/classes:target/zvtm-0.9.7-SNAPSHOT.jar" net.claribole.eval.alphalens.EvalAcqLabel "$@"
+java -Xmx1024M -Xms512M -classpath "target/classes:target/zvtm-0.9.8-SNAPSHOT.jar" net.claribole.eval.alphalens.EvalAcqLabel "$@"
