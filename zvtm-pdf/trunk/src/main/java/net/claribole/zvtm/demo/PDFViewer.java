94c38ca224cd81205807f927ae6ccd1a1b89aed2
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2008-12-04 14:47:14 +0000

    - PDF page glyph now working (not fully operational) - demo pdf document viewer
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1384 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ zvtm-pdf/trunk/src/main/java/net/claribole/zvtm/demo/PDFViewer.java
@@ -0,0 +1,219 @@
+/*   AUTHOR :            Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   Copyright (c) INRIA, 2008. All Rights Reserved
+ *   Licensed under the GNU LGPL. For full terms see the file COPYING.
+ *
+ * $Id:  $
+ */
+
+package net.claribole.zvtm.demo;
+
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+import java.util.Vector;
+
+import com.xerox.VTM.engine.VirtualSpaceManager;
+import com.xerox.VTM.engine.VirtualSpace;
+import com.xerox.VTM.engine.View;
+import com.xerox.VTM.engine.ViewPanel;
+import com.xerox.VTM.engine.Camera;
+import com.xerox.VTM.glyphs.Glyph;
+import net.claribole.zvtm.engine.ViewEventHandler;
+import net.claribole.zvtm.glyphs.ZPDFPage;
+
+import com.sun.pdfview.PDFFile;
+import com.sun.pdfview.PDFPage;
+
+public class PDFViewer {
+	
+	static final int NAV_ANIM_DURATION = 300;
+
+	VirtualSpaceManager vsm;
+	VirtualSpace vs;
+	static final String spaceName = "pdfSpace";
+	ViewEventHandler eh;
+	Camera mCamera;
+
+	View pdfView;
+
+	PDFViewer(String pdfFilePath){
+		vsm = new VirtualSpaceManager();
+		vsm.setDebug(true);
+		initGUI();
+		load(new File(pdfFilePath));
+	}
+
+	public void initGUI(){
+		vs = vsm.addVirtualSpace(spaceName);
+		vsm.setZoomLimit(-90);
+		mCamera = vsm.addCamera(spaceName);
+		Vector cameras = new Vector();
+		cameras.add(mCamera);
+		pdfView = vsm.addExternalView(cameras, "ZVTM PDF Viewer", View.STD_VIEW, 800, 600, false, true, true, null);
+		pdfView.setBackgroundColor(Color.WHITE);
+		eh = new PDFViewerEventHandler(this);
+		pdfView.setEventHandler(eh);
+		pdfView.setAntialiasing(true);
+		mCamera.setAltitude(0);
+		vsm.repaintNow();
+	}
+
+	void load(File f){
+		try {
+			RandomAccessFile raf = new RandomAccessFile(f, "r");
+			FileChannel channel = raf.getChannel();
+			ByteBuffer buf = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
+			PDFFile pdfFile = new PDFFile(buf);
+			for (int i=0;i<pdfFile.getNumPages();i++){
+				try {
+					vsm.addGlyph(new ZPDFPage(i*700, 0, 0, pdfFile.getPage(i+1), 1, 1), vs);				
+				}
+				catch ( Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		catch ( Exception e) { 
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args){
+		System.out.println("-----------------");
+		System.out.println("General information");
+		System.out.println("JVM version: "+System.getProperty("java.vm.vendor")+" "+System.getProperty("java.vm.name")+" "+System.getProperty("java.vm.version"));
+		System.out.println("OS type: "+System.getProperty("os.name")+" "+System.getProperty("os.version")+"/"+System.getProperty("os.arch")+" "+System.getProperty("sun.cpu.isalist"));
+		System.out.println("-----------------");
+		System.out.println("Directory information");
+		System.out.println("Java Classpath: "+System.getProperty("java.class.path"));	
+		System.out.println("Java directory: "+System.getProperty("java.home"));
+		System.out.println("Launching from: "+System.getProperty("user.dir"));
+		System.out.println("-----------------");
+		System.out.println("User informations");
+		System.out.println("User name: "+System.getProperty("user.name"));
+		System.out.println("User home directory: "+System.getProperty("user.home"));
+		System.out.println("-----------------");
+		new PDFViewer((args.length > 0) ? args[0] : null);
+	}
+
+}
+
+class PDFViewerEventHandler implements ViewEventHandler {
+
+	PDFViewer application;
+
+	long lastJPX,lastJPY;
+
+	PDFViewerEventHandler(PDFViewer appli){
+		application = appli;
+	}
+
+	boolean dragging = false;
+
+	public void press1(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
+		lastJPX=jpx;
+		lastJPY=jpy;
+		v.setDrawDrag(true);
+		application.vsm.activeView.mouse.setSensitivity(false);
+	}
+
+	public void release1(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
+		application.vsm.animator.Xspeed=0;
+		application.vsm.animator.Yspeed=0;
+		application.vsm.animator.Aspeed=0;
+		v.setDrawDrag(false);
+		application.vsm.activeView.mouse.setSensitivity(true);
+	}
+
+	public void click1(ViewPanel v,int mod,int jpx,int jpy,int clickNumber, MouseEvent e){
+		Glyph g = v.lastGlyphEntered();
+		if (g != null){
+			application.vsm.centerOnGlyph(g, application.mCamera, PDFViewer.NAV_ANIM_DURATION);
+		}
+		else {
+			application.vsm.getGlobalView(application.mCamera, PDFViewer.NAV_ANIM_DURATION);
+		}
+	}
+
+	public void press2(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
+
+	}
+
+	public void release2(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
+	}
+
+	public void click2(ViewPanel v,int mod,int jpx,int jpy,int clickNumber, MouseEvent e){
+	}
+
+	public void press3(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
+	}
+
+	public void release3(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
+	}
+
+	public void click3(ViewPanel v,int mod,int jpx,int jpy,int clickNumber, MouseEvent e){}
+
+	public void mouseMoved(ViewPanel v,int jpx,int jpy, MouseEvent e){}
+
+	public void mouseDragged(ViewPanel v,int mod,int buttonNumber,int jpx,int jpy, MouseEvent e){
+		if (buttonNumber == 1){
+			Camera c = application.vsm.getActiveCamera();
+			float a = (c.focal+Math.abs(c.altitude))/c.focal;
+			if (mod == SHIFT_MOD) {
+				application.vsm.animator.Xspeed = 0;
+				application.vsm.animator.Yspeed = 0;
+				application.vsm.animator.Aspeed = (c.altitude>0) ? (long)((lastJPY-jpy)*(a/50.0f)) : (long)((lastJPY-jpy)/(a*50));
+				//50 is just a speed factor (too fast otherwise)
+			}
+			else {
+				application.vsm.animator.Xspeed = (c.altitude>0) ? (long)((jpx-lastJPX)*(a/50.0f)) : (long)((jpx-lastJPX)/(a*50));
+				application.vsm.animator.Yspeed = (c.altitude>0) ? (long)((lastJPY-jpy)*(a/50.0f)) : (long)((lastJPY-jpy)/(a*50));
+				application.vsm.animator.Aspeed = 0;
+			}
+		}
+	}
+
+	public void mouseWheelMoved(ViewPanel v,short wheelDirection,int jpx,int jpy, MouseWheelEvent e){
+		Camera c = application.vsm.getActiveCamera();
+		float a = (c.focal+Math.abs(c.altitude))/c.focal;
+		if (wheelDirection == WHEEL_UP){
+			c.altitudeOffset(-a*5);
+			application.vsm.repaintNow();
+		}
+		else {
+			//wheelDirection == WHEEL_DOWN
+			c.altitudeOffset(a*5);
+			application.vsm.repaintNow();
+		}
+	}
+
+	public void enterGlyph(Glyph g){
+	}
+
+	public void exitGlyph(Glyph g){
+	}
+
+	public void Ktype(ViewPanel v,char c,int code,int mod, KeyEvent e){}
+
+	public void Kpress(ViewPanel v,char c,int code,int mod, KeyEvent e){}
+
+	public void Krelease(ViewPanel v,char c,int code,int mod, KeyEvent e){}
+
+	public void viewActivated(View v){}
+
+	public void viewDeactivated(View v){}
+
+	public void viewIconified(View v){}
+
+	public void viewDeiconified(View v){}
+
+	public void viewClosing(View v){System.exit(0);}
+
+}
--- zvtm-pdf/trunk/src/main/java/net/claribole/zvtm/glyphs/ZPDFPage.java
+++ zvtm-pdf/trunk/src/main/java/net/claribole/zvtm/glyphs/ZPDFPage.java
@@ -1,4 +1,4 @@
-/*   FILE: DPath.java
+/*   FILE: ZPDFPage.java
  *   AUTHOR :            Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
  *   Copyright (c) INRIA, 2008. All Rights Reserved
  *   Licensed under the GNU LGPL. For full terms see the file COPYING.
@@ -8,10 +8,14 @@
 
 package net.claribole.zvtm.glyphs;
 
+import java.awt.Image;
+import java.awt.Rectangle;
+
 import com.xerox.VTM.engine.Camera;
 import com.xerox.VTM.engine.LongPoint;
 import com.xerox.VTM.glyphs.Glyph;
 import com.xerox.VTM.glyphs.ClosedShape;
+import com.xerox.VTM.glyphs.VImage;
 import com.xerox.VTM.glyphs.RectangularShape;
 import net.claribole.zvtm.glyphs.projection.RProjectedCoordsP;
 
@@ -23,28 +27,160 @@ import java.awt.Shape;
 import java.awt.Stroke;
 import java.awt.geom.AffineTransform;
 
-import com.sun.pdfview.PDFFile;
 import com.sun.pdfview.PDFPage;
 
 /** Glyph encapsulating a PDFPage from <a href="https://pdf-renderer.dev.java.net/">SwingLabs' PDFRenderer</a>.
-	*@author Emmanuel Pietriga
-	*/
+ *@author Emmanuel Pietriga
+ */
 
 public class ZPDFPage extends ClosedShape implements RectangularShape {
+
+	Image pageImage;
 
 	/** Page width in virtual space. */
 	long vw;
 	/** Page height in virtual space. */
 	long vh;
+	/** Aspect ratio: width divided by height (read-only). */
+    public float ar;
 
-    public void initCams(int nbCam){}
+    /** For internal use. Made public for easier outside package subclassing. */
+    public AffineTransform at;
 
-    public void addCamera(int verifIndex){}
+    /** For internal use. Made public for easier outside package subclassing. */
+    public RProjectedCoordsP[] pc;
+	
+	/** For internal use. Made public for easier outside package subclassing. */
+    public boolean zoomSensitive = true;
 
-    public void removeCamera(int index){}
+    /** For internal use. Made public for easier outside package subclassing. */
+    public float scaleFactor = 1.0f;
+    
+    /** For internal use. Made public for easier outside package subclassing. */
+    public float trueCoef = 1.0f;
+
+    /** Indicates when a border is drawn around the image (read-only).
+     * One of DRAW_BORDER_*
+     */
+    public short drawBorder = VImage.DRAW_BORDER_ALWAYS;
+    
+	/** Instantiate a PDF page as a ZVTM glyph, rendered at a resolution that matches the default scale for that page.
+	 *@param page the PDF page from pdf-renderer
+	 */
+	public ZPDFPage(long x, long y, int z, PDFPage page){
+		vx = x;
+		vy = y;
+		vz = z;
+		// get the width and height for the doc at the default zoom 
+		Rectangle rect = new Rectangle(0, 0, (int)page.getBBox().getWidth(), (int)page.getBBox().getHeight());
+		// generate the image
+		pageImage = page.getImage(rect.width, rect.height,
+		 	rect,   // clip rect
+			null,   // null for the ImageObserver
+			true,   // fill background with white
+			true);  // block until drawing is done
+		vw = Math.round(pageImage.getWidth(null)/2.0);
+		vh = Math.round(pageImage.getHeight(null)/2.0);
+		if (vw==0 && vh==0){ar = 1.0f;}
+		else {ar = (float)vw/(float)vh;}
+		computeSize();
+		orient = 0;
+	}
+
+	/** Instantiate a PDF page as a ZVTM glyph, rendered at a resolution that matches the default scale for that page.
+	 *@param page the PDF page from pdf-renderer
+	 *@param detailFactor multiplication factor applied to compute the actual width and height of the bitmap image in which to render the page, taking the default rendering scale as a basis
+     *@param scale scaleFactor in virtual space w.r.t specified image size (default is 1.0)
+	 */
+	public ZPDFPage(long x, long y, int z, PDFPage page, float detailFactor, double scale){
+		vx = x;
+		vy = y;
+		vz = z;
+		// get the width and height for the doc at the default zoom 
+		Rectangle rect = new Rectangle(0, 0, (int)(detailFactor*page.getBBox().getWidth()), (int)(detailFactor*page.getBBox().getHeight()));
+		// generate the image
+		pageImage = page.getImage((int)(detailFactor*rect.width), (int)(detailFactor*rect.height),
+		 	rect,   // clip rect
+			null,   // null for the ImageObserver
+			true,   // fill background with white
+			true);  // block until drawing is done
+		vw = Math.round(pageImage.getWidth(null)/2.0);
+		vh = Math.round(pageImage.getHeight(null)/2.0);
+		if (vw==0 && vh==0){ar = 1.0f;}
+		else {ar = (float)vw/(float)vh;}
+		computeSize();
+		orient = 0;		
+		scaleFactor = (float)scale;
+	}
+
+	/** Instantiate a PDF page as a ZVTM glyph, rendered at a resolution that matches the default scale for that page.
+	 *@param page the PDF page from pdf-renderer
+	 *@param w width of bitmap image in which to render the page
+	 *@param h height of bitmap image in which to render the page
+     *@param scale scaleFactor in virtual space w.r.t specified image size (default is 1.0)
+	 */
+	public ZPDFPage(long x, long y, int z, PDFPage page, int w, int h, double scale){
+		vx = x;
+		vy = y;
+		vz = z;
+		// get the width and height for the doc at the default zoom
+		//XXX:TBW would probably be a good idea to throw some kind of warning if 
+		//        w and h are smaller than default scale ? not sure... depends on how page.getImage() behaves in this case
+		Rectangle rect = new Rectangle(0, 0, w, h);
+		// generate the image
+		pageImage = page.getImage(rect.width, rect.height,
+		 	rect,   // clip rect
+			null,   // null for the ImageObserver
+			true,   // fill background with white
+			true);  // block until drawing is done
+		vw = Math.round(pageImage.getWidth(null)/2.0);
+		vh = Math.round(pageImage.getHeight(null)/2.0);
+		if (vw==0 && vh==0){ar = 1.0f;}
+		else {ar = (float)vw/(float)vh;}
+		computeSize();
+		orient = 0;		
+		scaleFactor = (float)scale;		
+	}
+
+	public void initCams(int nbCam){
+		pc=new RProjectedCoordsP[nbCam];
+		for (int i=0;i<nbCam;i++){
+			pc[i]=new RProjectedCoordsP();
+		}
+	}
+
+	public void addCamera(int verifIndex){
+		if (pc!=null){
+			if (verifIndex==pc.length){
+				RProjectedCoordsP[] ta=pc;
+				pc=new RProjectedCoordsP[ta.length+1];
+				for (int i=0;i<ta.length;i++){
+					pc[i]=ta[i];
+				}
+				pc[pc.length-1]=new RProjectedCoordsP();
+			}
+			else {System.err.println("ZPDFPage: Error while adding camera "+verifIndex);}
+		}
+		else {
+			if (verifIndex==0){
+				pc=new RProjectedCoordsP[1];
+				pc[0]=new RProjectedCoordsP();
+			}
+			else {System.err.println("ZPDFPage: Error while adding camera "+verifIndex);}
+		}
+	}
+
+	public void removeCamera(int index){
+		pc[index]=null;
+	}
+
+    /** For internal use. */
+    public void computeSize(){
+        size=(float)Math.sqrt(Math.pow(vw,2)+Math.pow(vh,2));
+    }
 
     /** Get glyph's size (radius of bounding circle). */
-    public float getSize(){/*XXX:TBW*/return 0;}
+    public float getSize(){return size;}
 
     /** Set glyph's size by setting its bounding circle's radius.
      *@see #reSize(float factor)
@@ -73,40 +209,212 @@ public class ZPDFPage extends ClosedShape implements RectangularShape {
 	public long getWidth(){return vw;}
 
 	public long getHeight(){return vh;}
-	
-	public boolean fillsView(long w, long h, int camIndex){
-		//XXX:TBW
-		return false;		
-	}
-	
-	public boolean coordInside(int x,int y,int camIndex){
-		//XXX:TBW
-		return false;
-	}
-	
-	public void resetMouseIn(){
-//		for (int i=0;i<pc.length;i++){
-//			resetMouseIn(i);
-//		}
+
+	/** Set to false if the image should not be scaled according to camera's altitude. Its size can still be changed, but its apparent size will always be the same, no matter the camera's altitude.
+		*@see #isZoomSensitive()
+		*/
+	public void setZoomSensitive(boolean b){
+		if (zoomSensitive!=b){
+			zoomSensitive=b;
+			if (vsm != null){
+				vsm.repaintNow();
+			}
+		}
 	}
 
-	public void resetMouseIn(int i){
-//  	if (pc[i]!=null){pc[i].prevMouseIn=false;}
-//  	borderColor = bColor;
+	/** Indicates whether the image is scaled according to camera's altitude.
+		*@see #setZoomSensitive(boolean b)
+		*/
+	public boolean isZoomSensitive(){
+		return zoomSensitive;
+	}
+
+	/** Should a border be drawn around the bitmap image.
+		*@param p one of DRAW_BORDER_*
+		*/
+	public void setDrawBorderPolicy(short p){
+		if (drawBorder!=p){
+			drawBorder=p;
+			if (vsm != null){
+				vsm.repaintNow();
+			}
+		}
+	}
+	
+	public boolean fillsView(long w,long h,int camIndex){
+		//can contain transparent pixel (we have no way of knowing without analysing the image data -could be done when constructing the object or setting the image)
+		return false; 
+	}
+
+	public boolean coordInside(int x,int y,int camIndex){
+		if ((x>=(pc[camIndex].cx-pc[camIndex].cw)) && (x<=(pc[camIndex].cx+pc[camIndex].cw)) && (y>=(pc[camIndex].cy-pc[camIndex].ch)) && (y<=(pc[camIndex].cy+pc[camIndex].ch))){return true;}
+		else {return false;}
 	}
 
 	public short mouseInOut(int x,int y,int camIndex){
-		//XXX:TBW
-		return Glyph.NO_CURSOR_EVENT;
+		if (coordInside(x,y,camIndex)){
+			//if the mouse is inside the glyph
+			if (!pc[camIndex].prevMouseIn){
+				//if it was not inside it last time, mouse has entered the glyph
+				pc[camIndex].prevMouseIn=true;
+				return Glyph.ENTERED_GLYPH;
+			}
+			else {
+				//if it was inside last time, nothing has changed
+				return Glyph.NO_CURSOR_EVENT;
+			}
+		}
+		else{
+			//if the mouse is not inside the glyph
+			if (pc[camIndex].prevMouseIn){
+				//if it was inside it last time, mouse has exited the glyph
+				pc[camIndex].prevMouseIn=false;
+				return Glyph.EXITED_GLYPH;
+			}
+			else {
+				//if it was not inside last time, nothing has changed
+				return Glyph.NO_CURSOR_EVENT;
+			}
+		}
+	}
+
+	public void resetMouseIn(){
+		for (int i=0;i<pc.length;i++){
+			resetMouseIn(i);
+		}
+	}
+
+	public void resetMouseIn(int i){
+		if (pc[i]!=null){pc[i].prevMouseIn=false;}
+		borderColor = bColor;
 	}
 	
-	public void project(Camera c, Dimension d){}
+	public void project(Camera c, Dimension d){
+		int i = c.getIndex();
+		coef = (float)(c.focal/(c.focal+c.altitude));
+		//find coordinates of object's geom center wrt to camera center and project
+		//translate in JPanel coords
+		pc[i].cx = (d.width/2)+Math.round((vx-c.posx)*coef);
+		pc[i].cy = (d.height/2)-Math.round((vy-c.posy)*coef);
+		//project width and height
+		if (zoomSensitive){
+			pc[i].cw = Math.round(vw*coef);
+			pc[i].ch = Math.round(vh*coef);
+		}
+		else{
+			pc[i].cw = (int)vw;
+			pc[i].ch = (int)vh;
+		}
+	}
 
-    public void projectForLens(Camera c, int lensWidth, int lensHeight, float lensMag, long lensx, long lensy){}
+	public void projectForLens(Camera c, int lensWidth, int lensHeight, float lensMag, long lensx, long lensy){
+		int i = c.getIndex();
+		coef = ((float)(c.focal/(c.focal+c.altitude))) * lensMag;
+		//find coordinates of object's geom center wrt to camera center and project
+		//translate in JPanel coords
+		pc[i].lcx = lensWidth/2 + Math.round((vx-lensx)*coef);
+		pc[i].lcy = lensHeight/2 - Math.round((vy-lensy)*coef);
+		//project width and height
+		if (zoomSensitive){
+			pc[i].lcw = Math.round(vw*coef);
+			pc[i].lch = Math.round(vh*coef);
+		}
+		else {
+			pc[i].lcw = (int)vw;
+			pc[i].lch = (int)vh;
+		}
+	}
 
-    public void draw(Graphics2D g,int vW,int vH,int i,Stroke stdS,AffineTransform stdT, int dx, int dy){}
+	public void draw(Graphics2D g,int vW,int vH,int i,Stroke stdS,AffineTransform stdT, int dx, int dy){
+		if ((pc[i].cw>1) && (pc[i].ch>1)){
+			if (zoomSensitive){
+				trueCoef = scaleFactor*coef;
+			}
+			else{
+				trueCoef = scaleFactor;
+			}
+			//a threshold greater than 0.01 causes jolts when zooming-unzooming around the 1.0 scale region
+			if (Math.abs(trueCoef-1.0f)<0.01f){trueCoef=1.0f;}
+			if (trueCoef!=1.0f){
+				// translate
+				at = AffineTransform.getTranslateInstance(dx+pc[i].cx-pc[i].cw,dy+pc[i].cy-pc[i].ch);
+				g.setTransform(at);
+				// rescale and draw
+				g.drawImage(pageImage,AffineTransform.getScaleInstance(trueCoef,trueCoef),null);
+				g.setTransform(stdT);
+				if (drawBorder==1){
+					if (pc[i].prevMouseIn){
+						g.setColor(borderColor);
+						g.drawRect(dx+pc[i].cx-pc[i].cw,dy+pc[i].cy-pc[i].ch,2*pc[i].cw-1,2*pc[i].ch-1);
+					}
+				}
+				else if (drawBorder==2){
+					g.setColor(borderColor);
+					g.drawRect(dx+pc[i].cx-pc[i].cw,dy+pc[i].cy-pc[i].ch,2*pc[i].cw-1,2*pc[i].ch-1);
+				}
+			}
+			else {
+				g.drawImage(pageImage, dx+pc[i].cx-pc[i].cw,dy+pc[i].cy-pc[i].ch,null);
+				if (drawBorder == 1){
+					if (pc[i].prevMouseIn){
+						g.setColor(borderColor);
+						g.drawRect(dx+pc[i].cx-pc[i].cw,dy+pc[i].cy-pc[i].ch,2*pc[i].cw-1,2*pc[i].ch-1);
+					}
+				}
+				else if (drawBorder == 2){
+					g.setColor(borderColor);
+					g.drawRect(dx+pc[i].cx-pc[i].cw,dy+pc[i].cy-pc[i].ch,2*pc[i].cw-1,2*pc[i].ch-1);
+				}
+			}
+		}
+		else {
+			g.setColor(this.borderColor);
+			g.fillRect(dx+pc[i].cx,dy+pc[i].cy,1,1);
+		}
+	}
 
-    public void drawForLens(Graphics2D g,int vW,int vH,int i,Stroke stdS,AffineTransform stdT, int dx, int dy){}
+	public void drawForLens(Graphics2D g,int vW,int vH,int i,Stroke stdS,AffineTransform stdT, int dx, int dy){
+		if ((pc[i].lcw > 1) && (pc[i].lch > 1)){
+			if (zoomSensitive){trueCoef=scaleFactor*coef;}
+			else {trueCoef=scaleFactor;}
+			if (Math.abs(trueCoef-1.0f)<0.01f){
+				//a threshold greater than 0.01 causes jolts when zooming-unzooming around the 1.0 scale region
+				trueCoef=1.0f;
+			}
+			if (trueCoef!=1.0f){
+				g.setTransform(AffineTransform.getTranslateInstance(dx+pc[i].lcx-pc[i].lcw, dy+pc[i].lcy-pc[i].lch));
+				g.drawImage(pageImage, AffineTransform.getScaleInstance(trueCoef,trueCoef), null);
+				g.setTransform(stdT);
+				if (drawBorder==1){
+					if (pc[i].prevMouseIn){
+						g.setColor(borderColor);
+						g.drawRect(dx+pc[i].lcx-pc[i].lcw, dy+pc[i].lcy-pc[i].lch, 2*pc[i].lcw-1, 2*pc[i].lch-1);
+					}
+				}
+				else if (drawBorder==2){
+					g.setColor(borderColor);
+					g.drawRect(dx+pc[i].lcx-pc[i].lcw, dy+pc[i].lcy-pc[i].lch, 2*pc[i].lcw-1, 2*pc[i].lch-1);
+				}
+			}
+			else {
+				g.drawImage(pageImage, dx+pc[i].lcx-pc[i].lcw, dy+pc[i].lcy-pc[i].lch, null);
+				if (drawBorder == 1){
+					if (pc[i].prevMouseIn){
+						g.setColor(borderColor);
+						g.drawRect(dx+pc[i].lcx-pc[i].lcw, dy+pc[i].lcy-pc[i].lch, 2*pc[i].lcw-1, 2*pc[i].lch-1);
+					}
+				}
+				else if (drawBorder == 2){
+					g.setColor(borderColor);
+					g.drawRect(dx+pc[i].lcx-pc[i].lcw, dy+pc[i].lcy-pc[i].lch, 2*pc[i].lcw-1, 2*pc[i].lch-1);
+				}
+			}
+		}
+		else {
+			g.setColor(this.borderColor);
+			g.fillRect(dx+pc[i].lcx,dy+pc[i].lcy,1,1);
+		}
+	}
 
 	public Object clone(){
 		return null;
