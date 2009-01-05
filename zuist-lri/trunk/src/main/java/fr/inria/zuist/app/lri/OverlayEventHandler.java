0e3efd8cbf932ebfb565970e4ccb11abf3fc855a
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2009-01-05 09:30:14 +0000

    removed useless code coming from UISTExplorer
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1449 74e165f3-c697-40a6-91af-552c8936d7fe

--- zuist-lri/trunk/src/main/java/fr/inria/zuist/app/lri/OverlayEventHandler.java
+++ zuist-lri/trunk/src/main/java/fr/inria/zuist/app/lri/OverlayEventHandler.java
@@ -36,35 +36,7 @@ class OverlayEventHandler implements ViewEventHandler {
     }
 
     public void release1(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
-        if (application.ovm.showingLinks){
-            Vector labels = v.getMouse().getIntersectingTexts(application.ovCamera);
-            boolean clickedOnLink = false;
-            if (labels != null){
-                g = (Glyph)labels.elementAt(0);
-                v.parent.setActiveLayer(0);
-                String type = g.getType();
-                application.ovm.hideLinks(false);
-                if (type != null){
-                    if (type.startsWith("O")){
-                        application.rememberLocation();
-                        application.goToObject(type.substring(1), true, LRIExplorer.AUTHOR_CAMERA_ALTITUDE);
-                    }
-                    else {
-                        // starts with "R"
-                        application.rememberLocation();
-                        application.goToRegion(type.substring(1));
-                    }
-                }
-                clickedOnLink = true;
-            }
-            if (!clickedOnLink){
-                // if not clicking on a label
-                // dismiss the overlay, not matter whether user clicked on faded rect or empty region
-                application.ovm.hideLinks(true);
-                v.parent.setActiveLayer(0);            
-            }
-        }
-        else if (application.ovm.showingAbout){
+        if (application.ovm.showingAbout){
             application.ovm.hideAbout();
             v.parent.setActiveLayer(0);
         }
@@ -76,36 +48,25 @@ class OverlayEventHandler implements ViewEventHandler {
 
     public void release2(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){}
 
-    public void click2(ViewPanel v,int mod,int jpx,int jpy,int clickNumber, MouseEvent e){
+    public void click2(ViewPanel v,int mod,int jpx,int jpy,int clickNumber, MouseEvent e){}
 
-    }
+    public void press3(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){}
 
-    public void press3(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){System.err.println("a");
-    }
-
-    public void release3(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
-    }
+    public void release3(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){}
 
     public void click3(ViewPanel v,int mod,int jpx,int jpy,int clickNumber, MouseEvent e){}
 
-    public void mouseMoved(ViewPanel v,int jpx,int jpy, MouseEvent e){
-    }
+    public void mouseMoved(ViewPanel v,int jpx,int jpy, MouseEvent e){}
 
-    public void mouseDragged(ViewPanel v,int mod,int buttonNumber,int jpx,int jpy, MouseEvent e){
+    public void mouseDragged(ViewPanel v,int mod,int buttonNumber,int jpx,int jpy, MouseEvent e){}
 
-    }
-
-    public void mouseWheelMoved(ViewPanel v,short wheelDirection,int jpx,int jpy, MouseWheelEvent e){
-
-    }
+    public void mouseWheelMoved(ViewPanel v,short wheelDirection,int jpx,int jpy, MouseWheelEvent e){}
 
     public void enterGlyph(Glyph g){}
 
     public void exitGlyph(Glyph g){}
 
-    public void Kpress(ViewPanel v,char c,int code,int mod, KeyEvent e){
-
-    }
+    public void Kpress(ViewPanel v,char c,int code,int mod, KeyEvent e){}
 
     public void Ktype(ViewPanel v,char c,int code,int mod, KeyEvent e){}
 
