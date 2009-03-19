48d935b7e9825a3f9f273a0185552ae2e5d37622
Romain Primet <>
2009-03-19 14:53:46 +0000

    javadoc
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1529 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm/branches/TRY-RP-NewAnimManager/src/main/java/net/claribole/zvtm/animation/DefaultTimingHandler.java
+++ zvtm/branches/TRY-RP-NewAnimManager/src/main/java/net/claribole/zvtm/animation/DefaultTimingHandler.java
@@ -8,11 +8,14 @@ package net.claribole.zvtm.animation;
 
 /**
  * A class that overrides most TimingHandler methods with dummy (empty) ones.
- * The only method that is *not* overriden (ie left abstract) is timingEvent().
+ * The only method that is <b>not</b> overriden (ie left abstract) is {@link #timingEvent}.
  * This is because a handler that does not respond to timing events is 
  * likely useless.
  * Users should override any TimingHandler methods that they
  * need (and are compelled to provide an implementation for timingEvent)
+ * @see TimingHandler
+ * @see Animation
+ * @author Romain Primet
  */
 public abstract class DefaultTimingHandler implements TimingHandler{
     public void begin(Object subject, Animation.Dimension dim){}
