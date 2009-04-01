a14d3b149c4f73be83e99045fe48167c8a7477ed
Romain Primet <>
2009-04-01 16:26:50 +0000

    back to a functional branch
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1589 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm/branches/TRY-RP-NewAnimManager/src/main/java/net/claribole/zvtm/animation/examples/TestCircle.java
+++ zvtm/branches/TRY-RP-NewAnimManager/src/main/java/net/claribole/zvtm/animation/examples/TestCircle.java
@@ -200,13 +200,13 @@ public class TestCircle {
 	}
 
 	public void release3(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
-	    application.vsm.animator.Xspeed=0;
-	    application.vsm.animator.Yspeed=0;
-	    application.vsm.animator.Aspeed=0;
+	    application.vsm.getAnimationManager().setXspeed(0);
+	    application.vsm.getAnimationManager().setYspeed(0);
+	    application.vsm.getAnimationManager().setZspeed(0);
 	    v.setDrawDrag(false);
 	    application.vsm.activeView.mouse.setSensitivity(true);
 	    /*Camera c=v.cams[0];
-	      application.vsm.animator.createCameraAnimation(500,2,new LongPoint(lastX-application.vsm.mouse.vx,lastY-application.vsm.mouse.vy),c.getID());*/
+	      application.vsm.getAnimationManager().createCameraAnimation(500,2,new LongPoint(lastX-application.vsm.mouse.vx,lastY-application.vsm.mouse.vy),c.getID());*/
 	}
 
 	public void click3(ViewPanel v,int mod,int jpx,int jpy,int clickNumber, MouseEvent e){}
@@ -220,15 +220,15 @@ public class TestCircle {
 		Camera c=application.vsm.getActiveCamera();
 		float a=(c.focal+Math.abs(c.altitude))/c.focal;
 		if (mod == META_SHIFT_MOD) {
-		    application.vsm.animator.Xspeed=0;
-		    application.vsm.animator.Yspeed=0;
-		    application.vsm.animator.Aspeed=(c.altitude>0) ? (long)((lastJPY-jpy)*(a/50.0f)) : (long)((lastJPY-jpy)/(a*50));
+		    application.vsm.getAnimationManager().setXspeed(0);
+		    application.vsm.getAnimationManager().setYspeed(0);
+		    application.vsm.getAnimationManager().setZspeed((c.altitude>0) ? (long)((lastJPY-jpy)*(a/50.0f)) : (long)((lastJPY-jpy)/(a*50)));
 		    //50 is just a speed factor (too fast otherwise)
 		}
 		else {
-		    application.vsm.animator.Xspeed=(c.altitude>0) ? (long)((jpx-lastJPX)*(a/50.0f)) : (long)((jpx-lastJPX)/(a*50));
-		    application.vsm.animator.Yspeed=(c.altitude>0) ? (long)((lastJPY-jpy)*(a/50.0f)) : (long)((lastJPY-jpy)/(a*50));
-		    application.vsm.animator.Aspeed=0;
+		    application.vsm.getAnimationManager().setXspeed((c.altitude>0) ? (long)((jpx-lastJPX)*(a/50.0f)) : (long)((jpx-lastJPX)/(a*50)));
+		    application.vsm.getAnimationManager().setYspeed((c.altitude>0) ? (long)((lastJPY-jpy)*(a/50.0f)) : (long)((lastJPY-jpy)/(a*50)));
+		    application.vsm.getAnimationManager().setZspeed(0);
 		}
 	    }
 	}
