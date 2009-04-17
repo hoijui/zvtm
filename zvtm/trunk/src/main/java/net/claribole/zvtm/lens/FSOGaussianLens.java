4d54ae22f80ebba578f99856915473aba0c6bbf7
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2009-04-17 06:59:38 +0000

    focus offset feature now provided as an interface ; should make the class hierarchy cleaner
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1704 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm/trunk/src/main/java/net/claribole/zvtm/lens/FSOGaussianLens.java
+++ zvtm/trunk/src/main/java/net/claribole/zvtm/lens/FSOGaussianLens.java
@@ -7,18 +7,12 @@
 
 package net.claribole.zvtm.lens;
 
+import java.awt.image.WritableRaster;
+
 /**Profile: gaussian - Distance metric: L(2) (circular shape)<br>Size expressed as an absolute value in pixels. Parameterable focus offset.*/
 
-public class FSOGaussianLens extends FSOLens {
-
-    //gain function parameters
-    protected double a = 0;
-    protected double b = 0;
-    protected double c = 0;
-    protected double e = 0;
-
-    double d = 0;
-
+public class FSOGaussianLens extends FSGaussianLens implements ParameterableFocusOffset {
+    
     /**
      * create a lens with a maximum magnification factor of 2.0
      */
@@ -56,110 +50,128 @@ public class FSOGaussianLens extends FSOLens {
      *@param y vertical coordinate of the lens' center (as an offset w.r.t the view's center coordinates)
      */
     public FSOGaussianLens(float mm, int outerRadius, int innerRadius, int x, int y){
-        this.MM = mm;
-        this.LR1 = outerRadius;
-        this.LR2 = innerRadius;
-        updateMagBufferWorkingDimensions();
-        a = Math.PI/(LR1-LR2);
-        b = - Math.PI*LR2/(LR1-LR2);
-        c = (MM-1)/2;
-        e = (1+MM)/2;
-        lx = x;
-        ly = y;
+        super(mm, outerRadius, innerRadius, x, y);
     }
 
-    /**
-     * set the lens' outer radius (beyond which no magnification is applied - outward)
-     *@param r radius in pixels
-     */
-    public void setOuterRadius(int r){
-	super.setOuterRadius(r);
-	a = Math.PI/(LR1-LR2);
-	b = - Math.PI*LR2/(LR1-LR2);
+    int dx = 0;
+    int dy = 0;
+
+    public void setXfocusOffset(int x){
+        dx = x;
     }
 
-    /**
-     * set the lens' inner radius (beyond which maximum magnification is applied - inward)
-     *
-     *@param r radius in pixels
-     */
-    public void setInnerRadius(int r){
-	super.setInnerRadius(r);
-	a = Math.PI/(LR1-LR2);
-	b = - Math.PI*LR2/(LR1-LR2);
+    public void setYfocusOffset(int y){
+        dy = y;
+    }
+    
+    public int getXfocusOffset(){
+        return dx;
+    }
+    
+    public int getYfocusOffset(){
+        return dy;
     }
 
-
-    /**
-     * set the lens' radii
-     *
-     *@param outerRadius outer radius (beyond which no magnification is applied - outward)
-     *@param innerRadius inner radius (beyond which maximum magnification is applied - inward)
-     */
-    public void setRadii(int outerRadius, int innerRadius){
-	this.setRadii(outerRadius, innerRadius, true);
+    synchronized void transformI(WritableRaster iwr, WritableRaster ewr){
+        synchronized(this){
+            // get source pixels in an array
+            iwr.getDataElements(lurd[0], lurd[1], lensWidth, lensHeight, oPixelsI);
+            // get magnified source pixels in a second array
+            ewr.getDataElements(0, 0, mbw, mbh, mPixelsI);
+            // transfer them to the target array taking the gain function into account
+            for (int x=lurd[0];x<lurd[2];x++){
+                for (int y=lurd[1];y<lurd[3];y++){
+                    /* gain is computed w.r.t main buffer pixels
+                        (we do not want to compute the gain for pixels that won't be in the output) */
+                        this.gf(x,y,gain);
+                    if (gain[0] > mSwitchThreshold || gain[1] > mSwitchThreshold){
+                        /* following 3 commented lines left here for documentation of what the actual
+                            single instruction means, x0 and y0 being mere int variables */
+                        //x0 = Math.round(((x-lurd[0]) * MM - mbw/2.0f) / gain[0] + mbw/2.0f);
+                        //y0 = Math.round(((y-lurd[1]) * MM - mbh/2.0f) / gain[1] + mbh/2.0f);
+                        //tPixelsI[(y-lurd[1])*(lensWidth)+(x-lurd[0])] = mPixelsI[Math.round(y0*mbw+x0)];
+                        tPixelsI[(y-lurd[1])*(lensWidth)+(x-lurd[0])] =
+                            mPixelsI[Math.round(((y-lurd[1]) * MM - mbh/2.0f) / gain[1] + mbh/2.0f+dy)*mbw + Math.round(((x-lurd[0]) * MM - mbw/2.0f) / gain[0] + mbw/2.0f+dx)];
+                    }
+                    else {
+                        //x0 = Math.round((((float)x-sw-lx)/gain[0])+sw+lx);
+                        //y0 = Math.round((((float)y-sh-ly)/gain[1])+sh+ly);
+                        //tPixelsI[(y-lurd[1])*(lensWidth)+(x-lurd[0])] = oPixelsI[(y0-lurd[1])*(lensWidth)+(x0-lurd[0])];
+                        tPixelsI[(y-lurd[1])*(lensWidth)+(x-lurd[0])] =
+                            oPixelsI[(Math.round((((float)y-sh-ly)/gain[1])+sh+ly)-lurd[1])*(lensWidth)+(Math.round((((float)x-sw-lx)/gain[0])+sw+lx)-lurd[0])];
+                    }
+                }
+            }
+            // transfer pixels in the target array back to the raster
+            iwr.setDataElements(lurd[0], lurd[1], lensWidth, lensHeight, tPixelsI);
+        }
     }
 
-    /**
-     * set the lens' radii
-     *
-     *@param outerRadius outer radius (beyond which no magnification is applied - outward)
-     *@param innerRadius inner radius (beyond which maximum magnification is applied - inward)
-     */
-    public void setRadii(int outerRadius, int innerRadius, boolean forceRaster){
-	super.setRadii(outerRadius, innerRadius, forceRaster);
-	a = Math.PI/(LR1-LR2);
-	b = - Math.PI*LR2/(LR1-LR2);
+    synchronized void transformS(WritableRaster iwr, WritableRaster ewr){
+        // get source pixels in an array
+        iwr.getDataElements(lurd[0], lurd[1], lensWidth, lensHeight, oPixelsS);
+        // get magnified source pixels in a second array
+        ewr.getDataElements(0, 0, mbw, mbh, mPixelsS);
+        // transfer them to the target array taking the gain function into account
+        for (int x=lurd[0];x<lurd[2];x++){
+            for (int y=lurd[1];y<lurd[3];y++){
+                /* gain is computed w.r.t main buffer pixels
+                    (we do not want to compute the gain for pixels that won't be in the output) */
+                    this.gf(x,y,gain);
+                if (gain[0] > mSwitchThreshold || gain[1] > mSwitchThreshold){
+                    /* following 3 commented lines left here for documentation of what the actual
+                        single instruction means, x0 and y0 being mere int variables */
+                    //x0 = Math.round(((x-lurd[0]) * MM - mbw/2.0f) / gain[0] + mbw/2.0f);
+                    //y0 = Math.round(((y-lurd[1]) * MM - mbh/2.0f) / gain[1] + mbh/2.0f);
+                    //tPixelsS[(y-lurd[1])*(lensWidth)+(x-lurd[0])] = mPixelsS[Math.round(y0*mbw+x0];
+                    tPixelsS[(y-lurd[1])*(lensWidth)+(x-lurd[0])] =
+                        mPixelsS[Math.round(((y-lurd[1]) * MM - mbh/2.0f) / gain[1] + mbh/2.0f+dy)*mbw+Math.round(((x-lurd[0]) * MM - mbw/2.0f) / gain[0] + mbw/2.0f+dx)];
+                }
+                else {
+                    //x0 = Math.round((((float)x-sw-lx)/gain[0])+sw+lx);
+                    //y0 = Math.round((((float)y-sh-ly)/gain[1])+sh+ly);
+                    //tPixelsS[(y-lurd[1])*(lensWidth)+(x-lurd[0])] = oPixelsS[(y0-lurd[1])*(lensWidth)+(x0-lurd[0])];
+                    tPixelsS[(y-lurd[1])*(lensWidth)+(x-lurd[0])] =
+                        oPixelsS[(Math.round((((float)y-sh-ly)/gain[1])+sh+ly)-lurd[1])*(lensWidth)+(Math.round((((float)x-sw-lx)/gain[0])+sw+lx)-lurd[0])];
+                }
+            }
+        }
+        // transfer pixels in the target array back to the raster
+        iwr.setDataElements(lurd[0], lurd[1], lensWidth, lensHeight, tPixelsS);
     }
 
-    /**
-     * set the lens' radii and maximum magnification
-     *
-     *@param mm maximum magnification factor, mm in [0,+inf[
-     *@param outerRadius outer radius (beyond which no magnification is applied - outward)
-     *@param innerRadius inner radius (beyond which maximum magnification is applied - inward)
-     */
-    public void setMMandRadii(float mm, int outerRadius, int innerRadius){
-	this.setMMandRadii(mm, outerRadius, innerRadius, true);
+    synchronized void transformB(WritableRaster iwr, WritableRaster ewr){
+        // get source pixels in an array
+        iwr.getDataElements(lurd[0], lurd[1], lensWidth, lensHeight, oPixelsB);
+        // get magnified source pixels in a second array
+        ewr.getDataElements(0, 0, mbw, mbh, mPixelsB);
+        // transfer them to the target array taking the gain function into account
+        for (int x=lurd[0];x<lurd[2];x++){
+            for (int y=lurd[1];y<lurd[3];y++){
+                /* gain is computed w.r.t main buffer pixels
+                    (we do not want to compute the gain for pixels that won't be in the output) */
+                    this.gf(x,y,gain);
+                if (gain[0] > mSwitchThreshold || gain[1] > mSwitchThreshold){
+                    /* following 3 commented lines left here for documentation of what the actual
+                        single instruction means, x0 and y0 being mere int variables */
+                    //x0 = Math.round(((x-lurd[0]) * MM - mbw/2.0f) / gain[0] + mbw/2.0f);
+                    //y0 = Math.round(((y-lurd[1]) * MM - mbh/2.0f) / gain[1] + mbh/2.0f);
+                    //tPixelsB[(y-lurd[1])*(lensWidth)+(x-lurd[0])] = mPixelsB[Math.round(y0*mbw+x0];
+                    tPixelsB[(y-lurd[1])*(lensWidth)+(x-lurd[0])] =
+                        mPixelsB[Math.round(((y-lurd[1]) * MM - mbh/2.0f) / gain[1] + mbh/2.0f+dy)*mbw+Math.round(((x-lurd[0]) * MM - mbw/2.0f) / gain[0] + mbw/2.0f+dx)];
+                }
+                else {
+                    //x0 = Math.round((((float)x-sw-lx)/gain[0])+sw+lx);
+                    //y0 = Math.round((((float)y-sh-ly)/gain[1])+sh+ly);
+                    //tPixelsB[(y-lurd[1])*(lensWidth)+(x-lurd[0])] = oPixelsB[(y0-lurd[1])*(lensWidth)+(x0-lurd[0])];
+                    tPixelsB[(y-lurd[1])*(lensWidth)+(x-lurd[0])] =
+                        oPixelsB[(Math.round((((float)y-sh-ly)/gain[1])+sh+ly)-lurd[1])*(lensWidth)+(Math.round((((float)x-sw-lx)/gain[0])+sw+lx)-lurd[0])];
+                }
+            }
+        }
+        // transfer pixels in the target array back to the raster
+        iwr.setDataElements(lurd[0], lurd[1], lensWidth, lensHeight, tPixelsB);
     }
-
-    /**
-     * set the lens' radii and maximum magnification
-     *
-     *@param mm maximum magnification factor, mm in [0,+inf[
-     *@param outerRadius outer radius (beyond which no magnification is applied - outward)
-     *@param innerRadius inner radius (beyond which maximum magnification is applied - inward)
-     */
-    public void setMMandRadii(float mm, int outerRadius, int innerRadius, boolean forceRaster){
-	super.setMMandRadii(mm, outerRadius, innerRadius, forceRaster);
-	a = Math.PI/(LR1-LR2);
-	b = - Math.PI*LR2/(LR1-LR2);
-	c = (MM-1)/2;
-	e = (1+MM)/2;
-    }
-
-    public void setMaximumMagnification(float mm){
-	this.setMaximumMagnification(mm, true);
-    }
-
-    public void setMaximumMagnification(float mm, boolean forceRaster){
-	super.setMaximumMagnification(mm, forceRaster);
-	c = (MM-1)/2;
-	e = (1+MM)/2;
-    }
-
-    public void gf(float x, float y, float[] g){
-	d = Math.sqrt(Math.pow(x-sw-lx,2) + Math.pow(y-sh-ly,2));
-	if (d <= LR2){
-	    g[0] = g[1] = MM;
-	}
-	else if (d <= LR1){
-	    g[0] = g[1] = (float)(c * Math.cos(a*d+b) + e);
-	}
-	else {
-	    g[0] = g[1] = 1;
-	}
-    }
-
+    
 }
 
--- zvtm/trunk/src/main/java/net/claribole/zvtm/lens/FSOLinearLens.java
+++ zvtm/trunk/src/main/java/net/claribole/zvtm/lens/FSOLinearLens.java
@@ -7,15 +7,11 @@
 
 package net.claribole.zvtm.lens;
 
+import java.awt.image.WritableRaster;
+
 /**Profile: linear - Distance metric: L(2) (circular shape)<br>Size expressed as an absolute value in pixels. Parameterable focus offset.*/
 
-public class FSOLinearLens extends FSOLens {
-
-    //gain function parameters
-    protected float a = 0;
-    protected float b = 0;
-
-    double d = 0;
+public class FSOLinearLens extends FSLinearLens implements ParameterableFocusOffset {
 
     /**
      * create a lens with a maximum magnification factor of 2.0
@@ -54,103 +50,127 @@ public class FSOLinearLens extends FSOLens {
      *@param y vertical coordinate of the lens' center (as an offset w.r.t the view's center coordinates)
      */
     public FSOLinearLens(float mm, int outerRadius, int innerRadius, int x, int y){
-	this.MM = mm;
-	this.LR1 = outerRadius;
-	this.LR2 = innerRadius;
-	updateMagBufferWorkingDimensions();
-	a = (1-MM)/(float)(LR1 - LR2);
-	b = (MM*LR1-LR2)/(float)(LR1 - LR2);
-	lx = x;
-	ly = y;
+	    super(mm, outerRadius, innerRadius, x, y);
     }
 
-    /**
-     * set the lens' outer radius (beyond which no magnification is applied - outward)
-     *
-     *@param r radius in pixels
-     */
-    public void setOuterRadius(int r){
-	super.setOuterRadius(r);
-	a = (1-MM)/(float)(LR1 - LR2);
-	b = (MM*LR1-LR2)/(float)(LR1 - LR2);
+    int dx = 0;
+    int dy = 0;
+
+    public void setXfocusOffset(int x){
+        dx = x;
     }
 
-    /**
-     * set the lens' inner radius (beyond which maximum magnification is applied - inward)
-     *
-     *@param r radius in pixels
-     */
-    public void setInnerRadius(int r){
-	super.setInnerRadius(r);
-	a = (1-MM)/(float)(LR1 - LR2);
-	b = (MM*LR1-LR2)/(float)(LR1 - LR2);
+    public void setYfocusOffset(int y){
+        dy = y;
+    }
+    
+    public int getXfocusOffset(){
+        return dx;
+    }
+    
+    public int getYfocusOffset(){
+        return dy;
     }
 
-    /**
-     * set the lens' radii
-     *
-     *@param outerRadius outer radius (beyond which no magnification is applied - outward)
-     *@param innerRadius inner radius (beyond which maximum magnification is applied - inward)
-     */
-    public void setRadii(int outerRadius, int innerRadius){
-	this.setRadii(outerRadius, innerRadius, true);
+    synchronized void transformI(WritableRaster iwr, WritableRaster ewr){
+        synchronized(this){
+            // get source pixels in an array
+            iwr.getDataElements(lurd[0], lurd[1], lensWidth, lensHeight, oPixelsI);
+            // get magnified source pixels in a second array
+            ewr.getDataElements(0, 0, mbw, mbh, mPixelsI);
+            // transfer them to the target array taking the gain function into account
+            for (int x=lurd[0];x<lurd[2];x++){
+                for (int y=lurd[1];y<lurd[3];y++){
+                    /* gain is computed w.r.t main buffer pixels
+                        (we do not want to compute the gain for pixels that won't be in the output) */
+                        this.gf(x,y,gain);
+                    if (gain[0] > mSwitchThreshold || gain[1] > mSwitchThreshold){
+                        /* following 3 commented lines left here for documentation of what the actual
+                            single instruction means, x0 and y0 being mere int variables */
+                        //x0 = Math.round(((x-lurd[0]) * MM - mbw/2.0f) / gain[0] + mbw/2.0f);
+                        //y0 = Math.round(((y-lurd[1]) * MM - mbh/2.0f) / gain[1] + mbh/2.0f);
+                        //tPixelsI[(y-lurd[1])*(lensWidth)+(x-lurd[0])] = mPixelsI[Math.round(y0*mbw+x0)];
+                        tPixelsI[(y-lurd[1])*(lensWidth)+(x-lurd[0])] =
+                            mPixelsI[Math.round(((y-lurd[1]) * MM - mbh/2.0f) / gain[1] + mbh/2.0f+dy)*mbw + Math.round(((x-lurd[0]) * MM - mbw/2.0f) / gain[0] + mbw/2.0f+dx)];
+                    }
+                    else {
+                        //x0 = Math.round((((float)x-sw-lx)/gain[0])+sw+lx);
+                        //y0 = Math.round((((float)y-sh-ly)/gain[1])+sh+ly);
+                        //tPixelsI[(y-lurd[1])*(lensWidth)+(x-lurd[0])] = oPixelsI[(y0-lurd[1])*(lensWidth)+(x0-lurd[0])];
+                        tPixelsI[(y-lurd[1])*(lensWidth)+(x-lurd[0])] =
+                            oPixelsI[(Math.round((((float)y-sh-ly)/gain[1])+sh+ly)-lurd[1])*(lensWidth)+(Math.round((((float)x-sw-lx)/gain[0])+sw+lx)-lurd[0])];
+                    }
+                }
+            }
+            // transfer pixels in the target array back to the raster
+            iwr.setDataElements(lurd[0], lurd[1], lensWidth, lensHeight, tPixelsI);
+        }
     }
 
-    /**
-     * set the lens' radii
-     *
-     *@param outerRadius outer radius (beyond which no magnification is applied - outward)
-     *@param innerRadius inner radius (beyond which maximum magnification is applied - inward)
-     */
-    public void setRadii(int outerRadius, int innerRadius, boolean forceRaster){
-	super.setRadii(outerRadius, innerRadius, forceRaster);
-	a = (1-MM)/(float)(LR1 - LR2);
-	b = (MM*LR1-LR2)/(float)(LR1 - LR2);
+    synchronized void transformS(WritableRaster iwr, WritableRaster ewr){
+        // get source pixels in an array
+        iwr.getDataElements(lurd[0], lurd[1], lensWidth, lensHeight, oPixelsS);
+        // get magnified source pixels in a second array
+        ewr.getDataElements(0, 0, mbw, mbh, mPixelsS);
+        // transfer them to the target array taking the gain function into account
+        for (int x=lurd[0];x<lurd[2];x++){
+            for (int y=lurd[1];y<lurd[3];y++){
+                /* gain is computed w.r.t main buffer pixels
+                    (we do not want to compute the gain for pixels that won't be in the output) */
+                    this.gf(x,y,gain);
+                if (gain[0] > mSwitchThreshold || gain[1] > mSwitchThreshold){
+                    /* following 3 commented lines left here for documentation of what the actual
+                        single instruction means, x0 and y0 being mere int variables */
+                    //x0 = Math.round(((x-lurd[0]) * MM - mbw/2.0f) / gain[0] + mbw/2.0f);
+                    //y0 = Math.round(((y-lurd[1]) * MM - mbh/2.0f) / gain[1] + mbh/2.0f);
+                    //tPixelsS[(y-lurd[1])*(lensWidth)+(x-lurd[0])] = mPixelsS[Math.round(y0*mbw+x0];
+                    tPixelsS[(y-lurd[1])*(lensWidth)+(x-lurd[0])] =
+                        mPixelsS[Math.round(((y-lurd[1]) * MM - mbh/2.0f) / gain[1] + mbh/2.0f+dy)*mbw+Math.round(((x-lurd[0]) * MM - mbw/2.0f) / gain[0] + mbw/2.0f+dx)];
+                }
+                else {
+                    //x0 = Math.round((((float)x-sw-lx)/gain[0])+sw+lx);
+                    //y0 = Math.round((((float)y-sh-ly)/gain[1])+sh+ly);
+                    //tPixelsS[(y-lurd[1])*(lensWidth)+(x-lurd[0])] = oPixelsS[(y0-lurd[1])*(lensWidth)+(x0-lurd[0])];
+                    tPixelsS[(y-lurd[1])*(lensWidth)+(x-lurd[0])] =
+                        oPixelsS[(Math.round((((float)y-sh-ly)/gain[1])+sh+ly)-lurd[1])*(lensWidth)+(Math.round((((float)x-sw-lx)/gain[0])+sw+lx)-lurd[0])];
+                }
+            }
+        }
+        // transfer pixels in the target array back to the raster
+        iwr.setDataElements(lurd[0], lurd[1], lensWidth, lensHeight, tPixelsS);
     }
 
-
-    /**
-     * set the lens' radii and maximum magnification
-     *
-     *@param mm maximum magnification factor, mm in [0,+inf[
-     *@param outerRadius outer radius (beyond which no magnification is applied - outward)
-     *@param innerRadius inner radius (beyond which maximum magnification is applied - inward)
-     */
-    public void setMMandRadii(float mm, int outerRadius, int innerRadius){
-	this.setMMandRadii(mm, outerRadius, innerRadius, true);
+    synchronized void transformB(WritableRaster iwr, WritableRaster ewr){
+        // get source pixels in an array
+        iwr.getDataElements(lurd[0], lurd[1], lensWidth, lensHeight, oPixelsB);
+        // get magnified source pixels in a second array
+        ewr.getDataElements(0, 0, mbw, mbh, mPixelsB);
+        // transfer them to the target array taking the gain function into account
+        for (int x=lurd[0];x<lurd[2];x++){
+            for (int y=lurd[1];y<lurd[3];y++){
+                /* gain is computed w.r.t main buffer pixels
+                    (we do not want to compute the gain for pixels that won't be in the output) */
+                    this.gf(x,y,gain);
+                if (gain[0] > mSwitchThreshold || gain[1] > mSwitchThreshold){
+                    /* following 3 commented lines left here for documentation of what the actual
+                        single instruction means, x0 and y0 being mere int variables */
+                    //x0 = Math.round(((x-lurd[0]) * MM - mbw/2.0f) / gain[0] + mbw/2.0f);
+                    //y0 = Math.round(((y-lurd[1]) * MM - mbh/2.0f) / gain[1] + mbh/2.0f);
+                    //tPixelsB[(y-lurd[1])*(lensWidth)+(x-lurd[0])] = mPixelsB[Math.round(y0*mbw+x0];
+                    tPixelsB[(y-lurd[1])*(lensWidth)+(x-lurd[0])] =
+                        mPixelsB[Math.round(((y-lurd[1]) * MM - mbh/2.0f) / gain[1] + mbh/2.0f+dy)*mbw+Math.round(((x-lurd[0]) * MM - mbw/2.0f) / gain[0] + mbw/2.0f+dx)];
+                }
+                else {
+                    //x0 = Math.round((((float)x-sw-lx)/gain[0])+sw+lx);
+                    //y0 = Math.round((((float)y-sh-ly)/gain[1])+sh+ly);
+                    //tPixelsB[(y-lurd[1])*(lensWidth)+(x-lurd[0])] = oPixelsB[(y0-lurd[1])*(lensWidth)+(x0-lurd[0])];
+                    tPixelsB[(y-lurd[1])*(lensWidth)+(x-lurd[0])] =
+                        oPixelsB[(Math.round((((float)y-sh-ly)/gain[1])+sh+ly)-lurd[1])*(lensWidth)+(Math.round((((float)x-sw-lx)/gain[0])+sw+lx)-lurd[0])];
+                }
+            }
+        }
+        // transfer pixels in the target array back to the raster
+        iwr.setDataElements(lurd[0], lurd[1], lensWidth, lensHeight, tPixelsB);
     }
-
-    /**
-     * set the lens' radii and maximum magnification
-     *
-     *@param mm maximum magnification factor, mm in [0,+inf[
-     *@param outerRadius outer radius (beyond which no magnification is applied - outward)
-     *@param innerRadius inner radius (beyond which maximum magnification is applied - inward)
-     */
-    public void setMMandRadii(float mm, int outerRadius, int innerRadius, boolean forceRaster){
-	super.setMMandRadii(mm, outerRadius, innerRadius, forceRaster);
-	a = (1-MM)/(float)(LR1 - LR2);
-	b = (MM*LR1-LR2)/(float)(LR1 - LR2);
-    }
-
-    public void setMaximumMagnification(float mm){
-	this.setMaximumMagnification(mm, true);
-    }
-
-    public void setMaximumMagnification(float mm, boolean forceRaster){
-	super.setMaximumMagnification(mm, forceRaster);
-	a = (1-MM)/(float)(LR1 - LR2);
-	b = (MM*LR1-LR2)/(float)(LR1 - LR2);
-    }
-
-    public void gf(float x, float y, float[] g){
-	d = Math.sqrt(Math.pow(x-sw-lx,2) + Math.pow(y-sh-ly,2));
-	if (d <= LR2)
-	    g[0] = g[1] = MM;
-	else if (d <= LR1)
-	    g[0] = g[1] = a * (float)d + b;
-	else
-	    g[0] = g[1] = 1;
-    }
-
+    
 }
