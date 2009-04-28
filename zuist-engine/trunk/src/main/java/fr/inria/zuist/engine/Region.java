b5991b405d0bec75d954913056f29442d18c16f3
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2009-04-28 10:22:23 +0000

    fixed javadoc
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1726 74e165f3-c697-40a6-91af-552c8936d7fe

--- zuist-engine/trunk/src/main/java/fr/inria/zuist/engine/Region.java
+++ zuist-engine/trunk/src/main/java/fr/inria/zuist/engine/Region.java
@@ -1,5 +1,5 @@
 /*   AUTHOR :           Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
- *   Copyright (c) INRIA, 2007-2008. All Rights Reserved
+ *   Copyright (c) INRIA, 2007-2009. All Rights Reserved
  *   Licensed under the GNU LGPL. For full terms see the file COPYING.
  *
  * $Id: Region.java,v 1.12 2007/10/02 09:34:10 pietriga Exp $
@@ -14,7 +14,7 @@ import java.util.Vector;
 import com.xerox.VTM.glyphs.Glyph;
 import com.xerox.VTM.glyphs.VRectangle;
 
-/** Information about a region in the scene. Regions belong to a single level and contain objects.
+/** Information about a region in the scene. Regions contain objects. They span one or more contiguous level(s).
  *@author Emmanuel Pietriga
  */
 
