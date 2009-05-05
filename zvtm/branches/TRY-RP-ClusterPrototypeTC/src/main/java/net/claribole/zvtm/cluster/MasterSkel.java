3d55d33b140e10edc4367cc153a97f3b7b8a1f31
Romain Primet <>
2009-05-05 07:46:45 +0000

    still drafting MasterSkel
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1743 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm/branches/TRY-RP-ClusterPrototypeTC/src/main/java/net/claribole/zvtm/cluster/MasterSkel.java
+++ zvtm/branches/TRY-RP-ClusterPrototypeTC/src/main/java/net/claribole/zvtm/cluster/MasterSkel.java
@@ -1,8 +1,13 @@
 package net.claribole.zvtm.cluster;
 
 import net.claribole.zvtm.cluster.MetaCamera;
+
+import com.xerox.VTM.glyphs.Glyph;
+import com.xerox.VTM.glyphs.VRectangle;
 import com.xerox.VTM.engine.VirtualSpace;
 import com.xerox.VTM.engine.VirtualSpaceManager;
+
+import java.awt.Color;
 
 //master application skeleton
 public class MasterSkel {
@@ -17,10 +22,18 @@ public class MasterSkel {
 		//to unfreeze the slaves
 		vsm.signalMasterReady();
 		//add an external view for interaction (scrolling) purposes
+		//TODO which camera to use, if any? metacam[0]?
+
 		//add a few glyphs to the virtual space
+		Glyph rect = new VRectangle(0,0,0,60,40,Color.GREEN);
+		Glyph anotherRect = new VRectangle(30,50,0,60,70,Color.RED);
+
+		vsm.addGlyph(rect, vs);
+		vsm.addGlyph(anotherRect, vs);
 	}
 
 	public static void main(String[] args){
 		new MasterSkel();
 	}
 }
+
