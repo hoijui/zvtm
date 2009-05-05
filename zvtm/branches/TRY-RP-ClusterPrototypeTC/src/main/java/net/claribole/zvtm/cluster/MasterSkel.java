80ad4405b9197e69c6e650e5026a9aa5ba1db92f
Romain Primet <>
2009-05-05 08:02:08 +0000

    add view to master application
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1744 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm/branches/TRY-RP-ClusterPrototypeTC/src/main/java/net/claribole/zvtm/cluster/MasterSkel.java
+++ zvtm/branches/TRY-RP-ClusterPrototypeTC/src/main/java/net/claribole/zvtm/cluster/MasterSkel.java
@@ -4,10 +4,13 @@ import net.claribole.zvtm.cluster.MetaCamera;
 
 import com.xerox.VTM.glyphs.Glyph;
 import com.xerox.VTM.glyphs.VRectangle;
+import com.xerox.VTM.engine.Camera;
+import com.xerox.VTM.engine.View;
 import com.xerox.VTM.engine.VirtualSpace;
 import com.xerox.VTM.engine.VirtualSpaceManager;
 
 import java.awt.Color;
+import java.util.Vector;
 
 //master application skeleton
 public class MasterSkel {
@@ -30,6 +33,12 @@ public class MasterSkel {
 
 		vsm.addGlyph(rect, vs);
 		vsm.addGlyph(anotherRect, vs);
+
+		Vector<Camera> vcam = new Vector<Camera>();
+		vcam.add(vs.getMetaCamera().retrieveCamera(0));
+		View view = vsm.addExternalView(vcam, "masterView", View.STD_VIEW,
+				800, 600, false, true, true, null);
+		view.setBackgroundColor(Color.LIGHT_GRAY);
 	}
 
 	public static void main(String[] args){
