f18bc2ec274a6de6e84b02b35eb649d381187da2
Romain Primet <>
2009-05-14 12:25:01 +0000

    made slaveapp size consistent with wall size
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1817 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm/branches/TRY-RP-ClusterPrototypeTC/src/main/java/net/claribole/zvtm/cluster/SlaveApp.java
+++ zvtm/branches/TRY-RP-ClusterPrototypeTC/src/main/java/net/claribole/zvtm/cluster/SlaveApp.java
@@ -31,7 +31,7 @@ public class SlaveApp {
 		Vector<Camera> vcam = new Vector<Camera>();
 		vcam.add(vs.getMetaCamera().retrieveCamera(blockNumber));
 		view = vsm.addExternalView(vcam, "slaveView"  + blockNumber, View.STD_VIEW,
-				400, 240, false, true, true, null);
+				2560, 1600, false, true, true, null);
 		view.setBackgroundColor(Color.LIGHT_GRAY);
 		vcam.get(0).setOwningView(view); 
 	}
