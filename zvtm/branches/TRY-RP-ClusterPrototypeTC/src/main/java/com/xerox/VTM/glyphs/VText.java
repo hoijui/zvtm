d12bba05e52a15e96d83848533c7ed3d5d9dabd8
Romain Primet <>
2009-05-14 07:26:24 +0000

    reverted field suppression
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1812 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm/branches/TRY-RP-ClusterPrototypeTC/src/main/java/com/xerox/VTM/glyphs/VText.java
+++ zvtm/branches/TRY-RP-ClusterPrototypeTC/src/main/java/com/xerox/VTM/glyphs/VText.java
@@ -80,6 +80,9 @@ public class VText extends Glyph {
      */
     public Font font;
 
+	protected AffineTransform at;
+	protected Rectangle2D bounds;
+
     /** Text that should be painted (read-only), use access methods to change.
      *@see #setText(String t)
      *@see #getText()
@@ -364,14 +367,12 @@ public class VText extends Glyph {
 			//if this value is < to about 0.5, AffineTransform.scale does not work properly (anyway, font is too small to be readable)
 			g.setFont((font!=null) ? font : VirtualSpaceManager.getMainFont());
 			if (!pc[i].valid){
-				Rectangle2D bounds;
 				bounds = g.getFontMetrics().getStringBounds(text,g);
 				// cw and ch actually hold width and height of text *in virtual space*
 				pc[i].cw = (int)Math.round(bounds.getWidth() * scaleFactor);
 				pc[i].ch = (int)Math.round(bounds.getHeight() * scaleFactor);
 				pc[i].valid=true;
 			}
-			AffineTransform at;
 			if (text_anchor==TEXT_ANCHOR_START){at=AffineTransform.getTranslateInstance(dx+pc[i].cx,dy+pc[i].cy);}
 			else if (text_anchor==TEXT_ANCHOR_MIDDLE){at=AffineTransform.getTranslateInstance(dx+pc[i].cx-pc[i].cw*coef/2.0f,dy+pc[i].cy);}
 			else {at=AffineTransform.getTranslateInstance(dx+pc[i].cx-pc[i].cw*coef,dy+pc[i].cy);}
@@ -392,14 +393,12 @@ public class VText extends Glyph {
 			//if this value is < to about 0.5, AffineTransform.scale does not work properly (anyway, font is too small to be readable)
 			g.setFont((font!=null) ? font : VirtualSpaceManager.getMainFont());
 			if (!pc[i].lvalid){
-				Rectangle2D bounds;
 				bounds = g.getFontMetrics().getStringBounds(text,g);
 				// lcw and lch actually hold width and height of text *in virtual space*
 				pc[i].lcw = (int)Math.round(bounds.getWidth() * scaleFactor);
 				pc[i].lch = (int)Math.round(bounds.getHeight() * scaleFactor);
 				pc[i].lvalid=true;
 			}
-			AffineTransform at;
 			if (text_anchor==TEXT_ANCHOR_START){at=AffineTransform.getTranslateInstance(dx+pc[i].lcx,dy+pc[i].lcy);}
 			else if (text_anchor==TEXT_ANCHOR_MIDDLE){at=AffineTransform.getTranslateInstance(dx+pc[i].lcx-pc[i].lcw*coef/2.0f,dy+pc[i].lcy);}
 			else {at=AffineTransform.getTranslateInstance(dx+pc[i].lcx-pc[i].lcw*coef,dy+pc[i].lcy);}
