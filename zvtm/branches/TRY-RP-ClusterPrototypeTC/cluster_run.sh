40e26964c6af200a52385fadaeae853cfd701275
Romain Primet <>
2009-05-14 15:39:14 +0000

    added start script for the WILD cluster
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1822 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ zvtm/branches/TRY-RP-ClusterPrototypeTC/cluster_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#start the TC server
+ssh wild@d2.wild.lri.fr "export DISPLAY=:0.0 && cd /home/wild/romain/zvtm_tc && mvn -DactiveNodes=master tc:run" &
+
+#start client nodes
+ssh wild@d1.wild.lri.fr "export DISPLAY=:0.0 && cd /home/wild/romain/zvtm_tc && mvn -DactiveNodes=slave0 -DstartServer=false tc:run" &
+ssh wild@d1.wild.lri.fr "export DISPLAY=:0.1 && cd /home/wild/romain/zvtm_tc && mvn -DactiveNodes=slave3 -DstartServer=false tc:run" &
+ssh wild@d2.wild.lri.fr "export DISPLAY=:0.0 && cd /home/wild/romain/zvtm_tc && mvn -DactiveNodes=slave1 -DstartServer=false tc:run" &
+ssh wild@d2.wild.lri.fr "export DISPLAY=:0.1 && cd /home/wild/romain/zvtm_tc && mvn -DactiveNodes=slave4 -DstartServer=false tc:run" &
+ssh wild@d3.wild.lri.fr "export DISPLAY=:0.0 && cd /home/wild/romain/zvtm_tc && mvn -DactiveNodes=slave2 -DstartServer=false tc:run" &
+ssh wild@d3.wild.lri.fr "export DISPLAY=:0.1 && cd /home/wild/romain/zvtm_tc && mvn -DactiveNodes=slave5 -DstartServer=false tc:run" &
+
