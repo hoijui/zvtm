f9a69d521caddf24f29cce66b3c906a69591f28d
Romain Primet <>
2009-05-15 12:15:08 +0000

    opengl arg
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1834 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm/branches/TRY-RP-ClusterPrototypeTC/cluster_run.sh
+++ zvtm/branches/TRY-RP-ClusterPrototypeTC/cluster_run.sh
@@ -2,10 +2,10 @@
 
 #start the TC server
 #start client nodes
-ssh wild@d1.wild.lri.fr "export DISPLAY=:0.0 && cd /home/wild/romain/zvtm_tc && mvn -DactiveNodes=slave0 -DstartServer=false tc:run" &
-ssh wild@d1.wild.lri.fr "export DISPLAY=:0.1 && cd /home/wild/romain/zvtm_tc && mvn -DactiveNodes=slave3 -DstartServer=false tc:run" &
-ssh wild@d2.wild.lri.fr "export DISPLAY=:0.0 && cd /home/wild/romain/zvtm_tc && mvn -DactiveNodes=slave1 -DstartServer=false tc:run" &
-ssh wild@d2.wild.lri.fr "export DISPLAY=:0.1 && cd /home/wild/romain/zvtm_tc && mvn -DactiveNodes=slave4 -DstartServer=false tc:run" &
-ssh wild@d3.wild.lri.fr "export DISPLAY=:0.0 && cd /home/wild/romain/zvtm_tc && mvn -DactiveNodes=slave2 -DstartServer=false tc:run" &
-ssh wild@d3.wild.lri.fr "export DISPLAY=:0.1 && cd /home/wild/romain/zvtm_tc && mvn -DactiveNodes=slave5 -DstartServer=false tc:run" &
+ssh wild@d1.wild.lri.fr "export DISPLAY=:0.0 && cd /home/wild/romain/zvtm_tc && mvn -DactiveNodes=slave0 -DstartServer=false -Dsun.java2d.opengl=True tc:run" &
+ssh wild@d1.wild.lri.fr "export DISPLAY=:0.1 && cd /home/wild/romain/zvtm_tc && mvn -DactiveNodes=slave3 -DstartServer=false -Dsun.java2d.opengl=True tc:run" &
+ssh wild@d2.wild.lri.fr "export DISPLAY=:0.0 && cd /home/wild/romain/zvtm_tc && mvn -DactiveNodes=slave1 -DstartServer=false -Dsun.java2d.opengl=True tc:run" &
+ssh wild@d2.wild.lri.fr "export DISPLAY=:0.1 && cd /home/wild/romain/zvtm_tc && mvn -DactiveNodes=slave4 -DstartServer=false -Dsun.java2d.opengl=True tc:run" &
+ssh wild@d3.wild.lri.fr "export DISPLAY=:0.0 && cd /home/wild/romain/zvtm_tc && mvn -DactiveNodes=slave2 -DstartServer=false -Dsun.java2d.opengl=True tc:run" &
+ssh wild@d3.wild.lri.fr "export DISPLAY=:0.1 && cd /home/wild/romain/zvtm_tc && mvn -DactiveNodes=slave5 -DstartServer=false -Dsun.java2d.opengl=True tc:run" &
 
