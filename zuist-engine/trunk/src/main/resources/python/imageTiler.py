697898634a5dc3a236e3249aa4a21c1cda647a02
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2009-05-19 19:40:47 +0000

    more cmdline features
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1856 74e165f3-c697-40a6-91af-552c8936d7fe

--- zuist-engine/trunk/src/main/resources/python/imageTiler.py
+++ zuist-engine/trunk/src/main/resources/python/imageTiler.py
@@ -9,7 +9,12 @@ from PIL import Image
 # http://effbot.org/zone/element-index.htm
 import elementtree.ElementTree as ET
 
+CMD_LINE_HELP = "ZUIST Image Tiling Script\n\nUsage:\n\n\timageTyler <src_image_path> <target_dir> [<tile_width>x<tile_height>] [trace_level]\n"
+
 TRACE_LEVEL = 1
+
+TILE_W = 500
+TILE_H = 500
 
 ################################################################################
 # Create target directory if it does not exist yet
@@ -45,9 +50,14 @@ if len(sys.argv) > 2:
     SRC_PATH = os.path.realpath(sys.argv[1])
     TGT_DIR = os.path.realpath(sys.argv[2])
     if len(sys.argv) > 3:
-        TRACE_LEVEL = int(sys.argv[3])
+        TILE_W = int(sys.argv[3].split("x")[0])
+        TILE_H = int(sys.argv[3].split("x")[0])
+        if len(sys.argv) > 4:
+            TRACE_LEVEL = int(sys.argv[4])
 else:
+    log(CMD_LINE_HELP)
     sys.exit(0)
 
+log("Tile Size: %sx%s" % (TILE_W, TILE_H), 1)
 createTargetDir()
 processSrcImg()
