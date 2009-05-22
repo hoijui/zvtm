8cb2bf2d0659b07106e5914e8981a958dc829b74
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2009-05-22 11:01:47 +0000

    - fixed bug: original image was not rescaled (not generated at all, actually) for level 0 - started working on regions
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1877 74e165f3-c697-40a6-91af-552c8936d7fe

--- zuist-engine/trunk/src/main/resources/python/imageTiler.py
+++ zuist-engine/trunk/src/main/resources/python/imageTiler.py
@@ -15,6 +15,10 @@ import elementtree.ElementTree as ET
 # ---------
 # | 3 | 4 |
 # ---------
+TL = 1
+TR = 2
+BL = 3
+BR = 4
 
 CMD_LINE_HELP = "ZUIST Image Tiling Script\n\nUsage:\n\n" + \
     " \timageTyler <src_image_path> <target_dir> [options]\n\n" + \
@@ -46,9 +50,10 @@ def createTargetDir():
         os.mkdir(TGT_DIR)
         
 ################################################################################
-# Count number of levels in ZUIST scene (source image size from PIL, tile size)
+# Count number of levels in ZUIST scene
+# (source image size from PIL, parent XML element)
 ################################################################################
-def generateLevels(src_sz, parent):
+def generateLevels(src_sz, rootEL):
     # number of horizontal tiles at lowest level
     htc = src_sz[0] / TILE_SIZE
     if src_sz[0] % TILE_SIZE > 0:
@@ -65,7 +70,7 @@ def generateLevels(src_sz, parent):
     for i in range(int(res)):
         depth = int(res-i-1)
         altitudes.append(int(F*math.pow(2,i+1)-F))
-        level = ET.SubElement(parent, "level")
+        level = ET.SubElement(rootEL, "level")
         level.set("depth", str(depth))
         level.set("floor", str(altitudes[-2]))
         level.set("ceiling", str(altitudes[-1]))
@@ -74,40 +79,48 @@ def generateLevels(src_sz, parent):
     return res
 
 ################################################################################
-# 
+# generate tiles (rec calls)
+# (tile ID of parent, position in parent tile, current level, number of levels
+#  top left coords, size of source image, parent XML element)
 ################################################################################
-def buildTiles(parentTileID, inc, level, levelCount, x, y, src_sz, parent):
+def buildTiles(parentTileID, pos, level, levelCount, x, y, src_sz, rootEL, parentRegionID):
     if (level >= levelCount):
         return
     tileID = copy(parentTileID)
-    tileID[level] = tileID[level] + inc
+    tileID[level] = tileID[level] + pos
     tileIDstr = "-".join([str(s) for s in tileID])
     if x > src_sz[0] or y > src_sz[1]:
         log("Ignoring tile %s (out of bounds)" % tileIDstr, 3)
         return
     scale = math.pow(2, levelCount-level-1)
-    if (level != 0):
-            # generate image except for level 0 where we use original image
-            tilePath = "%s/%s%s.jpg" % (TGT_DIR, TILE_FILE_PREFIX, tileIDstr)
-            if os.path.exists(tilePath) and not FORCE_GENERATE_TILES:
-                log("%s already exists (skipped)" % tilePath, 2)
-            else:    
-                log("----\nGenerating tile %s" % tileIDstr, 2)
-                ccl = "convert %s -crop %sx%s+%s+%s -quality 95 %s" % (SRC_PATH, str(int(TILE_SIZE*scale)), str(int(TILE_SIZE*scale)), str(int(x)), str(int(y)), tilePath)
-                os.system(ccl)
-                log("Cropping: %s" % ccl, 3)
-                if scale > 1.0:
-                    ccl = "convert %s -resize %sx%s -quality 95 %s" % (tilePath, str(int(TILE_SIZE)), str(int(TILE_SIZE)), tilePath)
-                    os.system(ccl)
-                    log("Rescaling %s" % ccl, 3)
+    # generate image tile
+    # generate image except for level 0 where we use original image
+    tilePath = "%s/%s%s.jpg" % (TGT_DIR, TILE_FILE_PREFIX, tileIDstr)
+    if os.path.exists(tilePath) and not FORCE_GENERATE_TILES:
+        log("%s already exists (skipped)" % tilePath, 2)
+    else:    
+        log("----\nGenerating tile %s" % tileIDstr, 2)
+        ccl = "convert %s -crop %sx%s+%s+%s -quality 95 %s" % (SRC_PATH, str(int(TILE_SIZE*scale)), str(int(TILE_SIZE*scale)), str(int(x)), str(int(y)), tilePath)
+        os.system(ccl)
+        log("Cropping: %s" % ccl, 3)
+        if scale > 1.0:
+            ccl = "convert %s -resize %sx%s -quality 95 %s" % (tilePath, str(int(TILE_SIZE)), str(int(TILE_SIZE)), tilePath)
+            os.system(ccl)
+            log("Rescaling %s" % ccl, 3)
+    # generate ZUIST region and image object
+    regionEL = ET.SubElement(rootEL, "region")
+    regionEL.set("id", "R%s" % tileIDstr)
+    if parentRegionID is not None:
+        regionEL.set("containedIn", parentRegionID)
+    regionEL.set("levels", str(level))
     # call to lower level, top left
-    buildTiles(tileID, 1, level+1, levelCount, x, y, src_sz, parent)
+    buildTiles(tileID, TL, level+1, levelCount, x, y, src_sz, rootEL, regionEL.get("id"))
     # call to lower level, top right
-    buildTiles(tileID, 2, level+1, levelCount, x+TILE_SIZE*scale/2, y, src_sz, parent)
+    buildTiles(tileID, TR, level+1, levelCount, x+TILE_SIZE*scale/2, y, src_sz, rootEL, regionEL.get("id"))
     # call to lower level, bottom left
-    buildTiles(tileID, 3, level+1, levelCount, x, y+TILE_SIZE*scale/2, src_sz, parent)
+    buildTiles(tileID, BL, level+1, levelCount, x, y+TILE_SIZE*scale/2, src_sz, rootEL, regionEL.get("id"))
     # call to lower level, bottom right
-    buildTiles(tileID, 4, level+1, levelCount, x+TILE_SIZE*scale/2, y+TILE_SIZE*scale/2, src_sz, parent)
+    buildTiles(tileID, BR, level+1, levelCount, x+TILE_SIZE*scale/2, y+TILE_SIZE*scale/2, src_sz, rootEL, regionEL.get("id"))
     
 ################################################################################
 # Create tiles and ZUIST XML scene from source image
@@ -121,7 +134,7 @@ def processSrcImg():
     src_im = Image.open(SRC_PATH)
     src_sz = src_im.size
     levelCount = generateLevels(src_sz, outputroot)
-    buildTiles([0 for i in range(int(levelCount))], 1, 0, levelCount, 0, 0, src_sz, outputroot)
+    buildTiles([0 for i in range(int(levelCount))], 1, 0, levelCount, 0, 0, src_sz, outputroot, None)
     # serialize the XML tree
     tree = ET.ElementTree(outputroot)
     log("Writing %s" % outputSceneFile)
