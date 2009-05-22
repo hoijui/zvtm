39c1815f8485dd926fef380433237b64f155a9b8
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2009-05-22 07:55:07 +0000

    counting levels, tiles are square
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1873 74e165f3-c697-40a6-91af-552c8936d7fe

--- zuist-engine/trunk/src/main/resources/python/imageTiler.py
+++ zuist-engine/trunk/src/main/resources/python/imageTiler.py
@@ -9,12 +9,11 @@ from PIL import Image
 # http://effbot.org/zone/element-index.htm
 import elementtree.ElementTree as ET
 
-CMD_LINE_HELP = "ZUIST Image Tiling Script\n\nUsage:\n\n\timageTyler <src_image_path> <target_dir> [<tile_width>x<tile_height>] [trace_level]\n"
+CMD_LINE_HELP = "ZUIST Image Tiling Script\n\nUsage:\n\n\timageTyler <src_image_path> <target_dir> [tile_size] [trace_level]\n"
 
 TRACE_LEVEL = 1
 
-TILE_W = 500
-TILE_H = 500
+TILE_SIZE = 500
 
 ################################################################################
 # Create target directory if it does not exist yet
@@ -23,12 +22,33 @@ def createTargetDir():
     if not os.path.exists(TGT_DIR):
         log("Creating target directory %s" % TGT_DIR, 2)
         os.mkdir(TGT_DIR)
+        
+################################################################################
+# Count number of levels in ZUIST scene (source image size from PIL, tile size)
+################################################################################
+def countLevels(src_sz, tile_sz):
+    # number of horizontal tiles at lowest level
+    htc = src_sz[0] / tile_sz
+    if src_sz[0] % tile_sz > 0:
+        htc += 1
+    # number of vertical tiles at lowest level
+    vtc = src_sz[1] / tile_sz
+    if src_sz[1] % tile_sz > 0:
+        vtc += 1
+    return math.ceil(max(math.log(vtc,2)+1, math.log(htc,2)+1))
 
 ################################################################################
 # Create tiles and ZUIST XML scene from source image
 ################################################################################
 def processSrcImg():
     outputSceneFile = "%s/img_scene.xml" % TGT_DIR
+    log("Loading source image from %s" % SRC_PATH, 2)
+    src_im = Image.open(SRC_PATH)
+    src_sz = src_im.size
+    levelCount = countLevels(src_sz, TILE_SIZE)
+    log("Will generate %s level(s)" % levelCount, 2)
+    
+    
     # prepare the XML scene
     outputroot = ET.Element("scene")
     # serialize the XML tree
@@ -50,14 +70,13 @@ if len(sys.argv) > 2:
     SRC_PATH = os.path.realpath(sys.argv[1])
     TGT_DIR = os.path.realpath(sys.argv[2])
     if len(sys.argv) > 3:
-        TILE_W = int(sys.argv[3].split("x")[0])
-        TILE_H = int(sys.argv[3].split("x")[0])
+        TILE_SIZE = int(sys.argv[3])
         if len(sys.argv) > 4:
             TRACE_LEVEL = int(sys.argv[4])
 else:
     log(CMD_LINE_HELP)
     sys.exit(0)
 
-log("Tile Size: %sx%s" % (TILE_W, TILE_H), 1)
+log("Tile Size: %sx%s" % (TILE_SIZE, TILE_SIZE), 1)
 createTargetDir()
 processSrcImg()
