79b39a17060abfdebcbe53cf8b246e78dcc76cab
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2009-05-22 15:00:19 +0000

    added support for translucency in CircleNR
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1884 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm/trunk/src/main/java/net/claribole/zvtm/glyphs/CircleNR.java
+++ zvtm/trunk/src/main/java/net/claribole/zvtm/glyphs/CircleNR.java
@@ -44,6 +44,19 @@ public class CircleNR extends VCircle {
 	public CircleNR(long x, long y, int z, long r, Color c, Color bc){
 		super(x, y, z, r, c, bc);
 	}
+
+	/**
+		*@param x coordinate in virtual space
+		*@param y coordinate in virtual space
+		*@param z z-index (pass 0 if you do not use z-ordering)
+		*@param r radius in virtual space
+		*@param c fill color
+		*@param bc border color
+		*@param alpha alpha channel value in [0;1.0] 0 is fully transparent, 1 is opaque
+		*/
+	public CircleNR(long x, long y, int z, long r, Color c, Color bc, float alpha){
+		super(x, y, z, r, c, bc, alpha);
+	}
 	
 	public void project(Camera c, Dimension d){
 		int i=c.getIndex();
