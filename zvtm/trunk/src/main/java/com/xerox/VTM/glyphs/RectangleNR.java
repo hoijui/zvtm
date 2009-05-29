7059e72bf6cbcb098fe0d269a6a3f867730d8def
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2009-05-29 12:37:57 +0000

    added support for translucency
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@1966 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm/trunk/src/main/java/com/xerox/VTM/glyphs/RectangleNR.java
+++ zvtm/trunk/src/main/java/com/xerox/VTM/glyphs/RectangleNR.java
@@ -274,63 +274,142 @@ public class RectangleNR extends ClosedShape implements RectangularShape {
     }
 
     public void draw(Graphics2D g,int vW,int vH,int i,Stroke stdS,AffineTransform stdT, int dx, int dy){
-	if ((pc[i].cw>1) && (pc[i].ch>1)) {//repaint only if object is visible
-	    if (filled) {
-		g.setColor(this.color);
-		g.fillRect(dx+pc[i].cx-pc[i].cw,dy+pc[i].cy-pc[i].ch,2*pc[i].cw,2*pc[i].ch);
-	    }
-	    if (paintBorder){
-		g.setColor(borderColor);
-		g.drawRect(dx+pc[i].cx-pc[i].cw,dy+pc[i].cy-pc[i].ch,2*pc[i].cw-1,2*pc[i].ch-1);
-	    }
-	}
-	else if ((pc[i].cw<=1) ^ (pc[i].ch<=1)) {//repaint only if object is visible  (^ means xor)
-	    g.setColor(this.color);
-	    if (pc[i].cw<=1){
-		g.fillRect(dx+pc[i].cx,dy+pc[i].cy-pc[i].ch,1,2*pc[i].ch);
-	    }
-	    else if (pc[i].ch<=1){
-		g.fillRect(dx+pc[i].cx-pc[i].cw,dy+pc[i].cy,2*pc[i].cw,1);
-	    }
-	}
-	else {
-	    g.setColor(this.color);
-	    g.fillRect(dx+pc[i].cx,dy+pc[i].cy,1,1);
-	}
+        if (alphaC != null && alphaC.getAlpha()==0){return;}
+        if ((pc[i].cw>1) && (pc[i].ch>1)) {
+            if (alphaC != null){
+                g.setComposite(alphaC);
+                //repaint only if object is visible
+                if (filled) {
+                    g.setColor(this.color);
+                    g.fillRect(dx+pc[i].cx-pc[i].cw,dy+pc[i].cy-pc[i].ch,2*pc[i].cw,2*pc[i].ch);
+                }
+                if (paintBorder){
+                    g.setColor(borderColor);
+                    g.drawRect(dx+pc[i].cx-pc[i].cw,dy+pc[i].cy-pc[i].ch,2*pc[i].cw-1,2*pc[i].ch-1);
+                }
+                g.setComposite(acO);
+            }
+            else {
+                //repaint only if object is visible
+                if (filled) {
+                    g.setColor(this.color);
+                    g.fillRect(dx+pc[i].cx-pc[i].cw,dy+pc[i].cy-pc[i].ch,2*pc[i].cw,2*pc[i].ch);
+                }
+                if (paintBorder){
+                    g.setColor(borderColor);
+                    g.drawRect(dx+pc[i].cx-pc[i].cw,dy+pc[i].cy-pc[i].ch,2*pc[i].cw-1,2*pc[i].ch-1);
+                }
+            }
+        }
+        else if ((pc[i].cw<=1) ^ (pc[i].ch<=1)) {
+            //repaint only if object is visible  (^ means xor)
+            g.setColor(this.color);
+            if (alphaC != null){
+                g.setComposite(alphaC);
+                if (pc[i].cw<=1){
+                    g.fillRect(dx+pc[i].cx,dy+pc[i].cy-pc[i].ch,1,2*pc[i].ch);
+                }
+                else if (pc[i].ch<=1){
+                    g.fillRect(dx+pc[i].cx-pc[i].cw,dy+pc[i].cy,2*pc[i].cw,1);
+                }
+                g.setComposite(acO);
+            }
+            else {
+                //repaint only if object is visible
+                if (pc[i].cw<=1){
+                    g.fillRect(dx+pc[i].cx,dy+pc[i].cy-pc[i].ch,1,2*pc[i].ch);
+                }
+                else if (pc[i].ch<=1){
+                    g.fillRect(dx+pc[i].cx-pc[i].cw,dy+pc[i].cy,2*pc[i].cw,1);
+                }
+            }
+        }
+        else {
+            if (alphaC != null){
+                g.setComposite(alphaC);
+                g.setColor(this.color);
+                g.fillRect(dx+pc[i].cx,dy+pc[i].cy,1,1);
+                g.setComposite(acO);
+            }
+            else {
+                g.setColor(this.color);
+                g.fillRect(dx+pc[i].cx,dy+pc[i].cy,1,1);
+            }
+        }
     }
 
     public void drawForLens(Graphics2D g,int vW,int vH,int i,Stroke stdS,AffineTransform stdT, int dx, int dy){
-	if ((pc[i].lcw>1) && (pc[i].lch>1)) {//repaint only if object is visible
-	    if (filled) {
-		g.setColor(this.color);
-		g.fillRect(dx+pc[i].lcx-pc[i].lcw,dy+pc[i].lcy-pc[i].lch,2*pc[i].lcw,2*pc[i].lch);
-	    }
-	    if (paintBorder){
-		g.setColor(borderColor);
-		g.drawRect(dx+pc[i].lcx-pc[i].lcw,dy+pc[i].lcy-pc[i].lch,2*pc[i].lcw-1,2*pc[i].lch-1);
-	    }
-	}
-	else if ((pc[i].lcw<=1) ^ (pc[i].lch<=1)) {//repaint only if object is visible  (^ means xor)
-	    g.setColor(this.color);
-	    if (pc[i].lcw<=1){
-		g.fillRect(dx+pc[i].lcx,dy+pc[i].lcy-pc[i].lch,1,2*pc[i].lch);
-	    }
-	    else if (pc[i].lch<=1){
-		g.fillRect(dx+pc[i].lcx-pc[i].lcw,dy+pc[i].lcy,2*pc[i].lcw,1);
-	    }
-	}
-	else {
-	    g.setColor(this.color);
-	    g.fillRect(dx+pc[i].lcx,dy+pc[i].lcy,1,1);
-	}
+        if (alphaC != null && alphaC.getAlpha()==0){return;}
+        if ((pc[i].lcw>1) && (pc[i].lch>1)) {
+            if (alphaC != null){
+                g.setComposite(alphaC);
+                //repaint only if object is visible
+                if (filled) {
+                    g.setColor(this.color);
+                    g.fillRect(dx+pc[i].lcx-pc[i].lcw,dy+pc[i].lcy-pc[i].lch,2*pc[i].lcw,2*pc[i].lch);
+                }
+                if (paintBorder){
+                    g.setColor(borderColor);
+                    g.drawRect(dx+pc[i].lcx-pc[i].lcw,dy+pc[i].lcy-pc[i].lch,2*pc[i].lcw-1,2*pc[i].lch-1);
+                }
+                g.setComposite(acO);
+            }
+            else {
+                //repaint only if object is visible
+                if (filled) {
+                    g.setColor(this.color);
+                    g.fillRect(dx+pc[i].lcx-pc[i].lcw,dy+pc[i].lcy-pc[i].lch,2*pc[i].lcw,2*pc[i].lch);
+                }
+                if (paintBorder){
+                    g.setColor(borderColor);
+                    g.drawRect(dx+pc[i].lcx-pc[i].lcw,dy+pc[i].lcy-pc[i].lch,2*pc[i].lcw-1,2*pc[i].lch-1);
+                }
+            }
+        }
+        else if ((pc[i].lcw<=1) ^ (pc[i].lch<=1)) {
+            //repaint only if object is visible  (^ means xor)
+            g.setColor(this.color);
+            if (alphaC != null){
+                g.setComposite(alphaC);
+                if (pc[i].lcw<=1){
+                    g.fillRect(dx+pc[i].lcx,dy+pc[i].lcy-pc[i].lch,1,2*pc[i].lch);
+                }
+                else if (pc[i].lch<=1){
+                    g.fillRect(dx+pc[i].lcx-pc[i].lcw,dy+pc[i].lcy,2*pc[i].lcw,1);
+                }
+                g.setComposite(acO);
+            }
+            else {
+                //repaint only if object is visible
+                if (pc[i].lcw<=1){
+                    g.fillRect(dx+pc[i].lcx,dy+pc[i].lcy-pc[i].lch,1,2*pc[i].lch);
+                }
+                else if (pc[i].lch<=1){
+                    g.fillRect(dx+pc[i].lcx-pc[i].lcw,dy+pc[i].lcy,2*pc[i].lcw,1);
+                }
+            }
+        }
+        else {
+            if (alphaC != null){
+                g.setComposite(alphaC);
+                g.setColor(this.color);
+                g.fillRect(dx+pc[i].lcx,dy+pc[i].lcy,1,1);
+                g.setComposite(acO);
+            }
+            else {
+                g.setColor(this.color);
+                g.fillRect(dx+pc[i].lcx,dy+pc[i].lcy,1,1);
+            }
+        }
     }
 
     public Object clone(){
-	RectangleNR res=new RectangleNR(vx,vy,0,vw,vh,color);
-	res.borderColor=this.borderColor;
-	res.mouseInsideColor=this.mouseInsideColor;
-	res.bColor=this.bColor;
-	return res;
+        RectangleNR res = new RectangleNR(vx,vy,0,vw,vh,color);
+        res.borderColor = this.borderColor;
+        res.mouseInsideColor = this.mouseInsideColor;
+        res.bColor = this.bColor;
+        res.setTranslucencyValue(getTranslucencyValue());
+        return res;
     }
 
 }
