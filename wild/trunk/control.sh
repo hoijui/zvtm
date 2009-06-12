9085447bc09ad67ccac0eec48c3cb91dcc9c601f
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2009-06-12 14:43:32 +0000

    added heap size params
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@2048 74e165f3-c697-40a6-91af-552c8936d7fe

--- wild/trunk/control.sh
+++ wild/trunk/control.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp target/zuist4wild-0.1.0-SNAPSHOT.jar fr.inria.wild.zuist.Controller "$@" 
+java -Xmx2048M -Xms512M -cp target/zuist4wild-0.1.0-SNAPSHOT.jar fr.inria.wild.zuist.Controller "$@" 
