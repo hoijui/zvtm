8812c819feeb2d1b56549471d239a27a69f856a4
Romain Primet <>
2009-06-16 07:46:21 +0000

    fixed typo in startup script and added VSegment transient
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@2063 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm/branches/TRY-RP-ClusterPrototypeTC/cluster_run.sh
+++ zvtm/branches/TRY-RP-ClusterPrototypeTC/cluster_run.sh
@@ -17,7 +17,7 @@ do
 		  colNum $col 
 		  SLAVENUM1=`expr $? \* 8 + $row - 1`
 		  SLAVENUM2=`expr $SLAVENUM1 + 4`
-		  ssh wild@$col$row.wild.lri.fr -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "export DISPLAY=:0.0 && cd /home/wild/romain/zvtm_tc && mvn -DslaveWidth=2560 -DslaveHeight=1600 -DactiveNodes=slave -DslaveNum=$SLAVENUM1 -DstartServer=false tc:run-Dcom.tc.servers.server.host=192.168.1.1" &
+		  ssh wild@$col$row.wild.lri.fr -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "export DISPLAY=:0.0 && cd /home/wild/romain/zvtm_tc && mvn -DslaveWidth=2560 -DslaveHeight=1600 -DactiveNodes=slave -DslaveNum=$SLAVENUM1 -DstartServer=false tc:run" &
 		  ssh wild@$col$row.wild.lri.fr -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "export DISPLAY=:0.0 && cd /home/wild/romain/zvtm_tc && mvn -DslaveWidth=2560 -DslaveHeight=1600 -DactiveNodes=slave -DslaveNum=$SLAVENUM2 -DstartServer=false tc:run" &
 
       done
