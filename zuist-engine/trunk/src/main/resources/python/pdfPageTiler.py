bccb63947b09da6fef6486e0c5d115b214024f42
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2009-06-18 11:03:40 +0000

    version that actually works, and generates a meta scene with <include> statements for convenience when the generated tiled will serve as input for multiple ZUIST scenes
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@2089 74e165f3-c697-40a6-91af-552c8936d7fe

--- zuist-engine/trunk/src/main/resources/python/pdfPageTiler.py
+++ zuist-engine/trunk/src/main/resources/python/pdfPageTiler.py
@@ -11,19 +11,23 @@ import os, sys, math
 from copy import copy
 # http://developer.apple.com/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d/dq_python/dq_python.html
 from CoreGraphics import *
+# http://effbot.org/zone/element-index.htm
+import elementtree.ElementTree as ET
 
 CMD_LINE_HELP = "ZUIST PDF Page Tiler Script\n\nUsage:\n\n" + \
     " \tpdfPageTiler <src_image_path> <target_dir> [options]\n\n" + \
     "Options:\n\n"+\
     "\t-ts=N\t\ttile size (N in pixels)\n"+\
+    "\t-tl=N\t\ttrace level (N in [0:3])\n"+\
     "\t-f\t\tforce tile generation\n"+\
-    "\t-tl=N\t\ttrace level (N in [0:3])\n"
+    "\t-scale=s\ts scale factor w.r.t default size\n"
 
 TRACE_LEVEL = 1
 
 FORCE_GENERATE_TILES = False
 
 TILE_SIZE = 500
+PDF_SCALE_FACTOR = 1
 
 OUTPUT_FILE_EXT = "png"
 
@@ -43,20 +47,49 @@ def createTargetDir():
 def processSrcPDF():
     # source image
     log("Loading source image from %s" % SRC_PATH, 2)
-    #pdf_document = CGPDFDocumentCreateWithProvider(CGDataProviderCreateWithFilename(SRC_PATH))
-    #page_rect = pdf_document.getPage(1).getBoxRect(1)
-    #log("Default PDF page size: %d x %d" % (page_rect.getWidth(), page_rect.getHeight()), 1)
-    #page_width = int(page_rect.getWidth() * PDF_SCALE_FACTOR)
-    #page_height = int(page_rect.getHeight() * PDF_SCALE_FACTOR)
-    #log("Target bitmap size: %d x %d" % (page_width, page_height), 1)
-    #bitmap = CGBitmapContextCreateWithColor(page_width, page_height,\
-    #                                        COLOR_SPACE, (1,1,1,1))
-    #log("\tRescaling bitmap", 3)
-    #bitmap.scaleCTM(PDF_SCALE_FACTOR, PDF_SCALE_FACTOR)
-    #log("\tDrawing PDF to bitmap", 3)
-    #bitmap.drawPDFDocument(page_rect, pdf_document, 1)
-    #log("\tWriting bitmap to temp file", 3)
-    #bitmap.writeToFile(IMG_SRC_PATH, kCGImageFormatPNG)
+    pdf_document = CGPDFDocumentCreateWithProvider(CGDataProviderCreateWithFilename(SRC_PATH))
+    page = pdf_document.getPage(1)
+    page_rect = page.getBoxRect(kCGPDFCropBox)
+    log("Default PDF page size: %d x %d" % (page_rect.getWidth(), page_rect.getHeight()), 1)
+    page_width = int(page_rect.getWidth() * PDF_SCALE_FACTOR)
+    page_height = int(page_rect.getHeight() * PDF_SCALE_FACTOR)
+    log("Target bitmap size: %d x %d" % (page_width, page_height), 1)
+    outputSceneFile = "%s/scene.xml" % TGT_DIR
+    outputroot = ET.Element("scene")
+    x = 0
+    y = 0
+    while y < page_height:
+        x = 0
+        while x < page_width:
+            generateTile(pdf_document, page, x, y, "%s/tile-%d-%d.%s" % (TGT_DIR, x/TILE_SIZE, y/TILE_SIZE, OUTPUT_FILE_EXT))
+            
+            include = ET.SubElement(outputroot, "include")
+            include.set("src", "tile-%d-%d/scene.xml" % (x/TILE_SIZE, y/TILE_SIZE))
+            include.set("x", "%d" % x)
+            include.set("y", "%d" % y)
+            
+            x += TILE_SIZE
+        y += TILE_SIZE
+    log("Writing scene file %s" % outputSceneFile, 1)
+    tree = ET.ElementTree(outputroot)
+    tree.write(outputSceneFile, encoding='utf-8')
+
+
+def generateTile(pdf_document, page, x, y, tgtPath):
+    if os.path.exists(tgtPath) and not FORCE_GENERATE_TILES:
+        log("Tile %s already exists" % (tgtPath), 2)
+        return
+    log("Generating tile (%d,%d) (%d,%d)" % (x,y,x+TILE_SIZE,y+TILE_SIZE), 2)
+    bitmap = CGBitmapContextCreateWithColor(TILE_SIZE, TILE_SIZE,\
+                                            COLOR_SPACE, (1,1,1,1))    
+    rect = page.getBoxRect(kCGPDFMediaBox)
+    rect = rect.offset(-x,-y)
+    rect.size.width = int(rect.size.width * PDF_SCALE_FACTOR)
+    rect.size.height = int(rect.size.height * PDF_SCALE_FACTOR)
+    bitmap.drawPDFDocument(rect, pdf_document, kCGPDFCropBox)
+    log("\tWriting bitmap to %s" % tgtPath, 2)
+    bitmap.writeToFile(tgtPath, kCGImageFormatPNG)
+    
 
 ################################################################################
 # Trace exec on std output
@@ -74,12 +107,14 @@ if len(sys.argv) > 2:
     if len(sys.argv) > 3:
         for arg in sys.argv[3:]:
             if arg.startswith("-ts="):
-                TILE_SIZE = int(arg[4:])
+                TILE_SIZE = int(arg[len("-ts="):])
             elif arg == "-f":
                 FORCE_GENERATE_TILES = True
                 log("Force tile generation")
             elif arg.startswith("-tl="):
-                TRACE_LEVEL = int(arg[4:])
+                TRACE_LEVEL = int(arg[len("-tl="):])
+            elif arg.startswith("-scale="):
+                PDF_SCALE_FACTOR = float(arg[len("-scale="):])
 else:
     log(CMD_LINE_HELP)
     sys.exit(0)
