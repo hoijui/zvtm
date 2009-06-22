9ecf9c778ae50167e51285f3a59bbbdd0827437a
Romain Primet <>
2009-06-22 12:45:08 +0000

    updated TC server start command to ignore client-side SSH key checks
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@2101 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm/branches/TRY-RP-ClusterPrototypeTC/master_run.sh
+++ zvtm/branches/TRY-RP-ClusterPrototypeTC/master_run.sh
@@ -3,7 +3,7 @@
 #start the TC server
 #ssh wild@d2.wild.lri.fr "export DISPLAY=:0.0 && cd /home/wild/romain/zvtm_tc && mvn -DactiveNodes=master tc:run" &
 
-ssh wild@d2.wild.lri.fr "mvn tc:start" &
+ssh wild@d2.wild.lri.fr -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "cd /home/wild/romain/zvtm_tc && mvn tc:start" &
 sleep 4
 mvn -DstartServer=false -DactiveNodes=master tc:run&
 
