2c87b2de5e483ef8b7a135753cd6c6f759eeaadb
Romain Primet <>
2009-07-09 12:14:54 +0000

    adjusted params
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@2194 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm/branches/TRY-RP-ClusterPrototypeAJG/master_run.sh
+++ zvtm/branches/TRY-RP-ClusterPrototypeAJG/master_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #execute after cluster nodes
-java -Djava.net.preferIPv4Stack=true -cp target/zvtm-0.10.0-SNAPSHOT.jar:target/timingframework-1.0.jar:target/aspectjrt-1.5.4.jar:target/jgroups-2.7.0.GA.jar:target/commons-logging-1.1.jar:target/args4j-2.0.12.jar fr.inria.zvtm.clustering.TestColorRect -w 25000 -h 8000
+java -Djava.net.preferIPv4Stack=true -cp target/zvtm-0.10.0-SNAPSHOT.jar:target/timingframework-1.0.jar:target/aspectjrt-1.5.4.jar:target/jgroups-2.7.0.GA.jar:target/commons-logging-1.1.jar:target/args4j-2.0.12.jar fr.inria.zvtm.clustering.TestColorRect -w 25000 -h 8000 -x 200 -y 100
 
