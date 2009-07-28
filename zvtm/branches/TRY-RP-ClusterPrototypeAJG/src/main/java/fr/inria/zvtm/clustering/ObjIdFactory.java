2be52f08460bbe80bcaed0f1dc755b0cc52b44be
Romain Primet <>
2009-07-28 08:15:36 +0000

    added copyright headers
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@2239 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm/branches/TRY-RP-ClusterPrototypeAJG/src/main/java/fr/inria/zvtm/clustering/ObjIdFactory.java
+++ zvtm/branches/TRY-RP-ClusterPrototypeAJG/src/main/java/fr/inria/zvtm/clustering/ObjIdFactory.java
@@ -1,3 +1,9 @@
+/*   AUTHOR : Romain Primet (romain.primet@inria.fr)
+ *
+ *  (c) COPYRIGHT INRIA (Institut National de Recherche en Informatique et en Automatique), 2009.
+ *  Licensed under the GNU LGPL. For full terms see the file COPYING.
+ *
+ */ 
 package fr.inria.zvtm.clustering;
 
 import java.util.concurrent.atomic.AtomicLong;
