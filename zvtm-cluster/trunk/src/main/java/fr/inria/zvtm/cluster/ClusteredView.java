8e7c7c92853fea69afe67628cfa1d5db56f48970
Romain Primet <>
2009-10-01 08:58:20 +0000

    added skeletal example ColorRectCluster, updated ClusteredView
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@2435 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-cluster/trunk/src/main/java/fr/inria/zvtm/cluster/ClusteredView.java
+++ zvtm-cluster/trunk/src/main/java/fr/inria/zvtm/cluster/ClusteredView.java
@@ -1,16 +1,26 @@
 package fr.inria.zvtm.cluster;
 
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import fr.inria.zvtm.engine.Camera;
+
 public class ClusteredView {
+	//ObjId
 	private final int origin; //bottom-left block number
 	//width of a screen, in pixels, possibly including bezels
-	private final int blockWidth;
-	private final int blockHeight;
+	private int blockWidth;
+	private int blockHeight;
 	private final int nbRows;
 	private final int nbCols;
+	private final ArrayList<Camera> cameras = new ArrayList<Camera>();
 
 	public ClusteredView(int origin, 
 			int blockWidth, int blockHeight,
-			int nbRows, int nbCols){
+			int nbRows, int nbCols,
+			List<Camera> cameras){
 		if(origin < 0){
 		   throw new IllegalArgumentException("Blocks are 0-based naturals");
 		}
@@ -25,6 +35,20 @@ public class ClusteredView {
 		this.blockHeight = blockHeight;
 		this.nbRows = nbRows;
 		this.nbCols = nbCols;
+		Collections.copy(this.cameras, cameras);
 	}
 
+	public void setBlockSize(int blockWidth, int blockHeight){
+		if((blockWidth <=0) || (blockHeight <=0)){
+			throw new IllegalArgumentException("Block dimensions should be greater than 0");
+		}
+		this.blockWidth = blockWidth;
+		this.blockHeight = blockHeight;
+		//propagate changes to the Cameras associated with this View
+	}
+
+	public void setBackgroundColor(Color color){
+		//TODO implement
+		throw new UnsupportedOperationException();
+	}
 }
---
+++ zvtm-cluster/trunk/src/main/java/fr/inria/zvtm/cluster/examples/ColorRectCluster.java
@@ -0,0 +1,91 @@
+package fr.inria.zvtm.cluster.examples;
+
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+import fr.inria.zvtm.cluster.ClusteredView;
+import fr.inria.zvtm.engine.Camera;
+import fr.inria.zvtm.engine.View;
+import fr.inria.zvtm.engine.VirtualSpace;
+import fr.inria.zvtm.engine.VirtualSpaceManager;
+import fr.inria.zvtm.glyphs.VRectangle;
+
+import java.awt.Color;
+import java.util.Vector;
+
+/**
+ * Sample master application.
+ */
+public class ColorRectCluster {
+	//shortcut
+	private VirtualSpaceManager vsm = VirtualSpaceManager.INSTANCE; 
+
+	ColorRectCluster(CROptions options){
+		VirtualSpace vs = vsm.addVirtualSpace("testSpace");
+		Camera cam = vs.addCamera();
+		Vector<Camera> cameras = new Vector<Camera>();
+		cameras.add(cam);	
+		ClusteredView cv = 
+			new ClusteredView(0, //origin (block number)
+					400, //block width
+					300, //block height
+					2, //rows,
+					3, //cols
+					cameras);
+		cv.setBackgroundColor(Color.LIGHT_GRAY);
+
+		//the view below is just a standard, non-clustered view
+		//that lets an user navigate the scene
+		View view = vsm.addFrameView(cameras, "Master View",
+			   View.STD_VIEW, 800, 600, false, true, true, null);	
+
+		long xOffset = 0;
+		long yOffset = 0;
+		long rectWidth = options.width / options.xNum;
+		long rectHeight = options.height / options.yNum;
+		for(int i=0; i<options.xNum; ++i){
+			for(int j=0; j<options.yNum; ++j){
+				VRectangle rect = 
+					new VRectangle(xOffset+i*rectWidth,
+							yOffset+j*rectHeight,
+							0,
+							rectWidth/2,
+							rectHeight/2,
+							Color.getHSBColor((float)(i*j/(float)(options.xNum*options.yNum)), 1f, 1f));
+				rect.setDrawBorder(false);
+				vs.addGlyph(rect, false);
+			}
+		}
+	}
+
+	public static void main(String[] args){
+		CROptions options = new CROptions();
+		CmdLineParser parser = new CmdLineParser(options);
+		try{
+			parser.parseArgument(args);
+		} catch(CmdLineException ex){
+			System.err.println(ex.getMessage());
+			parser.printUsage(System.err);
+			return;
+		}
+
+		new ColorRectCluster(options);
+	}
+}
+
+class CROptions {
+	@Option(name = "-x", aliases = {"--xnum"}, usage = "number of subdivisions along x axis")
+	int xNum = 50;
+
+	@Option(name = "-y", aliases = {"--ynum"}, usage = "number of subdivisions along y axis")
+	int yNum = 20;
+
+	@Option(name = "-w", aliases = {"--width"}, usage = "color rect width")
+	int width = 800;
+
+	@Option(name = "-h", aliases = {"--height"}, usage = "color rect height")
+	int height = 600;
+}
+
