368156b0c65b2932b5ae81108dd50316697f5f78
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2009-10-07 13:24:56 +0000

    changed project/JAR file names so as not to conflict with ZVTM trunk
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@2460 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm/branches/TRY-RP-ClusterPrototypeAJG/jmx_slave.sh
+++ zvtm/branches/TRY-RP-ClusterPrototypeAJG/jmx_slave.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Run the generic slave using JMX monitoring
-java -Dcom.sun.management.jmxremote -Xmx2g -cp target/zvtm-0.10.0-SNAPSHOT.jar:target/timingframework-1.0.jar:target/aspectjrt-1.5.4.jar:target/jgroups-2.7.0.GA.jar:target/commons-logging-1.1.jar:target/args4j-2.0.12.jar fr.inria.zvtm.clustering.AJTestSlave -jmx
+java -Dcom.sun.management.jmxremote -Xmx2g -cp target/zvtm-cluster-proto-ajg-0.1.0-SNAPSHOT.jar:target/timingframework-1.0.jar:target/aspectjrt-1.5.4.jar:target/jgroups-2.7.0.GA.jar:target/commons-logging-1.1.jar:target/args4j-2.0.12.jar fr.inria.zvtm.clustering.AJTestSlave -jmx
 
