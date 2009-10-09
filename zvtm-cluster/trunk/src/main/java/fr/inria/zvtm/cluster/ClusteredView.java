3d0f8fe7af8125e5525279b7333fa2944fc514e9
Romain Primet <>
2009-10-09 13:38:43 +0000

    bugfix in ClusteredView
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@2477 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-cluster/trunk/src/main/java/fr/inria/zvtm/cluster/ClusteredView.java
+++ zvtm-cluster/trunk/src/main/java/fr/inria/zvtm/cluster/ClusteredView.java
@@ -6,21 +6,25 @@ import java.util.Collections;
 import java.util.List;
 
 import fr.inria.zvtm.engine.Camera;
+import fr.inria.zvtm.engine.VirtualSpaceManager;
 
-public class ClusteredView {
-	//ObjId
+public class ClusteredView implements Identifiable {
+	private final ObjId objId = ObjIdFactory.next();
 	private final int origin; //bottom-left block number
 	//width of a screen, in pixels, possibly including bezels
 	private int blockWidth;
 	private int blockHeight;
 	private final int nbRows;
 	private final int nbCols;
-	private final ArrayList<Camera> cameras = new ArrayList<Camera>();
+	private final ArrayList<Camera> cameras; 
 
 	public ClusteredView(int origin, 
 			int blockWidth, int blockHeight,
 			int nbRows, int nbCols,
 			List<Camera> cameras){
+		if(!VirtualSpaceManager.INSTANCE.isMaster()){
+			throw new IllegalStateException("Cannot create a ClusteredView on a non-master application");
+		}
 		if(origin < 0){
 		   throw new IllegalArgumentException("Blocks are 0-based naturals");
 		}
@@ -35,8 +39,10 @@ public class ClusteredView {
 		this.blockHeight = blockHeight;
 		this.nbRows = nbRows;
 		this.nbCols = nbCols;
-		Collections.copy(this.cameras, cameras);
+		this.cameras = new ArrayList<Camera>(cameras);
 	}
+
+	public ObjId getObjId(){ return objId; }
 
 	public void setBlockSize(int blockWidth, int blockHeight){
 		if((blockWidth <=0) || (blockHeight <=0)){
@@ -48,8 +54,6 @@ public class ClusteredView {
 	}
 
 	public void setBackgroundColor(Color color){
-		//TODO implement
-		throw new UnsupportedOperationException();
 	}
 }
 
