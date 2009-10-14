42cdae161b56fc4158dab59ccd164e7eb77ea4d1
Romain Primet <>
2009-10-14 12:04:04 +0000

    added event handler to ColorRectCluster example
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@2496 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-cluster/trunk/src/main/java/fr/inria/zvtm/cluster/examples/ColorRectCluster.java
+++ zvtm-cluster/trunk/src/main/java/fr/inria/zvtm/cluster/examples/ColorRectCluster.java
@@ -8,12 +8,19 @@ import org.kohsuke.args4j.Option;
 import fr.inria.zvtm.cluster.ClusteredView;
 import fr.inria.zvtm.engine.Camera;
 import fr.inria.zvtm.engine.View;
+import fr.inria.zvtm.engine.ViewEventHandler;
+import fr.inria.zvtm.engine.ViewPanel;
 import fr.inria.zvtm.engine.VirtualSpace;
 import fr.inria.zvtm.engine.VirtualSpaceManager;
+import fr.inria.zvtm.glyphs.Glyph;
 import fr.inria.zvtm.glyphs.VRectangle;
 
 import java.awt.Color;
 import java.util.Vector;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
 
 /**
  * Sample master application.
@@ -36,13 +43,13 @@ public class ColorRectCluster {
 					options.numCols, 
 					cameras);
 		cv.setBackgroundColor(Color.LIGHT_GRAY);
-
 		vsm.addClusteredView(cv);
 
 		//the view below is just a standard, non-clustered view
 		//that lets an user navigate the scene
 		View view = vsm.addFrameView(cameras, "Master View",
 			   View.STD_VIEW, 800, 600, false, true, true, null);	
+		view.setEventHandler(new ColorRectEventHandler());
 
 		long xOffset = 0;
 		long yOffset = 0;
@@ -75,6 +82,86 @@ public class ColorRectCluster {
 		}
 
 		new ColorRectCluster(options);
+	}
+
+	private class ColorRectEventHandler implements ViewEventHandler{
+		private int lastJPX;
+		private int lastJPY;
+
+		public void press1(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){}
+
+		public void release1(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){}
+
+		public void click1(ViewPanel v,int mod,int jpx,int jpy,int clickNumber, MouseEvent e){}
+
+		public void press2(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){}
+
+		public void release2(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){}
+
+		public void click2(ViewPanel v,int mod,int jpx,int jpy,int clickNumber, MouseEvent e){}
+
+		public void press3(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
+			lastJPX=jpx;
+			lastJPY=jpy;
+			v.setDrawDrag(true);
+			vsm.activeView.mouse.setSensitivity(false);
+			//because we would not be consistent  (when dragging the mouse, we computeMouseOverList, but if there is an anim triggered by {X,Y,A}speed, and if the mouse is not moving, this list is not computed - so here we choose to disable this computation when dragging the mouse with button 3 pressed)
+		}
+
+		public void release3(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
+			vsm.getAnimationManager().setXspeed(0);
+			vsm.getAnimationManager().setYspeed(0);
+			vsm.getAnimationManager().setZspeed(0);
+			v.setDrawDrag(false);
+			vsm.activeView.mouse.setSensitivity(true);
+		}
+
+		public void click3(ViewPanel v,int mod,int jpx,int jpy,int clickNumber, MouseEvent e){}
+
+		public void mouseMoved(ViewPanel v,int jpx,int jpy, MouseEvent e){}
+
+		public void mouseDragged(ViewPanel v,int mod,int buttonNumber,int jpx,int jpy, MouseEvent e){
+			if (buttonNumber == 3 || ((mod == META_MOD || mod == META_SHIFT_MOD) && buttonNumber == 1)){
+				Camera c=vsm.getActiveCamera();
+				float a=(c.focal+Math.abs(c.altitude))/c.focal;
+				if (mod == META_SHIFT_MOD) {
+					vsm.getAnimationManager().setXspeed(0);
+					vsm.getAnimationManager().setYspeed(0);
+					vsm.getAnimationManager().setZspeed((c.altitude>0) ? (long)((lastJPY-jpy)*(a/4.0f)) : (long)((lastJPY-jpy)/(a*4)));
+
+				}
+				else {
+					vsm.getAnimationManager().setXspeed((c.altitude>0) ? (long)((jpx-lastJPX)*(a/4.0f)) : (long)((jpx-lastJPX)/(a*4)));
+					vsm.getAnimationManager().setYspeed((c.altitude>0) ? (long)((lastJPY-jpy)*(a/4.0f)) : (long)((lastJPY-jpy)/(a*4)));
+				}
+			}
+		}
+
+		public void mouseWheelMoved(ViewPanel v,short wheelDirection,int jpx,int jpy, MouseWheelEvent e){}
+
+		public void enterGlyph(Glyph g){
+		}
+
+		public void exitGlyph(Glyph g){
+		}
+
+		public void Ktype(ViewPanel v,char c,int code,int mod, KeyEvent e){}
+
+		public void Kpress(ViewPanel v,char c,int code,int mod, KeyEvent e){
+		}
+
+		public void Krelease(ViewPanel v,char c,int code,int mod, KeyEvent e){}
+
+		public void viewActivated(View v){}
+
+		public void viewDeactivated(View v){}
+
+		public void viewIconified(View v){}
+
+		public void viewDeiconified(View v){}
+
+		public void viewClosing(View v){System.exit(0);}
+
 	}
 }
 
