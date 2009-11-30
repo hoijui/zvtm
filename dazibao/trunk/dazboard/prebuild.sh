d26e766431e91bbd44f75ae3f47a82f085058977
Romain Primet <>
2009-11-30 16:14:01 +0000

    added dependency to JavaOSC and prebuild script, since JavaOSC does not have a public maven repository
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@2683 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ dazibao/trunk/dazboard/prebuild.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Downloads javaOSC, unpacks it and extracts it into the
+# local maven repository (javaOSC is not managed by maven...)
+cd /tmp
+wget http://www.illposed.com/software/dls/javaosc.zip
+unzip javaosc.zip
+mvn install:install-file -DgroupId=fr.inria.zvtm -DartifactId=javaOSC -Dversion=20060402 -Dpackaging=jar -Dfile=javaosc/lib/javaosc.jar
+cd -
+
