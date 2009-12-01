31836007b8d67ad428ce440e300f242b4f004ec7
Romain Primet <>
2009-12-01 09:09:31 +0000

    fleshing out LaserEventSource (still non-functional)
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@2686 74e165f3-c697-40a6-91af-552c8936d7fe

--- dazibao/trunk/dazboard/src/main/java/fr/inria/zvtm/dazibao/LaserEventSource.java
+++ dazibao/trunk/dazboard/src/main/java/fr/inria/zvtm/dazibao/LaserEventSource.java
@@ -12,7 +12,6 @@ class LaserEventSource {
     private DazBoard app;
     private OSCPortIn receiver; //receives messages from the Vicon tracker
     public static final int LASER_OSC_PORT = 5678;
-    private Event currentEvent;
 
 	private static final int 
 		SCREEN = 2,
@@ -28,9 +27,11 @@ class LaserEventSource {
 		R_PRESS,
 		L_RELEASE,
 		R_RELEASE,
-		WHEEL,
-		R_DBL_CLICK
+		WHEEL
 	}
+
+    private static int LEFT = 0, RIGHT = 1;
+    private boolean previouslyPressed[] = new boolean[2];
 
     public LaserEventSource(DazBoard app){
         this.app = app;
@@ -49,6 +50,25 @@ class LaserEventSource {
                     System.err.println("malformed message");
                     return;
                 }
+                boolean pressedL = ((Integer)message.getArguments()[MOUSEL]).intValue() == 1;
+                boolean pressedR = ((Integer)message.getArguments()[MOUSER]).intValue() == 1;
+                int wheel = ((Integer)message.getArguments()[MOUSEW]).intValue();
+
+                Event event = Event.MOVE; 					
+				if (pressedL && !previouslyPressed[LEFT]) {
+					event = Event.L_PRESS;		
+				} else if (!pressedL && previouslyPressed[LEFT]) {
+					event = Event.L_RELEASE;		
+				} else if (pressedR && !previouslyPressed[RIGHT]) {
+					event = Event.R_PRESS;			
+				} else if (!pressedR && previouslyPressed[RIGHT]) {
+                    event = Event.R_RELEASE;
+				} else if (wheel != 0) {
+					event = Event.WHEEL;
+				}
+				
+				previouslyPressed[LEFT] = pressedL;
+				previouslyPressed[RIGHT] = pressedR;
             }
         };
         receiver.addListener("/osclaser", listener);
