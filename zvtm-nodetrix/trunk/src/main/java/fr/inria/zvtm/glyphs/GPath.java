7d99cdc5bb0e78bf776070431388c62bbd1db863
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2010-01-13 15:36:11 +0000

    new glyph: DPath with gradient color, used for extra edges
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@2758 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ zvtm-nodetrix/trunk/src/main/java/fr/inria/zvtm/glyphs/GPath.java
@@ -0,0 +1,125 @@
+/*   AUTHOR :           Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   Copyright (c) INRIA, 2010. All Rights Reserved
+ *   Licensed under the GNU LGPL. For full terms see the file COPYING.
+ *
+ * $Id$
+ */
+
+package fr.inria.zvtm.glyphs;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.awt.geom.PathIterator;
+import java.awt.geom.AffineTransform;
+import java.awt.RadialGradientPaint;
+import java.awt.geom.Point2D;
+
+public class GPath extends DPath {
+    
+    public GPath(){
+		super();
+	}
+
+	/**
+		*@param x start coordinate in virtual space
+		*@param y start coordinate in virtual space
+		*@param z z-index (pass 0 if you do not use z-ordering)
+		*@param c color
+		*/
+	public GPath(long x, long y, int z, Color c){
+	    super(x, y, z, c);
+    }
+    
+	/**
+		*@param x start coordinate in virtual space
+		*@param y start coordinate in virtual space
+		*@param z z-index (pass 0 if you do not use z-ordering)
+		*@param c color
+		*@param alpha alpha channel value in [0;1.0] 0 is fully transparent, 1 is opaque
+		*/
+	public GPath(long x, long y, int z, Color c, float alpha){
+		super(x, y, z, c, alpha);
+	}
+
+    /**
+	 *@param pi PathIterator describing this path (virtual space coordinates)
+     *@param z z-index (pass 0 if you do not use z-ordering)
+     *@param c color
+     */
+    public GPath(PathIterator pi, int z, Color c){
+        super(pi, z, c);
+    }
+    
+    /**
+	 *@param pi PathIterator describing this path (virtual space coordinates)
+     *@param z z-index (pass 0 if you do not use z-ordering)
+     *@param c color
+     *@param alpha alpha channel value in [0;1.0] 0 is fully transparent, 1 is opaque
+     */
+    public GPath(PathIterator pi, int z, Color c, float alpha){
+		super(pi, z, c, alpha);
+    }
+    
+    Color START_COLOR = new Color(118,98,252);
+    //Color END_COLOR = new Color(171,158,255);
+    Color END_COLOR = Color.WHITE;
+    Color[] GRADIENT_COLORS = {START_COLOR, END_COLOR};
+    float[] GRADIENT_DIST = {0.0f, 1.0f};
+    Point2D gradientCenter = new Point2D.Float();
+    
+    public void draw(Graphics2D g,int vW,int vH,int i,Stroke stdS,AffineTransform stdT, int dx, int dy){
+        if (alphaC != null && alphaC.getAlpha() == 0){return;}
+
+        gradientCenter.setLocation((float)elements[0].getX(i), (float)elements[0].getY(i));
+        RadialGradientPaint p = new RadialGradientPaint(gradientCenter,
+                                                        (float)Math.sqrt(Math.pow(elements[elements.length-1].getX(i)-pc[i].cx,2) + Math.pow(elements[elements.length-1].getY(i)-pc[i].cy,2)),
+                                                        GRADIENT_DIST, GRADIENT_COLORS);
+        g.setPaint(p);
+        
+        
+        if (stroke!=null) {
+            g.setStroke(stroke);
+            g.translate(dx,dy);
+            if (alphaC != null){
+                // translucent
+                g.setComposite(alphaC);
+                for (int j=0;j<elements.length;j++){
+                    if (elements[j].type == GPath.MOV){continue;}
+                    g.draw(elements[j].getShape(i));		
+                }
+                g.setComposite(acO);
+            }
+            else {
+                // opaque
+                for (int j=0;j<elements.length;j++){
+                    if (elements[j].type == GPath.MOV){continue;}
+                    g.draw(elements[j].getShape(i));		
+                }
+            }
+            g.translate(-dx,-dy);
+            g.setStroke(stdS);
+        }
+        else {
+            g.translate(dx,dy);
+            if (alphaC != null){
+                // translucent
+                g.setComposite(alphaC);
+                for (int j=0;j<elements.length;j++){
+                    if (elements[j].type == GPath.MOV){continue;}
+                    g.draw(elements[j].getShape(i));
+                }
+                g.setComposite(acO);
+            }
+            else {
+                // opaque
+                for (int j=0;j<elements.length;j++){
+                    if (elements[j].type == GPath.MOV){continue;}
+                    g.draw(elements[j].getShape(i));
+                }
+            }
+            g.translate(-dx,-dy);
+        }
+    }
+    
+}
