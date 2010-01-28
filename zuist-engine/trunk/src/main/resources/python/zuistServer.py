c294f618fd29f90274807b29f37fb216b78d78d7
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2010-01-28 13:04:30 +0000

    get any of the existing prerendered tiles
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@2840 74e165f3-c697-40a6-91af-552c8936d7fe

--- zuist-engine/trunk/src/main/resources/python/zuistServer.py
+++ zuist-engine/trunk/src/main/resources/python/zuistServer.py
@@ -1,9 +1,14 @@
+# Usage example: GET http://data.wild.lri.fr/py/zuistServer/getTile?z=9&col=322&row=224
+
 from mod_python import apache
 #http://www.modpython.org/live/current/doc-html/
 
-def getTile(req, test=""):    
-    req.log_error('handler')
-    req.content_type = 'image/png'
-    req.send_http_header()
-    req.sendfile("/tmp/image.png")
-    return apache.OK
+def getTile(req, z=-1, col=-1, row=-1):
+    try:
+        req.log_error('handler')
+        req.content_type = 'image/png'
+        req.send_http_header()
+        req.sendfile("/var/www/tiles/%s/%s/%s.png" % (z,col,row))
+        return apache.OK
+    except:
+        return apache.HTTP_NOT_FOUND
