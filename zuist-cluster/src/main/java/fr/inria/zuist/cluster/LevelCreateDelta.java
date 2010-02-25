2a69d721c39fb5068bcff9cb8ff464bd68fd13b2
Romain Primet <>
2010-02-25 13:29:26 +0000

    added xCreateDelta stubs
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@2972 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ zuist-cluster/src/main/java/fr/inria/zuist/cluster/LevelCreateDelta.java
@@ -0,0 +1,9 @@
+package fr.inria.zuist.cluster;
+
+import fr.inria.zvtm.cluster.Delta;
+import fr.inria.zvtm.cluster.SlaveUpdater;
+
+class LevelCreateDelta implements Delta {
+    public void apply(SlaveUpdater updater){}
+}
+
