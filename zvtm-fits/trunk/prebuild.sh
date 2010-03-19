e8e4b21060cd7f07e94fe8dfd7b5c2055349e0de
Romain Primet <>
2010-03-19 09:22:05 +0000

    make prebuild script fail more gracefully (i.e. check preceding return codes)
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@3085 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-fits/trunk/prebuild.sh
+++ zvtm-fits/trunk/prebuild.sh
@@ -3,8 +3,7 @@
 #Download and add the nom.tam.fits archive to the local 
 #maven repository.
 
-cd `mktemp -d`
-wget http://heasarc.gsfc.nasa.gov/docs/heasarc/fits/java/v1.0/v1.04.0/fits.jar
-mvn install:install-file -DgroupId=nom.tam.fits -DartifactId=fits -Dversion=1.04.0 -Dpackaging=jar -Dfile=fits.jar
-cd -
+cd `mktemp -d` &&\
+wget http://heasarc.gsfc.nasa.gov/docs/heasarc/fits/java/v1.0/v1.04.0/fits.jar &&\
+mvn install:install-file -DgroupId=nom.tam.fits -DartifactId=fits -Dversion=1.04.0 -Dpackaging=jar -Dfile=fits.jar 
 
