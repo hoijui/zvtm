04bae03f5902b5d66c9348ad1a0bdfa8b2de5adf
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2010-03-19 08:38:45 +0000

    - now generating levels - fixed cmd line arg parsing
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@3083 74e165f3-c697-40a6-91af-552c8936d7fe

--- zuist-engine/trunk/src/main/resources/python/paris26GP.py
+++ zuist-engine/trunk/src/main/resources/python/paris26GP.py
@@ -8,7 +8,6 @@
 # $Id$
 
 import os, sys, math
-from copy import copy
 # http://effbot.org/zone/element-index.htm
 import elementtree.ElementTree as ET
 
@@ -21,38 +20,51 @@ CMD_LINE_HELP = "ZUIST Paris 26GP Scene Script\n\nUsage:\n\n" + \
 TRACE_LEVEL = 1
 
 TILE_SIZE = 1024
+SCENE_DEPTH = 8
+
+# camera focal distance
+F = 100.0
+# camera max altitude
+MAX_ALT = 1000000
 
 ################################################################################
 # Create tiles and ZUIST XML scene from source image
 ################################################################################
 def processSrc():
     # source image
-    outputSceneFile = TGT_PATH
+    outputSceneFile = "%s/scene.xml" % SRC_TGT_DIR
     outputroot = ET.Element("scene")
-    x = 0
-    y = 0
-#    while y < IMG_W:
-#        x = 0
-#        while x < IMG_H:
-#            include = ET.SubElement(outputroot, "include")
-#            include.set("src", "tile-%d-%d/scene.xml" % (x/TILE_SIZE, y/TILE_SIZE))
-#            include.set("x", "%d" % x)
-#            include.set("y", "%d" % y)
-
-    for i in range(9):
-        x = 0
-        for j in range(22):
-            include = ET.SubElement(outputroot, "include")
-            include.set("src", "r%04dc%04d/scene.xml" % (i,j))
-            include.set("x", "%d" % x)
-            include.set("y", "%d" % y)
-            
-            x += TILE_SIZE
-        y -= TILE_SIZE
+    generateLevels(outputroot, SCENE_DEPTH)
+    generateTilePyramid(outputroot, SCENE_DEPTH)
     log("Writing scene file %s" % outputSceneFile, 1)
     tree = ET.ElementTree(outputroot)
     tree.write(outputSceneFile, encoding='utf-8')
 
+
+################################################################################
+# Generate ZUIST levels
+################################################################################
+def generateLevels(parentEL, levelCount):
+    altitudes = [0,]
+    for i in range(levelCount):
+        depth = int(levelCount-i-1)
+        altitudes.append(int(F*math.pow(2,i+1)-F))
+        if i == levelCount-1:
+            ceiling = MAX_ALT
+        else:
+            ceiling = altitudes[-1]
+        floor = altitudes[-2]
+        levelEL = ET.SubElement(parentEL, "level")
+        levelEL.set("depth", "%d" % depth)
+        levelEL.set("ceiling", "%d" % ceiling)
+        levelEL.set("floor", "%d" % floor)
+
+################################################################################
+# Generate tile pyramid
+################################################################################
+def generateTilePyramid(parentEL, levelCount):
+    return
+            
 ################################################################################
 # Trace exec on std output
 ################################################################################
@@ -64,19 +76,21 @@ def log(msg, level=0):
 # main
 ################################################################################
 if len(sys.argv) > 1:
-    SRC_TGT_PATH = os.path.realpath(sys.argv[1])
-    if len(sys.argv) > 3:
-        for arg in sys.argv[3:]:
-            if arg.startswith("-ts="):
-                TILE_SIZE = int(arg[len("-ts="):])
-            elif arg.startswith("-tl="):
-                TRACE_LEVEL = int(arg[len("-tl="):])
-else:
-    log(CMD_LINE_HELP)
-    sys.exit(0)
-
+    for arg in sys.argv[1:]:
+        if arg.startswith("-ts="):
+            TILE_SIZE = int(arg[len("-ts="):])
+            if TILE_SIZE == 256:
+                SCENE_DEPTH = 9
+        elif arg.startswith("-tl="):
+            TRACE_LEVEL = int(arg[len("-tl="):])
+        elif arg.startswith("-h"):
+            log(CMD_LINE_HELP)
+            sys.exit(0)
+        else:
+            SRC_TGT_DIR = os.path.realpath(arg)
+            
 log("--------------------")
-log("Saving to %s" % SRC_TGT_PATH, 1)
+log("Saving to %s" % SRC_TGT_DIR, 1)
 log("Tile Size: %dx%d" % (TILE_SIZE, TILE_SIZE), 1)
-#processSrc()
+processSrc()
 log("--------------------")
