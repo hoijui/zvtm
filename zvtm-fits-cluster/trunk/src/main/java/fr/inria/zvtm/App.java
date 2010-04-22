5aa09426c6b0f518b51190fcadf38ac3735aa52f
Romain Primet <>
2010-04-22 14:31:24 +0000

    added stub project zvtm-fits-cluster
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@3157 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ zvtm-fits-cluster/trunk/src/main/java/fr/inria/zvtm/App.java
@@ -0,0 +1,13 @@
+package fr.inria.zvtm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
