82502fdf1131365662d5e7cc2dfad083751ed16e
Romain Primet <>
2010-05-04 11:38:12 +0000

    fixed tile_simu
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@3184 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-fits/trunk/tile_simu.sh
+++ zvtm-fits/trunk/tile_simu.sh
@@ -6,6 +6,6 @@ for i in {0..7}
 do
     for j in {0..7}
     do
-        fitscopy SIMU_8k_8k.fits[`expr $i \* 1024 + 1`:`expr \( $i + 1 \) \* 1024`,`expr $j \* 1024 + 1`:`expr \( $j + 1 \) \* 1024`] tiles/SIMU_tiles_"$i"_"$j".fits
+        fitscopy SIMU_8k_8k.fits.bak[`expr $i \* 1024 + 1`:`expr \( $i + 1 \) \* 1024`,`expr $j \* 1024 + 1`:`expr \( $j + 1 \) \* 1024`] tiles/SIMU_tiles_"$i"_"$j".fits
     done
 done
