0388b2b4907caf1b9d92652f9d9f3d7ee88d64ca
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2010-05-10 10:12:57 +0000

    release 0.1.0
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@3197 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ zvtm-pdf/tags/0.1.0/trunk/src/main/java/fr/inria/zvtm/demo/PDFViewer.java
@@ -0,0 +1,228 @@
+/*   AUTHOR :            Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   Copyright (c) INRIA, 2008-2009. All Rights Reserved
+ *   Licensed under the GNU LGPL. For full terms see the file COPYING.
+ *
+ * $Id$
+ */
+
+package fr.inria.zvtm.demo;
+
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+import java.util.Vector;
+
+import fr.inria.zvtm.engine.VirtualSpaceManager;
+import fr.inria.zvtm.engine.VirtualSpace;
+import fr.inria.zvtm.engine.View;
+import fr.inria.zvtm.engine.ViewPanel;
+import fr.inria.zvtm.engine.Camera;
+import fr.inria.zvtm.glyphs.Glyph;
+import fr.inria.zvtm.engine.ViewEventHandler;
+import fr.inria.zvtm.glyphs.ZPDFPageImg;
+import fr.inria.zvtm.glyphs.ZPDFPageG2D;
+
+import com.sun.pdfview.PDFFile;
+import com.sun.pdfview.PDFPage;
+
+public class PDFViewer {
+	
+	static final int NAV_ANIM_DURATION = 300;
+	
+	static final short DIRECT_G2D_RENDERING = 0;
+	static final short OFFSCREEN_IMAGE_RENDERING = 1;
+	short rendering_technique = OFFSCREEN_IMAGE_RENDERING;
+
+	VirtualSpace vs;
+	static final String spaceName = "pdfSpace";
+	ViewEventHandler eh;
+	Camera mCamera;
+
+	View pdfView;
+
+	PDFViewer(String pdfFilePath, float df){
+		VirtualSpaceManager.INSTANCE.setDebug(true);
+		initGUI();
+		load(new File(pdfFilePath), df);
+	}
+
+	public void initGUI(){
+		vs = VirtualSpaceManager.INSTANCE.addVirtualSpace(spaceName);
+		mCamera = vs.addCamera();
+		Vector cameras = new Vector();
+		cameras.add(mCamera);
+		pdfView = VirtualSpaceManager.INSTANCE.addFrameView(cameras, "ZVTM PDF Viewer", View.STD_VIEW, 800, 600, false, true, true, null);
+		pdfView.setBackgroundColor(Color.WHITE);
+		eh = new PDFViewerEventHandler(this);
+		pdfView.setEventHandler(eh);
+		pdfView.setAntialiasing(true);
+		mCamera.setAltitude(0);
+		VirtualSpaceManager.INSTANCE.repaintNow();
+	}
+
+	void load(File f, float detailFactor){
+		try {
+			RandomAccessFile raf = new RandomAccessFile(f, "r");
+			FileChannel channel = raf.getChannel();
+			ByteBuffer buf = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
+			PDFFile pdfFile = new PDFFile(buf);
+			int page_width = (int)pdfFile.getPage(0).getBBox().getWidth();
+			for (int i=0;i<pdfFile.getNumPages();i++){
+				try {
+				    if (rendering_technique == DIRECT_G2D_RENDERING){
+				        vs.addGlyph(new ZPDFPageG2D(i*Math.round(page_width*1.1f), i*Math.round(page_width*1.1f), 0, pdfFile.getPage(i+1), detailFactor));
+				    }
+				    else {
+				        // OFFSCREEN_IMAGE_RENDERING
+				        vs.addGlyph(new ZPDFPageImg(i*Math.round(page_width*1.1f*detailFactor), i*Math.round(page_width*1.1f*detailFactor), 0, pdfFile.getPage(i+1), detailFactor, 1));
+				    }
+				}
+				catch ( Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		catch ( Exception e) { 
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args){
+		System.out.println("-----------------");
+		System.out.println("General information");
+		System.out.println("JVM version: "+System.getProperty("java.vm.vendor")+" "+System.getProperty("java.vm.name")+" "+System.getProperty("java.vm.version"));
+		System.out.println("OS type: "+System.getProperty("os.name")+" "+System.getProperty("os.version")+"/"+System.getProperty("os.arch")+" "+System.getProperty("sun.cpu.isalist"));
+		System.out.println("-----------------");
+		System.out.println("Directory information");
+		System.out.println("Java Classpath: "+System.getProperty("java.class.path"));	
+		System.out.println("Java directory: "+System.getProperty("java.home"));
+		System.out.println("Launching from: "+System.getProperty("user.dir"));
+		System.out.println("-----------------");
+		System.out.println("User informations");
+		System.out.println("User name: "+System.getProperty("user.name"));
+		System.out.println("User home directory: "+System.getProperty("user.home"));
+		System.out.println("-----------------");
+		new PDFViewer((args.length > 0) ? args[0] : null, (args.length > 1) ? Float.parseFloat(args[1]) : 1);
+	}
+
+}
+
+class PDFViewerEventHandler implements ViewEventHandler {
+
+	PDFViewer application;
+
+	long lastJPX,lastJPY;
+
+	PDFViewerEventHandler(PDFViewer appli){
+		application = appli;
+	}
+
+	boolean dragging = false;
+
+	public void press1(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
+		lastJPX=jpx;
+		lastJPY=jpy;
+		v.setDrawDrag(true);
+		VirtualSpaceManager.INSTANCE.activeView.mouse.setSensitivity(false);
+	}
+
+	public void release1(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
+		VirtualSpaceManager.INSTANCE.getAnimationManager().setXspeed(0);
+        VirtualSpaceManager.INSTANCE.getAnimationManager().setYspeed(0);
+        VirtualSpaceManager.INSTANCE.getAnimationManager().setZspeed(0);
+		v.setDrawDrag(false);
+		VirtualSpaceManager.INSTANCE.activeView.mouse.setSensitivity(true);
+	}
+
+	public void click1(ViewPanel v,int mod,int jpx,int jpy,int clickNumber, MouseEvent e){
+		Glyph g = v.lastGlyphEntered();
+		if (g != null){
+			application.pdfView.centerOnGlyph(g, application.mCamera, PDFViewer.NAV_ANIM_DURATION);
+		}
+		else {
+			application.pdfView.getGlobalView(application.mCamera, PDFViewer.NAV_ANIM_DURATION);
+		}
+	}
+
+	public void press2(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
+
+	}
+
+	public void release2(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
+	}
+
+	public void click2(ViewPanel v,int mod,int jpx,int jpy,int clickNumber, MouseEvent e){
+	}
+
+	public void press3(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
+	}
+
+	public void release3(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
+	}
+
+	public void click3(ViewPanel v,int mod,int jpx,int jpy,int clickNumber, MouseEvent e){}
+
+	public void mouseMoved(ViewPanel v,int jpx,int jpy, MouseEvent e){}
+
+	public void mouseDragged(ViewPanel v,int mod,int buttonNumber,int jpx,int jpy, MouseEvent e){
+		if (buttonNumber == 1){
+			Camera c = VirtualSpaceManager.INSTANCE.getActiveCamera();
+			float a = (c.focal+Math.abs(c.altitude))/c.focal;
+			if (mod == SHIFT_MOD) {
+			    VirtualSpaceManager.INSTANCE.getAnimationManager().setXspeed(0);
+                VirtualSpaceManager.INSTANCE.getAnimationManager().setYspeed(0);
+                VirtualSpaceManager.INSTANCE.getAnimationManager().setZspeed((c.altitude>0) ? (long)((lastJPY-jpy)*(a/50.0f)) : (long)((lastJPY-jpy)/(a*50)));
+				//50 is just a speed factor (too fast otherwise)
+			}
+			else {
+			    VirtualSpaceManager.INSTANCE.getAnimationManager().setXspeed((c.altitude>0) ? (long)((jpx-lastJPX)*(a/50.0f)) : (long)((jpx-lastJPX)/(a*50)));
+                VirtualSpaceManager.INSTANCE.getAnimationManager().setYspeed((c.altitude>0) ? (long)((lastJPY-jpy)*(a/50.0f)) : (long)((lastJPY-jpy)/(a*50)));
+                VirtualSpaceManager.INSTANCE.getAnimationManager().setZspeed(0);
+			}
+		}
+	}
+
+	public void mouseWheelMoved(ViewPanel v,short wheelDirection,int jpx,int jpy, MouseWheelEvent e){
+		Camera c = VirtualSpaceManager.INSTANCE.getActiveCamera();
+		float a = (c.focal+Math.abs(c.altitude))/c.focal;
+		if (wheelDirection == WHEEL_UP){
+			c.altitudeOffset(-a*5);
+			VirtualSpaceManager.INSTANCE.repaintNow();
+		}
+		else {
+			//wheelDirection == WHEEL_DOWN
+			c.altitudeOffset(a*5);
+			VirtualSpaceManager.INSTANCE.repaintNow();
+		}
+	}
+
+	public void enterGlyph(Glyph g){
+	}
+
+	public void exitGlyph(Glyph g){
+	}
+
+	public void Ktype(ViewPanel v,char c,int code,int mod, KeyEvent e){}
+
+	public void Kpress(ViewPanel v,char c,int code,int mod, KeyEvent e){}
+
+	public void Krelease(ViewPanel v,char c,int code,int mod, KeyEvent e){}
+
+	public void viewActivated(View v){}
+
+	public void viewDeactivated(View v){}
+
+	public void viewIconified(View v){}
+
+	public void viewDeiconified(View v){}
+
+	public void viewClosing(View v){System.exit(0);}
+
+}
---
+++ zvtm-pdf/tags/0.1.0/trunk/src/main/java/fr/inria/zvtm/glyphs/ZPDFPageImg.java
@@ -0,0 +1,335 @@
+/*   AUTHOR :            Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   Copyright (c) INRIA, 2008-2009. All Rights Reserved
+ *   Licensed under the GNU LGPL. For full terms see the file COPYING.
+ *
+ * $Id$
+ */
+
+package fr.inria.zvtm.glyphs;
+
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+
+import fr.inria.zvtm.engine.VirtualSpaceManager;
+import fr.inria.zvtm.engine.Camera;
+import fr.inria.zvtm.engine.LongPoint;
+import fr.inria.zvtm.glyphs.Glyph;
+import fr.inria.zvtm.glyphs.ClosedShape;
+import fr.inria.zvtm.glyphs.VImage;
+import fr.inria.zvtm.glyphs.RectangularShape;
+import fr.inria.zvtm.glyphs.projection.RProjectedCoordsP;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.Stroke;
+import java.awt.geom.AffineTransform;
+
+import com.sun.pdfview.PDFPage;
+
+/** Glyph encapsulating a PDFPage from <a href="https://pdf-renderer.dev.java.net/">SwingLabs' PDFRenderer</a>.
+ *@author Emmanuel Pietriga
+ */
+
+public class ZPDFPageImg extends ZPDFPage {
+
+	Image pageImage;
+
+    /** For internal use. Made public for easier outside package subclassing. */
+    public float trueCoef = 1.0f;
+
+    /** For internal use. Made public for easier outside package subclassing. */
+    public AffineTransform at;
+    
+	/** Instantiate a PDF page as a ZVTM glyph, rendered at a resolution that matches the default scale for that page.
+	 *@param page the PDF page from pdf-renderer
+	 */
+	public ZPDFPageImg(long x, long y, int z, PDFPage page){
+		vx = x;
+		vy = y;
+		vz = z;
+		// get the width and height for the doc at the default zoom 
+		Rectangle rect = new Rectangle(0, 0, (int)page.getBBox().getWidth(), (int)page.getBBox().getHeight());
+		// generate the image
+		pageImage = page.getImage(rect.width, rect.height,
+		 	rect,   // clip rect
+			null,   // null for the ImageObserver
+			true,   // fill background with white
+			true);  // block until drawing is done
+		vw = Math.round(rect.width/2.0);
+		vh = Math.round(rect.height/2.0);
+		if (vw==0 && vh==0){ar = 1.0f;}
+		else {ar = (float)vw/(float)vh;}
+		computeSize();
+		orient = 0;
+	}
+
+	/** Instantiate a PDF page as a ZVTM glyph, rendered at a resolution that matches the default scale for that page.
+	 *@param page the PDF page from pdf-renderer
+	 *@param detailFactor multiplication factor applied to compute the actual width and height of the bitmap image in which to render the page, taking the default rendering scale as a basis
+     *@param scale scaleFactor in virtual space w.r.t specified image size (default is 1.0)
+	 */
+	public ZPDFPageImg(long x, long y, int z, PDFPage page, float detailFactor, double scale){
+		vx = x;
+		vy = y;
+		vz = z;
+		// get the width and height for the doc at the default zoom 
+		Rectangle rect = new Rectangle(0, 0, (int)(page.getBBox().getWidth()), (int)(page.getBBox().getHeight()));
+		// generate the image
+		pageImage = page.getImage((int)(detailFactor*rect.width), (int)(detailFactor*rect.height),
+		 	rect,   // clip rect
+			null,   // null for the ImageObserver
+			true,   // fill background with white
+			true);  // block until drawing is done
+		vw = Math.round(detailFactor*rect.width*scale/2.0);
+		vh = Math.round(detailFactor*rect.height*scale/2.0);
+		if (vw==0 && vh==0){ar = 1.0f;}
+		else {ar = (float)vw/(float)vh;}
+		computeSize();
+		orient = 0;		
+		scaleFactor = (float)scale;
+	}
+
+	/** Instantiate a PDF page as a ZVTM glyph, rendered at a resolution that matches the default scale for that page.
+	 *@param page the PDF page from pdf-renderer
+	 *@param w width of bitmap image in which to render the page
+	 *@param h height of bitmap image in which to render the page
+     *@param scale scaleFactor in virtual space w.r.t specified image size (default is 1.0)
+	 */
+	public ZPDFPageImg(long x, long y, int z, PDFPage page, int w, int h, double scale){
+		vx = x;
+		vy = y;
+		vz = z;
+		// get the width and height for the doc at the default zoom
+		//XXX:TBW would probably be a good idea to throw some kind of warning if 
+		//        w and h are smaller than default scale ? not sure... depends on how page.getImage() behaves in this case
+		Rectangle rect = new Rectangle(0, 0, w, h);
+		// generate the image
+		pageImage = page.getImage(rect.width, rect.height,
+		 	rect,   // clip rect
+			null,   // null for the ImageObserver
+			true,   // fill background with white
+			true);  // block until drawing is done
+		vw = Math.round(rect.width*scale/2.0);
+		vh = Math.round(rect.height*scale/2.0);
+		if (vw==0 && vh==0){ar = 1.0f;}
+		else {ar = (float)vw/(float)vh;}
+		computeSize();
+		orient = 0;		
+		scaleFactor = (float)scale;		
+	}
+	
+	/** For internal use. Made public for easier outside package subclassing. */
+    public Object interpolationMethod = RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR;
+    
+    /** Specify how image should be interpolated when drawn at a scale different from its original scale.
+        *@param im one of java.awt.RenderingHints.{VALUE_INTERPOLATION_NEAREST_NEIGHBOR,VALUE_INTERPOLATION_BILINEAR,VALUE_INTERPOLATION_BICUBIC} ; default is VALUE_INTERPOLATION_NEAREST_NEIGHBOR
+        */
+    public void setInterpolationMethod(Object im){
+        interpolationMethod = im;
+    }
+    
+    /** Get information about how image should be interpolated when drawn at a scale different from its original scale.
+        *@return one of java.awt.RenderingHints.{VALUE_INTERPOLATION_NEAREST_NEIGHBOR,VALUE_INTERPOLATION_BILINEAR,VALUE_INTERPOLATION_BICUBIC} ; default is VALUE_INTERPOLATION_NEAREST_NEIGHBOR
+        */
+    public Object getInterpolationMethod(){
+        return interpolationMethod;
+    }
+
+	public void draw(Graphics2D g,int vW,int vH,int i,Stroke stdS,AffineTransform stdT, int dx, int dy){
+	    if (alphaC != null && alphaC.getAlpha()==0){return;}
+		if ((pc[i].cw>1) && (pc[i].ch>1)){
+			if (zoomSensitive){
+				trueCoef = scaleFactor*coef;
+			}
+			else{
+				trueCoef = scaleFactor;
+			}
+			//a threshold greater than 0.01 causes jolts when zooming-unzooming around the 1.0 scale region
+			if (Math.abs(trueCoef-1.0f)<0.01f){trueCoef=1.0f;}
+			if (trueCoef!=1.0f){
+				// translate
+				at = AffineTransform.getTranslateInstance(dx+pc[i].cx-pc[i].cw, dy+pc[i].cy-pc[i].ch);
+				g.setTransform(at);
+				if (alphaC != null){
+                    // translucent
+                    g.setComposite(alphaC);
+    				// rescale and draw				
+    				if (interpolationMethod != RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR){
+                        g.setRenderingHint(java.awt.RenderingHints.KEY_INTERPOLATION, interpolationMethod);
+                        g.drawImage(pageImage,AffineTransform.getScaleInstance(trueCoef,trueCoef),null);
+                        g.setRenderingHint(java.awt.RenderingHints.KEY_INTERPOLATION, java.awt.RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+                    }
+                    else {
+                        g.drawImage(pageImage,AffineTransform.getScaleInstance(trueCoef,trueCoef),null);
+                    }
+    				g.setTransform(stdT);
+                    if ((drawBorder==1 && pc[i].prevMouseIn) || drawBorder==2){
+                        g.setColor(borderColor);
+                        g.drawRect(dx+pc[i].cx-pc[i].cw, dy+pc[i].cy-pc[i].ch, 2*pc[i].cw-1, 2*pc[i].ch-1);
+                    }
+                    g.setComposite(acO);
+                }
+                else {
+                    // opaque
+    				// rescale and draw				
+    				if (interpolationMethod != RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR){
+                        g.setRenderingHint(java.awt.RenderingHints.KEY_INTERPOLATION, interpolationMethod);
+                        g.drawImage(pageImage,AffineTransform.getScaleInstance(trueCoef,trueCoef),null);
+                        g.setRenderingHint(java.awt.RenderingHints.KEY_INTERPOLATION, java.awt.RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+                    }
+                    else {
+                        g.drawImage(pageImage,AffineTransform.getScaleInstance(trueCoef,trueCoef),null);
+                    }
+    				g.setTransform(stdT);
+                    if ((drawBorder==1 && pc[i].prevMouseIn) || drawBorder==2){
+                        g.setColor(borderColor);
+                        g.drawRect(dx+pc[i].cx-pc[i].cw, dy+pc[i].cy-pc[i].ch, 2*pc[i].cw-1, 2*pc[i].ch-1);
+                    }
+                }
+            }
+			else {
+			    if (alphaC != null){
+                    // translucent
+                    g.setComposite(alphaC);
+    			    if (interpolationMethod != RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR){
+                        g.setRenderingHint(java.awt.RenderingHints.KEY_INTERPOLATION, interpolationMethod);
+                        g.drawImage(pageImage, dx+pc[i].cx-pc[i].cw, dy+pc[i].cy-pc[i].ch, null);
+                        g.setRenderingHint(java.awt.RenderingHints.KEY_INTERPOLATION, java.awt.RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+                    }
+                    else {
+                        g.drawImage(pageImage, dx+pc[i].cx-pc[i].cw, dy+pc[i].cy-pc[i].ch, null);
+                    }
+    				if ((drawBorder == 1 && pc[i].prevMouseIn) || drawBorder == 2){
+    					g.setColor(borderColor);
+    					g.drawRect(dx+pc[i].cx-pc[i].cw,dy+pc[i].cy-pc[i].ch,2*pc[i].cw-1,2*pc[i].ch-1);
+    				}
+                    g.setComposite(acO);
+                }
+                else {
+                    // opaque
+    			    if (interpolationMethod != RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR){
+                        g.setRenderingHint(java.awt.RenderingHints.KEY_INTERPOLATION, interpolationMethod);
+                        g.drawImage(pageImage, dx+pc[i].cx-pc[i].cw, dy+pc[i].cy-pc[i].ch, null);
+                        g.setRenderingHint(java.awt.RenderingHints.KEY_INTERPOLATION, java.awt.RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+                    }
+                    else {
+                        g.drawImage(pageImage, dx+pc[i].cx-pc[i].cw, dy+pc[i].cy-pc[i].ch, null);
+                    }
+    				if ((drawBorder == 1 && pc[i].prevMouseIn) || drawBorder == 2){
+    					g.setColor(borderColor);
+    					g.drawRect(dx+pc[i].cx-pc[i].cw,dy+pc[i].cy-pc[i].ch,2*pc[i].cw-1,2*pc[i].ch-1);
+    				}
+                }
+			}
+		}
+		else {
+			g.setColor(this.borderColor);
+			g.fillRect(dx+pc[i].cx,dy+pc[i].cy,1,1);
+		}
+	}
+
+	public void drawForLens(Graphics2D g,int vW,int vH,int i,Stroke stdS,AffineTransform stdT, int dx, int dy){
+		if ((pc[i].lcw > 1) && (pc[i].lch > 1)){
+			if (zoomSensitive){trueCoef=scaleFactor*coef;}
+			else {trueCoef=scaleFactor;}
+			if (Math.abs(trueCoef-1.0f)<0.01f){
+				//a threshold greater than 0.01 causes jolts when zooming-unzooming around the 1.0 scale region
+				trueCoef=1.0f;
+			}
+			if (trueCoef!=1.0f){
+				g.setTransform(AffineTransform.getTranslateInstance(dx+pc[i].lcx-pc[i].lcw, dy+pc[i].lcy-pc[i].lch));
+				
+				if (alphaC != null){
+                    // translucent
+                    g.setComposite(alphaC);
+                	if (interpolationMethod != RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR){
+                        g.setRenderingHint(java.awt.RenderingHints.KEY_INTERPOLATION, interpolationMethod);
+                        g.drawImage(pageImage, AffineTransform.getScaleInstance(trueCoef,trueCoef), null);
+                        g.setRenderingHint(java.awt.RenderingHints.KEY_INTERPOLATION, java.awt.RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+                    }
+                    else {
+                        g.drawImage(pageImage, AffineTransform.getScaleInstance(trueCoef,trueCoef), null);
+                    }
+    				g.setTransform(stdT);
+    				if ((drawBorder==1 && pc[i].prevMouseIn) || drawBorder==2){
+    					g.setColor(borderColor);
+    					g.drawRect(dx+pc[i].lcx-pc[i].lcw, dy+pc[i].lcy-pc[i].lch, 2*pc[i].lcw-1, 2*pc[i].lch-1);
+    				}
+                    g.setComposite(acO);
+                }
+                else {
+                 	if (interpolationMethod != RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR){
+                        g.setRenderingHint(java.awt.RenderingHints.KEY_INTERPOLATION, interpolationMethod);
+                        g.drawImage(pageImage, AffineTransform.getScaleInstance(trueCoef,trueCoef), null);
+                        g.setRenderingHint(java.awt.RenderingHints.KEY_INTERPOLATION, java.awt.RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+                    }
+                    else {
+                        g.drawImage(pageImage, AffineTransform.getScaleInstance(trueCoef,trueCoef), null);
+                    }
+    				g.setTransform(stdT);
+    				if ((drawBorder==1 && pc[i].prevMouseIn) || drawBorder==2){
+    					g.setColor(borderColor);
+    					g.drawRect(dx+pc[i].lcx-pc[i].lcw, dy+pc[i].lcy-pc[i].lch, 2*pc[i].lcw-1, 2*pc[i].lch-1);
+    				}
+                }				
+			}
+			else {
+			    if (alphaC != null){
+                    // translucent
+                    g.setComposite(alphaC);
+                    if (interpolationMethod != RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR){
+                        g.setRenderingHint(java.awt.RenderingHints.KEY_INTERPOLATION, interpolationMethod);
+        				g.drawImage(pageImage, dx+pc[i].lcx-pc[i].lcw, dy+pc[i].lcy-pc[i].lch, null);
+                        g.setRenderingHint(java.awt.RenderingHints.KEY_INTERPOLATION, java.awt.RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+                    }
+                    else {
+        				g.drawImage(pageImage, dx+pc[i].lcx-pc[i].lcw, dy+pc[i].lcy-pc[i].lch, null);
+                    }
+    				if ((drawBorder == 1 && pc[i].prevMouseIn) || drawBorder == 2){
+    					g.setColor(borderColor);
+    					g.drawRect(dx+pc[i].lcx-pc[i].lcw, dy+pc[i].lcy-pc[i].lch, 2*pc[i].lcw-1, 2*pc[i].lch-1);
+    				}
+                    g.setComposite(acO);
+                }
+                else {
+    			    if (interpolationMethod != RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR){
+                        g.setRenderingHint(java.awt.RenderingHints.KEY_INTERPOLATION, interpolationMethod);
+        				g.drawImage(pageImage, dx+pc[i].lcx-pc[i].lcw, dy+pc[i].lcy-pc[i].lch, null);
+                        g.setRenderingHint(java.awt.RenderingHints.KEY_INTERPOLATION, java.awt.RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+                    }
+                    else {
+        				g.drawImage(pageImage, dx+pc[i].lcx-pc[i].lcw, dy+pc[i].lcy-pc[i].lch, null);
+                    }
+    				if ((drawBorder == 1 && pc[i].prevMouseIn) || drawBorder == 2){
+    					g.setColor(borderColor);
+    					g.drawRect(dx+pc[i].lcx-pc[i].lcw, dy+pc[i].lcy-pc[i].lch, 2*pc[i].lcw-1, 2*pc[i].lch-1);
+    				}                    
+                }
+			}
+		}
+		else {
+			g.setColor(this.borderColor);
+			g.fillRect(dx+pc[i].lcx,dy+pc[i].lcy,1,1);
+		}
+	}
+
+	public Object clone(){
+		return null;
+	}
+
+	public void flush(){
+	    if (pageImage != null){
+	        pageImage.flush();
+	        pageImage = null;
+	    }
+	}
+	
+	public Image getpageImage() {
+		
+		return pageImage;
+	}
+}
