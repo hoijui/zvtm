a27624111d58f51480bc439a2dfbdba4f3b8180c
Romain Primet <>
2010-05-12 07:50:44 +0000

    drafting tiler
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@3216 74e165f3-c697-40a6-91af-552c8936d7fe

--- zuist-cluster-fits/trunk/src/main/resources/tiler.py
+++ zuist-cluster-fits/trunk/src/main/resources/tiler.py
@@ -27,22 +27,23 @@ def tile_image(filename, image_width, image_height, tile_width, tile_height, lev
 
     for i in range(levels):
         print "<!-- starting level %d -->" % i
-        tile_level(filename, image_width, image_height, tile_width, tile_height, 2**i)
+        tile_level(filename, image_width, image_height, tile_width, tile_height, i)
 
-def print_zuist_resource_info(tile_width, tile_height):
+def print_zuist_resource_info(tile_width, tile_height, level):
     """
     Prints the relevant <region> and <resource> sections.
     """
-    print "<region h=\"%d\" w=\"%d\" id=\"%s\" layer=\"%s\" levels=\"%d\" w=\"%d\" x=\"%d\" y=\"%d\">" % (tile_height, tile_width, "id", "layer", 0, 0, 0, 0)
-    print "<resource h=\"%d\" w=\"%d\" id=\"%s\" src=\"%s\" type=\"fits\"  x=\"%d\" y=\"%d\" z-index=\"0\"/>" % (tile_height, tile_width, "id", "src", 0, 0)
+    print "<region h=\"%d\" w=\"%d\" id=\"%s\" layer=\"%s\" levels=\"%d\" x=\"%d\" y=\"%d\">" % (tile_height, tile_width, "id", "imageLayer", level, 0, 0)
+    print "<resource h=\"%d\" w=\"%d\" id=\"%s\" src=\"%s\" type=\"fits\" x=\"%d\" y=\"%d\" z-index=\"0\"/>" % (tile_height, tile_width, "id", "src", 0, 0)
     print "</region>"
 
-def tile_level(filename, image_width, image_height, tile_width, tile_height, skip_factor):
+def tile_level(filename, image_width, image_height, tile_width, tile_height, level):
    """
    skip_factor should be a power of two (sf = 2^level), level 0
    has the resolution of the original image, level 1 has one fourth etc.
    called internally by tile_image. not for public consumption.
    """
+   skip_factor = 2**level
    xcount = image_width / (tile_width * skip_factor) 
    ycount = image_height / (tile_height * skip_factor)
    
@@ -50,7 +51,7 @@ def tile_level(filename, image_width, image_height, tile_width, tile_height, ski
        for j in range(ycount):
            print "echo fitscopy %s[%d:%d:%d,%d:%d:%d]" % (filename, i * tile_width * skip_factor + 1, (i + 1) * tile_width * skip_factor, skip_factor, j * tile_height * skip_factor + 1, (j + 1) * tile_height * skip_factor, skip_factor)
            #subprocess.Popen("echo fitscopy %s[%d:%d:%d,%d:%d:%d]" % (filename, i * tile_width * skip_factor + 1, (i + 1) * tile_width * skip_factor, skip_factor, j * tile_height * skip_factor + 1, (j + 1) * tile_height * skip_factor, skip_factor))
-           print_zuist_resource_info()
+           print_zuist_resource_info(tile_width, tile_height, level)
 
 if __name__ == "__main__":
    tile_image("foo", 8192, 8192, 1024, 1024, 3)
