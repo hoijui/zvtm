df8fc3fcf5d1ac052db6c45cba8893f88d47c8ee
Romain Primet <>
2010-05-17 08:26:31 +0000

    added scale factor support to zuist FITS image descriptions
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@3235 74e165f3-c697-40a6-91af-552c8936d7fe

--- zuist-cluster-fits/trunk/src/main/java/fr/inria/zuist/engine/FitsResourceHandler.java
+++ zuist-cluster-fits/trunk/src/main/java/fr/inria/zuist/engine/FitsResourceHandler.java
@@ -3,18 +3,36 @@ package fr.inria.zuist.engine;
 import java.net.URL;
 import java.awt.Color;
 import fr.inria.zvtm.glyphs.FitsImage;
+import fr.inria.zuist.engine.SceneManager;
 
 public class FitsResourceHandler implements ResourceHandler {
     public static final String RESOURCE_TYPE_FITS = "fits";
+    private static final String SC_ID = "sc="; //scale factor in params
+    private static final String SM_ID = "sm="; //scale method in params
 
     public ResourceDescription createResourceDescription(
             long x, long y, String id, int zindex, Region region, 
             URL resourceURL, boolean sensitivity, Color stroke, String params){
-        //TODO scaleFactor (hardcoded to 1 at the moment)
         //TODO scaleMethod (hardcoded to ASINH)
+        float scaleFactor = 1;
+        if (params != null){
+            String[] paramTokens = params.split(SceneManager.PARAM_SEPARATOR);
+            for (int i=0;i<paramTokens.length;i++) {
+                if (paramTokens[i].startsWith(SC_ID)){
+                    scaleFactor = Float.parseFloat(paramTokens[i].substring(SC_ID.length()));
+                }
+                else if (paramTokens[i].startsWith(SM_ID)){
+                    //TODO 
+                }
+                else {
+                    System.err.println("Uknown type of resource parameter: "+paramTokens[i]);
+                }
+            }            
+        }
+
         FitsImageDescription desc = new FitsImageDescription(
                 id, x, y, zindex, resourceURL, region, 
-                1, FitsImage.ScaleMethod.ASINH);
+                scaleFactor, FitsImage.ScaleMethod.ASINH);
         region.addObject(desc);
         return desc;             
             }
