f7c74c44657bdb7bedf21ead70695433af7aafa0
Romain Primet <>
2010-05-19 12:55:39 +0000

    added FitsViewer: a fork of the generic zuist viewer with a couple of additional possibilities (changing the image color mapping or transfer function)
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@3247 74e165f3-c697-40a6-91af-552c8936d7fe

--- zuist-cluster-fits/trunk/run.sh
+++ zuist-cluster-fits/trunk/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -cp target/zuist-cluster-fits-0.1.1-SNAPSHOT.jar:target/jgroups-2.7.0.GA.jar:target/aspectjrt-1.6.2.jar:target/args4j-2.0.12.jar:target/log4j-1.2.14.jar:target/zvtm-fits-0.1.1-SNAPSHOT.jar:target/slf4j-api-1.5.9-RC0.jar:target/slf4j-log4j12-1.5.9-RC0.jar:target/timingframework-1.0.jar:target/commons-collections-2.1.jar:target/commons-logging-1.1.jar:target/zvtm-svg-0.1.1-SNAPSHOT.jar fr.inria.zuist.cluster.viewer.Viewer "$@" 
+java -cp target/zuist-cluster-fits-0.1.1-SNAPSHOT.jar:target/jgroups-2.7.0.GA.jar:target/aspectjrt-1.6.2.jar:target/args4j-2.0.12.jar:target/log4j-1.2.14.jar:target/zvtm-fits-0.1.1-SNAPSHOT.jar:target/slf4j-api-1.5.9-RC0.jar:target/slf4j-log4j12-1.5.9-RC0.jar:target/timingframework-1.0.jar:target/commons-collections-2.1.jar:target/commons-logging-1.1.jar:target/zvtm-svg-0.1.1-SNAPSHOT.jar fr.inria.zuist.viewer.FitsViewer "$@" 
 
