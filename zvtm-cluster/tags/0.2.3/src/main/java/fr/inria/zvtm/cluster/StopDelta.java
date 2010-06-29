a7619d0b5ac113c56a93c5f268c5ecf87d7441f3
Romain Primet <>
2010-06-29 11:53:24 +0000

    tagged 0.2.3
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@3391 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ zvtm-cluster/tags/0.2.3/src/main/java/fr/inria/zvtm/cluster/StopDelta.java
@@ -0,0 +1,8 @@
+package fr.inria.zvtm.cluster;
+
+class StopDelta implements Delta{
+	public void apply(SlaveUpdater updater){
+        updater.stop();
+    }
+}
+
---
+++ zvtm-cluster/tags/0.2.3/src/main/resources/scripts/local_master.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#sample master run script. requires maven.
+
+DEPS_CP=`mvn org.apache.maven.plugins:maven-dependency-plugin:2.0:build-classpath -DexcludeArtifactIds="zvtm" | grep jgroups`
+
+ZVTM_CLUSTER_VERSION="0.2.3-SNAPSHOT"
+CLASSNAME="fr.inria.zvtm.cluster.examples.VImageExample"
+
+java -Djgroups.bind_addr="127.0.0.1" -Djava.net.preferIPv4Stack=true -cp .:target/zvtm-cluster-"$ZVTM_CLUSTER_VERSION".jar:$DEPS_CP "$CLASSNAME" "$@"
+
