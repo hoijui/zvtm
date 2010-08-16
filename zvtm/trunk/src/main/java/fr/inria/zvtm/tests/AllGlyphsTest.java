565901ca6c10e89fdc3428b2472363e65234149a
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2010-08-16 15:16:49 +0000

    more glyphs
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@3485 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm/trunk/src/main/java/fr/inria/zvtm/tests/AllGlyphsTest.java
+++ zvtm/trunk/src/main/java/fr/inria/zvtm/tests/AllGlyphsTest.java
@@ -52,22 +52,66 @@ public class AllGlyphsTest {
     }
 
     void populate(){
+        // reference frame
+        for (int i=0;i<6;i++){
+            for (int j=0;j<10;j++){
+                vs.addGlyph(new VRectangle(i*40,j*40,0,20,20,Color.GRAY));
+            }
+        }
         // circles
         VCircle c = new VCircle(0,0,0,20,Color.WHITE);
         vs.addGlyph(c);
-        c = new VCircle(40,0,0,10,Color.WHITE);
+        c = new VCircle(40,0,0,10,Color.RED);
         c.sizeTo(20);
         vs.addGlyph(c);
         // rectangles
         VRectangle r = new VRectangle(0,40,0,20,10,Color.WHITE);
         vs.addGlyph(r);
-        r = new VRectangle(40,40,0,10,5,Color.WHITE);
+        r = new VRectangle(40,40,0,10,5,Color.RED);
         r.sizeTo(22.36f);
         vs.addGlyph(r);
-        r = new VRectangle(80,40,0,10,5,Color.WHITE);
+        r = new VRectangle(80,40,0,10,5,Color.BLUE);
         r.setWidth(20);
         r.setHeight(10);
         vs.addGlyph(r);
+        FRectangle fr = new FRectangle(120,40,0,20,10,Color.GREEN,1f,0f);
+        vs.addGlyph(fr);
+        VRectangleOr or = new VRectangleOr(160,40,0,20,10,Color.YELLOW,(float)Math.PI/2f);
+        vs.addGlyph(or);
+        or = new VRectangleOr(160,40,0,10,5,Color.YELLOW,(float)Math.PI/2f);
+        or.sizeTo(22.36f);
+        vs.addGlyph(or);
+        or = new VRectangleOr(200,40,0,10,5,Color.ORANGE,(float)Math.PI/2f);
+        or.setWidth(20);
+        or.setHeight(10);
+        vs.addGlyph(or);
+        // path
+        DPath d = new DPath(0, 80, 0, Color.BLACK);
+        d.addSegment(20, 20, false);
+        d.addQdCurve(0, -40, -20, -20, false);
+        d.addCbCurve(-40, 20, -40, 0, 0, 20, false);
+        vs.addGlyph(d);
+        // polygons
+        LongPoint[] vertices = {new LongPoint(-20,120), new LongPoint(0,140), new LongPoint(20,120), new LongPoint(0,100)};
+        FPolygon fp = new FPolygon(vertices, Color.WHITE);
+        vs.addGlyph(fp);
+        LongPoint[] vertices2 = {new LongPoint(0,0), new LongPoint(20,20), new LongPoint(40,0), new LongPoint(20,-20)};
+        fp = new FPolygon(vertices2, Color.RED);
+        fp.moveTo(40, 120);
+        vs.addGlyph(fp);
+        
+        LongPoint[] vertices3 = {new LongPoint(60,120), new LongPoint(80,140), new LongPoint(100,120), new LongPoint(80,100)};
+        VPolygon vp = new VPolygon(vertices3, 0, Color.BLUE);
+        vs.addGlyph(vp);
+        LongPoint[] vertices4 = {new LongPoint(0,0), new LongPoint(20,20), new LongPoint(40,0), new LongPoint(20,-20)};
+        vp = new VPolygon(vertices4, 0, Color.GREEN);
+        vp.moveTo(120, 120);
+        vs.addGlyph(vp);
+        LongPoint[] vertices5 = {new LongPoint(0,0), new LongPoint(10,10), new LongPoint(20,0), new LongPoint(10,-10)};
+        vp = new VPolygon(vertices5, 0, Color.YELLOW);
+        vp.moveTo(160, 120);
+        vp.sizeTo(20);
+        vs.addGlyph(vp);
     }
 
     public static void main(String[] args){
