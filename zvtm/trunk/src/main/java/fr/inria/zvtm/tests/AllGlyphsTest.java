942fbee052aeb7d2b54c4105e70007c3b8222e77
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2010-08-17 13:05:15 +0000

    pan & zoom more glyphs
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@3507 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm/trunk/src/main/java/fr/inria/zvtm/tests/AllGlyphsTest.java
+++ zvtm/trunk/src/main/java/fr/inria/zvtm/tests/AllGlyphsTest.java
@@ -10,6 +10,8 @@
 package fr.inria.zvtm.tests;
 
 import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
 
 import java.util.Vector;
 
@@ -23,6 +25,7 @@ public class AllGlyphsTest {
     VirtualSpaceManager vsm;
     VirtualSpace vs;
     ViewEventHandler eh;
+    Camera mCam;
 
     View testView;
     
@@ -35,8 +38,9 @@ public class AllGlyphsTest {
     public void initTest(short ogl){
         eh = new TestEventHandler(this);
         vs = vsm.addVirtualSpace("s1");
+        mCam = vs.addCamera();
         Vector cameras = new Vector();
-        cameras.add(vs.addCamera());
+        cameras.add(mCam);
         vs.getCamera(0).setZoomFloor(-90f);
         short vt = View.STD_VIEW;
         switch(ogl){
@@ -86,7 +90,7 @@ public class AllGlyphsTest {
         or.setHeight(10);
         vs.addGlyph(or);
         // path
-        DPath d = new DPath(0, 80, 0, Color.BLACK);
+        DPath d = new DPath(0, 80, 0, Color.WHITE);
         d.addSegment(20, 20, false);
         d.addQdCurve(0, -40, -20, -20, false);
         d.addCbCurve(-40, 20, -40, 0, 0, 20, false);
@@ -99,7 +103,6 @@ public class AllGlyphsTest {
         fp = new FPolygon(vertices2, Color.RED);
         fp.moveTo(40, 120);
         vs.addGlyph(fp);
-        
         LongPoint[] vertices3 = {new LongPoint(60,120), new LongPoint(80,140), new LongPoint(100,120), new LongPoint(80,100)};
         VPolygon vp = new VPolygon(vertices3, 0, Color.BLUE);
         vs.addGlyph(vp);
@@ -112,6 +115,38 @@ public class AllGlyphsTest {
         vp.moveTo(160, 120);
         vp.sizeTo(20);
         vs.addGlyph(vp);
+        // point
+        VPoint pt = new VPoint(0, 160, 0, Color.WHITE);
+        vs.addGlyph(pt);
+        pt = new VPoint(10, 10, 0, Color.RED);
+        pt.moveTo(40, 160);
+        vs.addGlyph(pt);
+        // diamonds
+        VDiamond di = new VDiamond(0, 200, 0, 20, Color.WHITE);
+        vs.addGlyph(di);
+        di = new VDiamond(40, 200, 0, 10, Color.RED);
+        di.sizeTo(20);
+        vs.addGlyph(di);
+        di = new VDiamondOr(80, 200, 0, 10, Color.BLUE, (float)Math.PI);
+        di.sizeTo(20);
+        vs.addGlyph(di);
+        di = new VDiamondOr(0, 0, 0, 10, Color.GREEN, (float)Math.PI);
+        di.sizeTo(20);
+        di.moveTo(120, 200);
+        vs.addGlyph(di);
+        // triangles
+        VTriangle tr = new VTriangle(0, 240, 0, 20, Color.WHITE);
+        vs.addGlyph(tr);
+        tr = new VTriangle(40, 240, 0, 10, Color.RED);
+        tr.sizeTo(20);
+        vs.addGlyph(tr);
+        tr = new VTriangleOr(80, 240, 0, 10, Color.BLUE, (float)Math.PI);
+        tr.sizeTo(20);
+        vs.addGlyph(tr);
+        tr = new VTriangleOr(0, 0, 0, 10, Color.GREEN, (float)Math.PI);
+        tr.sizeTo(20);
+        tr.moveTo(120, 240);
+        vs.addGlyph(tr);
     }
 
     public static void main(String[] args){
@@ -136,11 +171,50 @@ public class AllGlyphsTest {
 
 class TestEventHandler extends DefaultEventHandler {
     
+    static float ZOOM_SPEED_COEF = 1.0f/50.0f;
+    static double PAN_SPEED_COEF = 50.0;
+    
     AllGlyphsTest test;
+    
+    int lastJPX,lastJPY;
     
     TestEventHandler(AllGlyphsTest t){
         this.test = t;
     }
     
+    public void press1(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
+        lastJPX = jpx;
+        lastJPY = jpy;
+        v.setDrawDrag(true);
+    }
+
+    public void release1(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
+        VirtualSpaceManager.INSTANCE.getAnimationManager().setXspeed(0);
+        VirtualSpaceManager.INSTANCE.getAnimationManager().setYspeed(0);
+        v.setDrawDrag(false);
+    }
+    
+    public void mouseDragged(ViewPanel v,int mod,int buttonNumber,int jpx,int jpy, MouseEvent e){
+        if (buttonNumber == 1){
+            Camera c = test.mCam;
+            float a=(c.focal+Math.abs(c.altitude))/c.focal;
+            VirtualSpaceManager.INSTANCE.getAnimationManager().setXspeed((c.altitude>0) ? (long)((jpx-lastJPX)*(a/PAN_SPEED_COEF)) : (long)((jpx-lastJPX)/(a*PAN_SPEED_COEF)));
+            VirtualSpaceManager.INSTANCE.getAnimationManager().setYspeed((c.altitude>0) ? (long)((lastJPY-jpy)*(a/PAN_SPEED_COEF)) : (long)((lastJPY-jpy)/(a*PAN_SPEED_COEF)));
+        }
+    }
+    
+    public void mouseWheelMoved(ViewPanel v, short wheelDirection, int jpx, int jpy, MouseWheelEvent e){
+        Camera c = test.mCam;
+        float a=(c.focal+Math.abs(c.altitude))/c.focal;
+        if (wheelDirection == WHEEL_UP){
+            c.altitudeOffset(-a*5);
+            VirtualSpaceManager.INSTANCE.repaintNow();
+        }
+        else {
+            //wheelDirection == WHEEL_DOWN
+            c.altitudeOffset(a*5);
+            VirtualSpaceManager.INSTANCE.repaintNow();
+        }
+    }
     
 }
