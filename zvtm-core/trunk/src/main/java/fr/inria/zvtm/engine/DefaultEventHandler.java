1fdedfd0a597ba8901f7ba89385ff4b662ece94c
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2010-08-28 09:06:38 +0000

    - @Override annotations, javadoc
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@3723 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-core/trunk/src/main/java/fr/inria/zvtm/engine/DefaultEventHandler.java
+++ zvtm-core/trunk/src/main/java/fr/inria/zvtm/engine/DefaultEventHandler.java
@@ -2,7 +2,7 @@
  *   DATE OF CREATION:  Fri May 26 15:01:11 2006
  *   AUTHOR :           Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
  *   MODIF:             Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
- *   Copyright (c) INRIA, 2006. All Rights Reserved
+ *   Copyright (c) INRIA, 2006-2010. All Rights Reserved
  *   Licensed under the GNU LGPL. For full terms see the file COPYING.
  *
  * $Id$
@@ -25,73 +25,78 @@ import fr.inria.zvtm.glyphs.Glyph;
 
 public class DefaultEventHandler implements ViewEventHandler {
     
-    /**mouse button 1 pressed callback
-     *@param mod one of NO_MODIFIER, SHIFT_MOD, CTRL_MOD, CTRL_SHIFT_MOD, META_MOD, META_SHIFT_MOD, ALT_MOD, ALT_SHIFT_MOD
-     */
+    @Override
     public void press1(ViewPanel v, int mod, int jpx, int jpy, MouseEvent e){}
-    /**mouse button 1 released callback
-     *@param mod one of NO_MODIFIER, SHIFT_MOD, CTRL_MOD, CTRL_SHIFT_MOD, META_MOD, META_SHIFT_MOD, ALT_MOD, ALT_SHIFT_MOD
-     */
+
+    @Override
     public void release1(ViewPanel v, int mod, int jpx, int jpy, MouseEvent e){}
-    /**mouse button 1 clicked callback
-     *@param mod one of NO_MODIFIER, SHIFT_MOD, CTRL_MOD, CTRL_SHIFT_MOD, META_MOD, META_SHIFT_MOD, ALT_MOD, ALT_SHIFT_MOD
-     */
+    
+    @Override
     public void click1(ViewPanel v, int mod, int jpx, int jpy, int clickNumber, MouseEvent e){}
-    /**mouse button 2 pressed callback
-     *@param mod one of ALT_MOD, ALT_SHIFT_MOD (the middle mouse button by itself is considered as being ALT-modified by default by Java)
-     */
+
+    @Override
     public void press2(ViewPanel v, int mod, int jpx, int jpy, MouseEvent e){}
-    /**mouse button 2 released callback
-     *@param mod one of ALT_MOD, ALT_SHIFT_MOD (the middle mouse button by itself is considered as being ALT-modified by default by Java)
-     */
+
+    @Override
     public void release2(ViewPanel v, int mod, int jpx, int jpy, MouseEvent e){}
-    /**mouse button 2 clicked callback
-     *@param mod one of ALT_MOD, ALT_SHIFT_MOD (the middle mouse button by itself is considered as being ALT-modified by default by Java)
-     */
+
+    @Override
     public void click2(ViewPanel v, int mod, int jpx, int jpy, int clickNumber, MouseEvent e){}
-    /**mouse button 3 pressed callback
-     *@param mod one of META_MOD, META_SHIFT_MOD (the right mouse button by itself is considered as being META-modified by default by Java)
-     */
+
+    @Override
     public void press3(ViewPanel v, int mod, int jpx, int jpy, MouseEvent e){}
-    /**mouse button 3 released callback
-     *@param mod one of META_MOD, META_SHIFT_MOD (the right mouse button by itself is considered as being META-modified by default by Java)
-     */
+
+    @Override
     public void release3(ViewPanel v, int mod, int jpx, int jpy, MouseEvent e){}
-    /**mouse button 3 clicked callback
-     *@param mod one of META_MOD, META_SHIFT_MOD (the right mouse button by itself is considered as being META-modified by default by Java)
-     */
+
+    @Override
     public void click3(ViewPanel v, int mod, int jpx, int jpy, int clickNumber, MouseEvent e){}
 
-    /**mouse moved callback (this callback needs to be activated by View.setNotifyMouseMoved() ; it is inactive by default)*/
+
+
+    @Override
     public void mouseMoved(ViewPanel v, int jpx, int jpy, MouseEvent e){}
 
-    /**mouse dragged callback
-     *@param buttonNumber one of 1 (left), 2 (middle) or 3 (right)
-     *@param jpx mouse coord in JPanel coord syst
-     *@param jpy mouse coord in JPanel coord syst
-     *@param mod see pressX/releaseX/clickX with X=buttonNumber for possible values
-     */
+    @Override
     public void mouseDragged(ViewPanel v, int mod, int buttonNumber, int jpx, int jpy, MouseEvent e){}
 
-    /**mouse wheel moved callback
-     *@param wheelDirection is one of WHEEL_UP, WHEEL_DOWN
-     *@param jpx mouse coord in JPanel coord syst
-     *@param jpy mouse coord in JPanel coord syst
-     */
+    @Override
     public void mouseWheelMoved(ViewPanel v, short wheelDirection, int jpx, int jpy, MouseWheelEvent e){}
 
+
+
+    @Override
     public void enterGlyph(Glyph g){}
 
+    @Override
     public void exitGlyph(Glyph g){}
 
-    /**beware: code is always 0 in Ktype (it is the value of KeyEvent.getKeyCode() which is always equal to VK_UNDEFINED according to Sun). If you need to access code, use Kpress or Krelease.*/
+
+
+    @Override
     public void Ktype(ViewPanel v, char c, int code, int mod, KeyEvent e){}
+    
+    @Override
     public void Kpress(ViewPanel v, char c, int code, int mod, KeyEvent e){}
+    
+    @Override
     public void Krelease(ViewPanel v, char c, int code, int mod, KeyEvent e){}
 
+
+
+    @Override
     public void viewActivated(View v){}
+
+    @Override
     public void viewDeactivated(View v){}
+
+    @Override
     public void viewIconified(View v){}
+
+    @Override
     public void viewDeiconified(View v){}
+
+    @Override
     public void viewClosing(View v){}
+
 }
