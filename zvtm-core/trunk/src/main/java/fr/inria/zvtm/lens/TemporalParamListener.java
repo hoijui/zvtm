4d18f8a24cd62ff33709f32132065235af2add9b
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2010-09-01 08:06:53 +0000

    svn kwd id
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@3790 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-core/trunk/src/main/java/fr/inria/zvtm/lens/TemporalParamListener.java
+++ zvtm-core/trunk/src/main/java/fr/inria/zvtm/lens/TemporalParamListener.java
@@ -2,7 +2,7 @@
  *   Copyright (c) INRIA, 2007-2009. All Rights Reserved
  *   Licensed under the GNU LGPL. For full terms see the file COPYING.
  *
- * $Id: SCFGaussianLens.java 2217 2009-07-21 04:39:20Z OlivierChapuis $
+ * $Id$
  */
 
 package fr.inria.zvtm.lens;
