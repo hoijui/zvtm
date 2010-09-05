1af3a2e1efb16591d68fc2ea03611d31fb197982
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2010-09-05 18:43:26 +0000

    preparing app for dual config desktop/wild
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@3807 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ zuist-wm/trunk/src/main/java/fr/inria/zuist/app/wm/WildWorldExplorer.java
@@ -0,0 +1,170 @@
+/*   AUTHOR :           Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   Copyright (c) INRIA, 2007-2010. All Rights Reserved
+ *   Licensed under the GNU LGPL. For full terms see the file COPYING.
+ *
+ * $Id$
+ */
+
+package fr.inria.zuist.app.wm;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.FilenameFilter;
+
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Graphics;
+import java.awt.GradientPaint;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.GraphicsEnvironment;
+import java.awt.GraphicsDevice;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.KeyAdapter;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
+import javax.swing.ImageIcon;
+import java.awt.geom.Point2D;
+
+import java.util.Vector;
+
+import java.io.File;
+
+import fr.inria.zvtm.engine.Camera;
+import fr.inria.zvtm.engine.VirtualSpaceManager;
+import fr.inria.zvtm.engine.VirtualSpace;
+import fr.inria.zvtm.engine.View;
+import fr.inria.zvtm.engine.Utils;
+import fr.inria.zvtm.glyphs.VSegment;
+import fr.inria.zvtm.glyphs.Glyph;
+import fr.inria.zvtm.glyphs.ClosedShape;
+import fr.inria.zvtm.glyphs.VImage;
+import fr.inria.zvtm.engine.Java2DPainter;
+import fr.inria.zvtm.widgets.TranslucentTextArea;
+import fr.inria.zvtm.animation.Animation;
+import fr.inria.zvtm.animation.EndAction;
+import fr.inria.zvtm.animation.interpolation.SlowInSlowOutInterpolator;
+
+import fr.inria.zuist.engine.SceneManager;
+import fr.inria.zuist.engine.ProgressListener;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.FactoryConfigurationError;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import org.geotools.factory.GeoTools;
+
+import org.geonames.Toponym;
+import org.geonames.InsufficientStyleException;
+
+/**
+ * @author Emmanuel Pietriga
+ */
+
+public class WildWorldExplorer extends WorldExplorer {
+
+    public WildWorldExplorer(boolean queryGN, boolean lad1, boolean air,
+                             boolean fullscreen, boolean grid, boolean opengl, boolean aa, File xmlSceneFile){
+        super(queryGN, lad1, air, fullscreen, grid, opengl, aa, xmlSceneFile);
+    }
+
+    void initGUI(boolean fullscreen, boolean opengl, boolean aa){
+        windowLayout();
+        vsm = VirtualSpaceManager.INSTANCE;
+        mSpace = vsm.addVirtualSpace(mSpaceName);
+        bSpace = vsm.addVirtualSpace(bSpaceName);
+        mCamera = mSpace.addCamera();
+        ovCamera = mSpace.addCamera();
+		bCamera = bSpace.addCamera();
+        Vector cameras = new Vector();
+        cameras.add(mCamera);
+        cameras.add(bCamera);
+        mCamera.stick(bCamera, true);
+        mView = vsm.addFrameView(cameras, mViewName, (opengl) ? View.OPENGL_VIEW : View.STD_VIEW, VIEW_W, VIEW_H, false, false, !fullscreen, null);
+        if (fullscreen && GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().isFullScreenSupported()){
+            GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().setFullScreenWindow((JFrame)mView.getFrame());
+        }
+        else {
+            mView.setVisible(true);
+        }
+		mView.setAntialiasing(aa);
+        eh = new ExplorerEventHandler(this);
+        mView.setListener(eh, 0);
+        mView.setListener(eh, 1);
+        mView.setBackgroundColor(BACKGROUND_COLOR);
+		mView.getCursor().setColor(Color.WHITE);
+		mView.getCursor().setHintColor(Color.WHITE);
+		mView.getCursor().setDynaSpotColor(Color.WHITE);
+        mView.getCursor().setDynaSpotLagTime(200);
+		mView.getCursor().activateDynaSpot(true);
+        mView.setJava2DPainter(this, Java2DPainter.AFTER_PORTALS);
+        updatePanelSize();
+        // console
+        JLayeredPane lp = ((JFrame)mView.getFrame()).getRootPane().getLayeredPane();
+        console = new TranslucentTextArea(2, 80);
+        console.setForeground(Color.WHITE);
+        console.setBackground(Color.BLACK);
+        lp.add(console, (Integer)(JLayeredPane.DEFAULT_LAYER+50));
+        console.setBounds(20, panelHeight-100, panelWidth-250, 96);
+        console.setMargin(new java.awt.Insets(5,5,5,5));
+        console.setVisible(false);
+        mView.setActiveLayer(1);
+        mView.getPanel().addComponentListener(eh);
+    }
+
+    public static void main(String[] args){
+        File xmlSceneFile = null;
+		boolean fs = false;
+		boolean ogl = false;
+		boolean aa = false;
+		boolean grid = false;
+		boolean queryGN = false;
+		boolean lad1 = false;
+		boolean air = false;
+		for (int i=0;i<args.length;i++){
+			if (args[i].startsWith("-")){
+				if (args[i].substring(1).equals("fs")){fs = true;}
+				else if (args[i].substring(1).equals("opengl")){ogl = true;}
+				else if (args[i].substring(1).equals("aa")){aa = true;}
+				else if (args[i].substring(1).equals("grid")){grid = true;}
+				else if (args[i].substring(1).equals("qgn")){queryGN = true;}
+				else if (args[i].substring(1).equals("lad1")){lad1 = true;}
+				else if (args[i].substring(1).equals("air")){air = true;}
+				else if (args[i].substring(1).equals("h") || args[i].substring(1).equals("--help")){WorldExplorer.printCmdLineHelp();System.exit(0);}
+			}
+            else {
+                // the only other thing allowed as a cmd line param is a scene file
+                File f = new File(args[i]);
+                if (f.exists()){
+                    if (f.isDirectory()){
+                        // if arg is a directory, take first xml file we find in that directory
+                        String[] xmlFiles = f.list(new FilenameFilter(){
+                                                public boolean accept(File dir, String name){return name.endsWith(".xml");}
+                                            });
+                        if (xmlFiles.length > 0){
+                            xmlSceneFile = new File(f, xmlFiles[0]);
+                        }
+                    }
+                    else {
+                        xmlSceneFile = f;                        
+                    }
+                }
+            }
+		}
+        if (!fs && Utils.osIsMacOS()){
+            System.setProperty("apple.laf.useScreenMenuBar", "true");
+        }
+        System.out.println("--help for command line options");
+        System.out.println("Using GeoTools v" + GeoTools.getVersion());
+        new WildWorldExplorer(queryGN, lad1, air, fs, grid, ogl, aa, xmlSceneFile);
+    }
+
+}
