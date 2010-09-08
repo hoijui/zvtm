a3e7774383b15eab0f80b7ac6a0ed9dd517cbe32
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2010-09-08 08:54:20 +0000

    updated header
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@3823 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-cluster/trunk/src/main/java/fr/inria/zvtm/cluster/examples/StickedCamerasExample.java
+++ zvtm-cluster/trunk/src/main/java/fr/inria/zvtm/cluster/examples/StickedCamerasExample.java
@@ -1,4 +1,4 @@
-/*   AUTHOR : Romain Primet (romain.primet@inria.fr)
+/*   AUTHOR : Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
  *
  *  (c) COPYRIGHT INRIA (Institut National de Recherche en Informatique et en Automatique), 2010.
  *  Licensed under the GNU LGPL. For full terms see the file COPYING.
