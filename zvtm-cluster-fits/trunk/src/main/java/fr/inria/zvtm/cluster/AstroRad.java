c6774a34812d3cf2bcf3c0b777c41a6ee3e2bacd
Romain Primet <>
2010-09-09 14:41:33 +0000

    still drafting AstroRad
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@3835 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-cluster-fits/trunk/src/main/java/fr/inria/zvtm/cluster/AstroRad.java
+++ zvtm-cluster-fits/trunk/src/main/java/fr/inria/zvtm/cluster/AstroRad.java
@@ -7,11 +7,19 @@ import java.util.Vector;
 
 import fr.inria.zvtm.engine.Camera;
 import fr.inria.zvtm.engine.View;
+import fr.inria.zvtm.engine.ViewPanel;
 import fr.inria.zvtm.engine.VirtualSpace;
 import fr.inria.zvtm.engine.VirtualSpaceManager;
+import fr.inria.zvtm.event.ViewListener;
 import fr.inria.zvtm.fits.FitsHistogram;
 import fr.inria.zvtm.fits.RangeSelection;
 import fr.inria.zvtm.glyphs.FitsImage;
+import fr.inria.zvtm.glyphs.Glyph;
+
+import java.awt.geom.Point2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
 
 /**
  * A clustered viewer for FITS images.
@@ -25,6 +33,7 @@ public class AstroRad {
     private RangeSelection rangeSel;
     private FitsImage selectedImage;
     private FitsHistogram hist;
+    private VirtualSpaceManager vsm = VirtualSpaceManager.INSTANCE;
 
     //todo:
     // - image thumbnail (tile)
@@ -37,9 +46,9 @@ public class AstroRad {
     }
 
     private void setup(){
-        VirtualSpaceManager.INSTANCE.setMaster("AstroRad");
-        imageSpace = VirtualSpaceManager.INSTANCE.addVirtualSpace("imageSpace");
-        controlSpace = VirtualSpaceManager.INSTANCE.addVirtualSpace("controlSpace");
+        vsm.setMaster("AstroRad");
+        imageSpace = vsm.addVirtualSpace("imageSpace");
+        controlSpace = vsm.addVirtualSpace("controlSpace");
         imageCamera = imageSpace.addCamera();
         controlCamera = controlSpace.addCamera();
         ArrayList<Camera> imgCamList = new ArrayList<Camera>();
@@ -49,8 +58,9 @@ public class AstroRad {
         Vector<Camera> cameras = new Vector<Camera>();
         cameras.add(imageCamera);
         cameras.add(controlCamera);
-        View view = VirtualSpaceManager.INSTANCE.addFrameView(cameras, "Master View",
+        View view = vsm.addFrameView(cameras, "Master View",
                 View.STD_VIEW, 800, 600, false, true, true, null);	
+        view.setListener(new PanZoomEventHandler());
 
         //setup cluster geometry
         ClusterGeometry clGeom = new ClusterGeometry(
@@ -61,8 +71,8 @@ public class AstroRad {
         //setup clustered views
         ClusteredView imageView = new ClusteredView(clGeom, 3, 6, 4, imgCamList);
         ClusteredView controlView = new ClusteredView(clGeom, 27, 2, 4, controlCamList);
-        VirtualSpaceManager.INSTANCE.addClusteredView(imageView);
-        VirtualSpaceManager.INSTANCE.addClusteredView(controlView);
+        vsm.addClusteredView(imageView);
+        vsm.addClusteredView(controlView);
 
         setupControlZone();
     }
@@ -70,7 +80,8 @@ public class AstroRad {
     private void setupControlZone(){
         rangeSel = new RangeSelection();
         controlSpace.addGlyph(rangeSel);
-        rangeSel.sizeTo(2000);
+        rangeSel.reSize(10);
+        rangeSel.move(-1000, 1000);
     }
 
     private void addImage(URL imgUrl){
@@ -99,6 +110,86 @@ public class AstroRad {
         }
 
         new AstroRad(new URL(args[0]));
+    }
+
+    private class PanZoomEventHandler implements ViewListener{
+        private int lastJPX;
+        private int lastJPY;
+
+        public void press1(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){}
+
+        public void release1(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){}
+
+        public void click1(ViewPanel v,int mod,int jpx,int jpy,int clickNumber, MouseEvent e){}
+
+        public void press2(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){}
+
+        public void release2(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){}
+
+        public void click2(ViewPanel v,int mod,int jpx,int jpy,int clickNumber, MouseEvent e){}
+
+        public void press3(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
+            lastJPX=jpx;
+            lastJPY=jpy;
+            v.setDrawDrag(true);
+            vsm.getActiveView().mouse.setSensitivity(false);
+            //because we would not be consistent  (when dragging the mouse, we computeMouseOverList, but if there is an anim triggered by {X,Y,A}speed, and if the mouse is not moving, this list is not computed - so here we choose to disable this computation when dragging the mouse with button 3 pressed)
+        }
+
+        public void release3(ViewPanel v,int mod,int jpx,int jpy, MouseEvent e){
+            vsm.getAnimationManager().setXspeed(0);
+            vsm.getAnimationManager().setYspeed(0);
+            vsm.getAnimationManager().setZspeed(0);
+            v.setDrawDrag(false);
+            vsm.getActiveView().mouse.setSensitivity(true);
+        }
+
+        public void click3(ViewPanel v,int mod,int jpx,int jpy,int clickNumber, MouseEvent e){}
+
+        public void mouseMoved(ViewPanel v,int jpx,int jpy, MouseEvent e){}
+
+        public void mouseDragged(ViewPanel v,int mod,int buttonNumber,int jpx,int jpy, MouseEvent e){
+            if (buttonNumber == 3 || ((mod == META_MOD || mod == META_SHIFT_MOD) && buttonNumber == 1)){
+                Camera c=vsm.getActiveCamera();
+                double a=(c.focal+Math.abs(c.altitude))/c.focal;
+                if (mod == META_SHIFT_MOD) {
+                    vsm.getAnimationManager().setXspeed(0);
+                    vsm.getAnimationManager().setYspeed(0);
+                    vsm.getAnimationManager().setZspeed((c.altitude>0) ? (lastJPY-jpy)*(a/4.0) : (lastJPY-jpy)/(a*4));
+
+                }
+                else {
+                    vsm.getAnimationManager().setXspeed((c.altitude>0) ? (jpx-lastJPX)*(a/4.0) : (jpx-lastJPX)/(a*4));
+                    vsm.getAnimationManager().setYspeed((c.altitude>0) ? (lastJPY-jpy)*(a/4.0) : (lastJPY-jpy)/(a*4));
+                }
+            }
+        }
+
+        public void mouseWheelMoved(ViewPanel v,short wheelDirection,int jpx,int jpy, MouseWheelEvent e){}
+
+        public void enterGlyph(Glyph g){
+        }
+
+        public void exitGlyph(Glyph g){
+        }
+
+        public void Ktype(ViewPanel v,char c,int code,int mod, KeyEvent e){}
+
+        public void Kpress(ViewPanel v,char c,int code,int mod, KeyEvent e){
+        }
+
+        public void Krelease(ViewPanel v,char c,int code,int mod, KeyEvent e){}
+
+        public void viewActivated(View v){}
+
+        public void viewDeactivated(View v){}
+
+        public void viewIconified(View v){}
+
+        public void viewDeiconified(View v){}
+
+        public void viewClosing(View v){System.exit(0);}
+
     }
 }
 
