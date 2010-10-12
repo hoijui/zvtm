01a44012975dcdf2f069317701cc7cab7c153907
Romain Primet <>
2010-10-12 08:50:16 +0000

    added WCS example
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@3935 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-fits/trunk/prebuild.sh
+++ zvtm-fits/trunk/prebuild.sh
@@ -5,5 +5,7 @@
 
 cd `mktemp -d` &&\
 wget http://skyservice.pha.jhu.edu/develop/vo/ivoafits/ivoafits-0.3.jar &&\
-mvn install:install-file -DgroupId=ivoa.fits -DartifactId=fits -Dversion=0.3.0 -Dpackaging=jar -Dfile=ivoafits-0.3.jar 
+mvn install:install-file -DgroupId=ivoa.fits -DartifactId=fits -Dversion=0.3.0 -Dpackaging=jar -Dfile=ivoafits-0.3.jar &&\
+wget http://skyview.gsfc.nasa.gov/jar/skyview.jar &&\
+mvn install:install-file -DgroupId=skyview -DartifactId=skyview -Dversion=2.4b -Dpackaging=jar -Dfile=skyview.jar
 
