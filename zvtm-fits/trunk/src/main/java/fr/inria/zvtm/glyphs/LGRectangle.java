48236b6cc7004b9bdb99cd255c7d451ff48c8708
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2010-10-12 10:11:13 +0000

    linear gradient paint rectangle
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@3938 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ zvtm-fits/trunk/src/main/java/fr/inria/zvtm/glyphs/LGRectangle.java
@@ -0,0 +1,31 @@
+/*   AUTHOR :          Romain Primet (romain.primet@inria.fr) 
+ *   Copyright (c) INRIA, 2010. All Rights Reserved
+ *   Licensed under the GNU LGPL. For full terms see the file COPYING.
+ *
+ */
+package fr.inria.zvtm.glyphs;
+
+import java.awt.LinearGradientPaint;
+import java.awt.Color;
+import fr.inria.zvtm.glyphs.PRectangle;
+
+public class LGRectangle extends PRectangle {
+    
+    /**
+		*@param x coordinate in virtual space
+		*@param y coordinate in virtual space
+		*@param z z-index (pass 0 if you do not use z-ordering)
+		*@param w width in virtual space
+		*@param h height in virtual space
+		*@param p gradient or texture paint
+		*@param bc border color
+		*/
+	public LGRectangle(double x, double y, int z, double w, double h, LinearGradientPaint p, Color bc){
+		super(x, y, z, w, h, p, bc);
+	}
+	
+	public LinearGradientPaint getGradient(){
+	    return (LinearGradientPaint)gp;
+	}
+    
+}
