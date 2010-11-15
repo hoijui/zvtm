a940ddd8257bba738481eb841a2b4f859f2ef560
Romain Primet <>
2010-11-15 13:11:02 +0000

    Drafting JSkyFitsImage
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@3966 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ zvtm-fits/trunk/src/main/java/fr/inria/zvtm/glyphs/JSkyFitsImage.java
@@ -0,0 +1,257 @@
+/*   AUTHOR : Romain Primet (romain.primet@inria.fr)
+ *
+ *  (c) COPYRIGHT INRIA (Institut National de Recherche en Informatique et en Automatique), 2010.
+ *  Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ */ 
+package fr.inria.zvtm.glyphs;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.awt.geom.AffineTransform;
+
+import fr.inria.zvtm.engine.Camera;
+import fr.inria.zvtm.glyphs.projection.RProjectedCoordsP;
+
+import jsky.image.fits.codec.FITSImage;
+
+//Fits support provided by JSky instead of IVOA FITS
+//Note: JSkyFitsImage requires JAI (Java Advanced Imaging)
+public class JSkyFitsImage extends ClosedShape implements RectangularShape {
+    private FITSImage fitsImage;
+
+    /** Width in virtual space (read-only). */
+    private double vw;
+    /** Height in virtual space (read-only). */
+    private double vh;
+
+    private RProjectedCoordsP[] pc;
+
+    public JSkyFitsImage(String fileOrUrl){
+        try{
+            fitsImage = new FITSImage(fileOrUrl);
+        } catch(Exception e){
+            //XXX change
+            throw new Error("Could not create FitsImage: " + e);
+        }
+    }
+
+    @Override public double getWidth(){
+        return vw;
+    }
+
+    @Override public double getHeight(){
+        return vh;
+    }
+
+    @Override public void setWidth(double w){
+    }
+
+    @Override public void setHeight(double h){
+    }
+
+    public float getScale(){
+        return fitsImage.getScale();
+    }
+
+    public void setScale(float scaleFactor){
+        //trigger a repaint if necessary
+        try{ 
+            fitsImage.setScale(scaleFactor);
+        } catch(Exception swallowed){
+            //dbg trace
+        }
+    }
+
+    @Override public double[] getBounds(){
+        double[] retval = new double[4];
+        return retval;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+        public boolean fillsView(double w, double h, int camIndex){
+            return false; //safe option
+        }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+        public short mouseInOut(int jpx, int jpy, int camIndex, double cvx, double cvy){
+            //XXX implement
+            return NO_CURSOR_EVENT;
+        }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+        public void resetMouseIn(){
+            //XXX ?
+        }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override 
+        public void resetMouseIn(int i){
+            //XXX ?
+        }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+        public boolean coordInside(int jpx, int jpy, int camIndex, double cvx, double cvy){
+            //XXX implement
+            return true;
+        }
+
+    //XXX implement visibleInRegion (clipping)
+
+    @Override 
+        public void removeCamera(int index){
+        }
+
+    @Override 
+        public void addCamera(int verifIndex){
+            if (pc!=null){
+                if (verifIndex==pc.length){
+                    RProjectedCoordsP[] ta=pc;
+                    pc=new RProjectedCoordsP[ta.length+1];
+                    for (int i=0;i<ta.length;i++){
+                        pc[i]=ta[i];
+                    }
+                    pc[pc.length-1]=new RProjectedCoordsP();
+                }
+                else {System.err.println("VImage:Error while adding camera "+verifIndex);}
+            }
+            else {
+                if (verifIndex==0){
+                    pc=new RProjectedCoordsP[1];
+                    pc[0]=new RProjectedCoordsP();
+                }
+                else {System.err.println("VImage:Error while adding camera "+verifIndex);}
+            }
+        }
+
+    @Override
+        public void initCams(int nbCam){
+            pc=new RProjectedCoordsP[nbCam];
+            for (int i=0;i<nbCam;i++){
+                pc[i]=new RProjectedCoordsP();
+            }
+        }
+
+    @Override
+        public void drawForLens(Graphics2D g,
+                int vW,
+                int vH,
+                int i,
+                Stroke stdS,
+                AffineTransform stdT,
+                int dx,
+                int dy){
+            draw(g, vW, vH, i, stdS, stdT, dx, dy);
+        }
+
+    @Override
+        public void draw(Graphics2D g,
+                int vW,
+                int vH,
+                int i,
+                Stroke stdS,
+                AffineTransform stdT,
+                int dx,
+                int dy){
+            //XXX change to make image zoomable
+            double trueCoef = 1; //scaleFactor * coef
+            AffineTransform at = AffineTransform.getTranslateInstance(dx+pc[i].cx-pc[i].cw,dy+pc[i].cy-pc[i].ch);
+            g.setTransform(at);
+            g.drawRenderedImage(fitsImage, AffineTransform.getScaleInstance(trueCoef,trueCoef));
+        }
+
+    @Override
+        public void projectForLens(Camera c,
+                int lensWidth,
+                int lensHeight,
+                float lensMag,
+                double lensx,
+                double lensy){
+           //XXX 
+        }
+
+    @Override
+        public void project(Camera c, Dimension d){
+            int i=c.getIndex();
+            coef = c.focal/(c.focal+c.altitude);
+            //find coordinates of object's geom center wrt to camera center and project
+            //translate in JPanel coords
+            pc[i].cx = (int)Math.round((d.width/2d)+(vx-c.vx)*coef);
+            pc[i].cy = (int)Math.round((d.height/2d)-(vy-c.vy)*coef);
+
+            //project width and height
+            pc[i].cw = (int)Math.round(vw/2d);
+            pc[i].ch = (int)Math.round(vh/2d);
+        }
+
+    @Override
+        public void highlight(boolean b,
+                Color selectedColor){
+            
+        }
+
+    @Override
+        public void orientTo(double angle){
+        }
+
+    @Override
+        public double getOrient(){
+            return 0f;
+        }
+
+    @Override 
+        public void reSize(double factor){
+        }
+
+    @Override
+        public void sizeTo(double newSize){
+            reSize(newSize/getSize()); 
+        }
+
+    @Override 
+        public void move(double dx, double dy){
+            vx += dx;
+            vy += dy;
+        }
+
+    @Override
+        public void moveTo(double x, double y){
+            move(x - vx, y - vy);
+        }
+
+    @Override
+        public boolean visibleInRegion(double wb, double nb, double eb, double sb, int i){
+           return true;
+           // double vw = (bbox[2] - bbox[0])/2d;
+           // double vh = (bbox[1] - bbox[3])/2d; 
+           // double cx = bbox[0] + vw;
+           // double cy = bbox[3] + vh; 
+           // return ((cx-vw)<=eb) && ((cx+vw)>=wb) && 
+           //     ((cy-vh)<=nb) && ((cy+vh)>=sb);
+        }
+
+    @Override 
+        public double getSize(){
+            //return (Math.sqrt((bbox[1] - bbox[3])*(bbox[1] - bbox[3]) + 
+            //            (bbox[2] - bbox[0])*(bbox[2] - bbox[0])));
+            return 0;
+        }
+
+}
+
