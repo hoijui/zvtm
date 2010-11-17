7c18f0538030cdab4feb0f1a991f250534091e21
Romain Primet <>
2010-11-17 09:13:37 +0000

    update prebuild script
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@3978 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-fits/trunk/prebuild.sh
+++ zvtm-fits/trunk/prebuild.sh
@@ -7,5 +7,6 @@ PREBUILD_ROOT=`pwd`
 cd `mktemp -d` &&\
 wget http://skyservice.pha.jhu.edu/develop/vo/ivoafits/ivoafits-0.3.jar &&\
 mvn install:install-file -DgroupId=ivoa.fits -DartifactId=fits -Dversion=0.3.0 -Dpackaging=jar -Dfile=ivoafits-0.3.jar &&\
-mvn install:install-file -DgroupId=jsky -DartifactId=jsky -Dversion=3.0 -Dpackaging=jar -Dfile=$PREBUILD_ROOT/lib/jsky-3.0.jar
+mvn install:install-file -DgroupId=jsky -DartifactId=jsky -Dversion=3.0 -Dpackaging=jar -Dfile=$PREBUILD_ROOT/lib/jsky-3.0.jar &&
+mvn install:install-file -DgroupId=hcompress -DartifactId=hcompress -Dversion=0.52 -Dpackaging=jar -Dfile=$PREBUILD_ROOT/lib/hcompress-0.52.jar
 
