696880169fccc18930a39065be21063487f729db
Romain Primet <>
2010-11-19 09:51:18 +0000

    modify WallCursor to allow more precise selection
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@3985 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-cluster-fits/trunk/src/main/java/fr/inria/zvtm/cluster/WallCursor.java
+++ zvtm-cluster-fits/trunk/src/main/java/fr/inria/zvtm/cluster/WallCursor.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.geom.Point2D;
 
 import fr.inria.zvtm.engine.VirtualSpace;
+import fr.inria.zvtm.glyphs.Composite;
 import fr.inria.zvtm.glyphs.SIRectangle;
 
 class WallCursor {
@@ -14,8 +15,9 @@ class WallCursor {
     private Color color;
     private final VirtualSpace target;
 
-    private SIRectangle hRect;
-    private SIRectangle vRect;
+    //private SIRectangle hRect;
+    //private SIRectangle vRect;
+    private Composite cursor;
 
     WallCursor(VirtualSpace target){
         this(target, 20, 160, Color.RED);
@@ -31,37 +33,57 @@ class WallCursor {
         this.length = length;
         this.color = color;
 
-        hRect = new SIRectangle(xPos, yPos, 0, length, thickness, color);
-        hRect.setDrawBorder(false);
-        vRect = new SIRectangle(xPos, yPos, 0, thickness, length, color);
-        vRect.setDrawBorder(false);
-        target.addGlyph(hRect);
-        target.addGlyph(vRect);
-        target.onTop(hRect, 0);
-        target.onTop(vRect, 0);
+        double qlength = length/4;
+
+        SIRectangle hRect1 = new SIRectangle(xPos-(qlength*3/2), yPos, 0, qlength, thickness, color);
+        SIRectangle hRect2 = new SIRectangle(xPos-(qlength/2), yPos, 0, qlength, 1, color);
+        SIRectangle hRect3 = new SIRectangle(xPos+(qlength/2), yPos, 0, qlength, 1, color);
+        SIRectangle hRect4 = new SIRectangle(xPos+(qlength*3/2), yPos, 0, qlength, thickness, color);
+        hRect1.setDrawBorder(false);
+        hRect2.setDrawBorder(false);
+        hRect3.setDrawBorder(false);
+        hRect4.setDrawBorder(false);
+
+        SIRectangle vRect1 = new SIRectangle(xPos, yPos-(qlength*3/2), 0, thickness, qlength, color);
+        SIRectangle vRect2 = new SIRectangle(xPos, yPos-(qlength/2), 0, 1, qlength, color);
+        SIRectangle vRect3 = new SIRectangle(xPos, yPos+(qlength/2), 0, 1, qlength, color);
+        SIRectangle vRect4 = new SIRectangle(xPos, yPos+(qlength*3/2), 0, thickness, qlength, color);
+        vRect1.setDrawBorder(false);
+        vRect2.setDrawBorder(false);
+        vRect3.setDrawBorder(false);
+        vRect4.setDrawBorder(false);
+
+        cursor = new Composite(); 
+        cursor.addChild(hRect1);
+        cursor.addChild(hRect2);
+        cursor.addChild(hRect3);
+        cursor.addChild(hRect4);
+        cursor.addChild(vRect1);
+        cursor.addChild(vRect2);
+        cursor.addChild(vRect3);
+        cursor.addChild(vRect4);
+        target.addGlyph(cursor);
+        target.onTop(cursor, 0);
     }
 
     void dispose(){
-        target.removeGlyph(hRect);
-        target.removeGlyph(vRect);
+        target.removeGlyph(cursor);
     }
 
     void onTop(int zIndex){
-        target.onTop(hRect, zIndex);
-        target.onTop(vRect, zIndex);
+        target.onTop(cursor, zIndex);
     }
 
     void moveTo(double x, double y){
         xPos = x;
         yPos = y;
-        hRect.moveTo(x, y);
-        vRect.moveTo(x, y);
+        cursor.moveTo(x, y);
     }
 
     Point2D.Double getPosition(){
         return new Point2D.Double(xPos, yPos);
     }
 
-    void setVisible(boolean v) { hRect.setVisible(v); vRect.setVisible(v);}
+    void setVisible(boolean v) { cursor.setVisible(v);}
 
 }
