bd34c9b25ec58a52e5c264fd4f7548186abef16b
Romain Primet <>
2011-01-03 13:43:36 +0000

    Factor duplicated code in method pickAndForward
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@4039 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-core/trunk/src/main/java/fr/inria/zvtm/event/SwingListener.java
+++ zvtm-core/trunk/src/main/java/fr/inria/zvtm/event/SwingListener.java
@@ -1,16 +1,10 @@
 package fr.inria.zvtm.event;
 
-import java.awt.AWTEvent;
-import java.awt.Color;
 import java.awt.Component;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Point2D;
 import javax.swing.SwingUtilities;
 
-import fr.inria.zvtm.engine.Camera;
-import fr.inria.zvtm.engine.View;
 import fr.inria.zvtm.engine.ViewPanel;
 import fr.inria.zvtm.engine.VirtualSpaceManager;
 import fr.inria.zvtm.glyphs.Glyph;
@@ -18,33 +12,25 @@ import fr.inria.zvtm.glyphs.VSwingComponent;
 
 public class SwingListener extends ViewAdapter {
     @Override public void press1(ViewPanel v, int mod, int jpx, int jpy, MouseEvent e){
-        //pick glyph under cursor, redispatch if it is an instance
-        //of VSwingComponent
-        Glyph[] pickList = v.getVCursor().getGlyphsUnderMouseList();
-        if(pickList.length == 0){
-            return;
-        }
-        Glyph pickedGlyph = pickList[pickList.length - 1];
-        if(pickedGlyph instanceof VSwingComponent){
-            redispatchMouse(v, e, (VSwingComponent)pickedGlyph);
-        }
+        pickAndForward(v, e);
     }
 
     @Override public void release1(ViewPanel v, int mod, int jpx, int jpy, MouseEvent e){
-        //pick glyph under cursor, redispatch if it is an instance
-        //of VSwingComponent
-        Glyph[] pickList = v.getVCursor().getGlyphsUnderMouseList();
-        if(pickList.length == 0){
-            return;
-        }
-        Glyph pickedGlyph = pickList[pickList.length - 1];
-        if(pickedGlyph instanceof VSwingComponent){
-            redispatchMouse(v, e, (VSwingComponent)pickedGlyph);
-        }
+      pickAndForward(v, e); 
     }
 
     @Override public void click1(ViewPanel v, int mod, int jpx, int jpy, int clickNumber, MouseEvent e){
-        //pick glyph under cursor, redispatch if it is an instance
+      pickAndForward(v, e); 
+    }
+    @Override public void click2(ViewPanel v, int mod, int jpx, int jpy, int clickNumber, MouseEvent e){
+      pickAndForward(v, e); 
+    }
+    @Override public void click3(ViewPanel v, int mod, int jpx, int jpy, int clickNumber, MouseEvent e){
+      pickAndForward(v, e); 
+    }
+    
+	private void pickAndForward(ViewPanel v, MouseEvent e) {
+		//pick glyph under cursor, redispatch if it is an instance
         //of VSwingComponent
         Glyph[] pickList = v.getVCursor().getGlyphsUnderMouseList();
         if(pickList.length == 0){
@@ -54,27 +40,7 @@ public class SwingListener extends ViewAdapter {
         if(pickedGlyph instanceof VSwingComponent){
             redispatchMouse(v, e, (VSwingComponent)pickedGlyph);
         }
-    }
-    @Override public void click2(ViewPanel v, int mod, int jpx, int jpy, int clickNumber, MouseEvent e){
-        Glyph[] pickList = v.getVCursor().getGlyphsUnderMouseList();
-        if(pickList.length == 0){
-            return;
-        }
-        Glyph pickedGlyph = pickList[pickList.length - 1];
-        if(pickedGlyph instanceof VSwingComponent){
-            redispatchMouse(v, e, (VSwingComponent)pickedGlyph);
-        }
-    }
-    @Override public void click3(ViewPanel v, int mod, int jpx, int jpy, int clickNumber, MouseEvent e){
-        Glyph[] pickList = v.getVCursor().getGlyphsUnderMouseList();
-        if(pickList.length == 0){
-            return;
-        }
-        Glyph pickedGlyph = pickList[pickList.length - 1];
-        if(pickedGlyph instanceof VSwingComponent){
-            redispatchMouse(v, e, (VSwingComponent)pickedGlyph);
-        }
-    }
+	}
 
     void redispatchMouse(ViewPanel v, final MouseEvent evt, VSwingComponent c){
         //we have an event location in View coordinates
