f82f50576a6902f617429f26e5c0a04bdc0ee04e
Julien Husson <>
2011-01-31 17:17:44 +0000

    Added TranslucentJLabel class
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@4130 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ zvtm-core/trunk/src/main/java/fr/inria/zvtm/widgets/TranslucentJLabel.java
@@ -0,0 +1,100 @@
+package fr.inria.halz.view;
+
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.Icon;
+import javax.swing.JLabel;
+
+import fr.inria.zvtm.widgets.TranslucentWidget;
+
+public class TranslucentJLabel extends JLabel implements TranslucentWidget {
+
+	AlphaComposite bgAC = AB_08;
+	AlphaComposite fgAC = AB_10;
+
+	public TranslucentJLabel() {
+		super();
+		init();
+	}
+
+	public TranslucentJLabel(Icon image) {
+		super();
+		init();
+	}
+
+	// Creates a JLabel instance with the specified image.
+	public TranslucentJLabel(Icon image, int horizontalAlignment) {
+		super(image, horizontalAlignment);
+		init();
+	}
+
+	// Creates a JLabel instance with the specified image and horizontal
+	// alignment.
+	public TranslucentJLabel(String text) {
+		super(text);
+		init();
+	}
+
+	// Creates a JLabel instance with the specified text.
+	public TranslucentJLabel(String text, Icon icon, int horizontalAlignment) {
+		super(text, icon, horizontalAlignment);
+		init();
+
+	}
+
+	// Creates a JLabel instance with the specified text, image, and horizontal
+	// alignment.
+	public TranslucentJLabel(String text, int horizontalAlignment) {
+		super(text, horizontalAlignment);
+		init();
+	}
+
+	// Creates a JLabel instance with the specified text and horizontal
+	// alignment.
+
+	void init() {
+		setOpaque(false);
+		initColors();
+	}
+
+	void initColors() {
+		setForeground(Color.WHITE);
+		setBackground(Color.BLACK);
+	}
+
+	/**
+	 * Set the translucence value of this text area's background.
+	 * 
+	 * @param alpha
+	 *            blending value, in [0.0,1.0]. Default is 0.8
+	 */
+	public void setBackgroundTranslucence(float alpha) {
+		this.bgAC = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha);
+	}
+
+	/**
+	 * Set the translucence value of this text area's foreground.
+	 * 
+	 * @param alpha
+	 *            blending value, in [0.0,1.0]. Default is 1.0
+	 */
+	public void setForegroundTranslucence(float alpha) {
+		this.fgAC = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha);
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setComposite(bgAC);
+		g2d.setColor(getBackground());
+		g2d.fillRect(0, 0, getWidth(), getHeight());
+		g2d.setComposite(fgAC);
+		g2d.setColor(getForeground());
+		g2d.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
+		super.paint(g2d);
+	}
+
+}
