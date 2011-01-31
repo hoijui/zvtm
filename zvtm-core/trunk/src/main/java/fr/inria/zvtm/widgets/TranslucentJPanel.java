b7b3f8378ad767ca20c2c0114be35914db584f64
Julien Husson <>
2011-01-31 13:02:23 +0000

    Added TranslucentJPanel class
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@4129 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ zvtm-core/trunk/src/main/java/fr/inria/zvtm/widgets/TranslucentJPanel.java
@@ -0,0 +1,66 @@
+package fr.inria.halz.view;
+
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.Vector;
+
+import javax.swing.JPanel;
+import javax.swing.ListModel;
+
+import fr.inria.zvtm.widgets.TranslucentWidget;
+
+public class TranslucentJPanel extends JPanel implements TranslucentWidget {
+
+	AlphaComposite bgAC = AB_08;
+	AlphaComposite fgAC = AB_10;
+
+	public TranslucentJPanel() {
+		super();
+		init();
+	}
+
+	void init() {
+		setOpaque(false);
+		initColors();
+	}
+
+	void initColors() {
+		setForeground(Color.WHITE);
+		setBackground(Color.BLACK);
+	}
+
+	/**
+	 * Set the translucence value of this text area's background.
+	 * 
+	 * @param alpha
+	 *            blending value, in [0.0,1.0]. Default is 0.8
+	 */
+	public void setBackgroundTranslucence(float alpha) {
+		this.bgAC = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha);
+	}
+
+	/**
+	 * Set the translucence value of this text area's foreground.
+	 * 
+	 * @param alpha
+	 *            blending value, in [0.0,1.0]. Default is 1.0
+	 */
+	public void setForegroundTranslucence(float alpha) {
+		this.fgAC = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha);
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setComposite(bgAC);
+		g2d.setColor(getBackground());
+		g2d.fillRect(0, 0, getWidth(), getHeight());
+		g2d.setComposite(fgAC);
+		g2d.setColor(getForeground());
+		g2d.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
+		super.paint(g2d);
+	}
+	
+}
