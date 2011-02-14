8ccf7fd51c422dd6534efffb43066f343fb65fb9
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2011-02-14 10:02:18 +0000

    javadoc
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@4161 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-core/trunk/publish_api.sh
+++ zvtm-core/trunk/publish_api.sh
@@ -2,7 +2,7 @@
 cd target/site
 tar -cvf api.tar apidocs/*
 gzip -c api.tar > api.tgz
-scp api.tgz epietrig,zvtm@web.sourceforge.net:/home/groups/z/zv/zvtm/htdocs/
+scp api.tgz epietrig,zvtm@web.sourceforge.net:/home/project-web/zvtm/htdocs/
 rm api.tgz
 rm api.tar
 
