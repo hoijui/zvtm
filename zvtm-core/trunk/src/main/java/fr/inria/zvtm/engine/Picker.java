fa86a14440a266475ef384909a44fed72afe3b00
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2011-02-16 12:35:24 +0000

    externalizing picking
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@4200 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ zvtm-core/trunk/src/main/java/fr/inria/zvtm/engine/Picker.java
@@ -0,0 +1,14 @@
+/*   AUTHOR :           Emmanuel Pietriga (emmanuel.pietriga@inria.fr)
+ *   Copyright (c) INRIA, 2011. All Rights Reserved
+ *   Licensed under the GNU LGPL. For full terms see the file COPYING.
+ *
+ * $Id$
+ */
+
+package fr.inria.zvtm.engine;
+
+public class Picker {
+    
+    
+    
+}
