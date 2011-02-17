915d8295d43eacb4e8df67baaf615758f3aecfc9
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2011-02-17 13:55:50 +0000

    API update
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@4213 74e165f3-c697-40a6-91af-552c8936d7fe

--- doc/developer_guide/publish_devguide.sh
+++ doc/developer_guide/publish_devguide.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-scp main.pdf epietrig,zvtm@web.sourceforge.net:/home/groups/z/zv/zvtm/htdocs/doc/zvtm_dev_guide.pdf
+scp main.pdf epietrig,zvtm@web.sourceforge.net:/home/project-web/zvtm/htdocs/doc/zvtm_dev_guide.pdf
 
