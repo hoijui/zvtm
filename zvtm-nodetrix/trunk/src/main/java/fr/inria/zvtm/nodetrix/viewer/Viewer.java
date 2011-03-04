b7fce89f355cf5dc636df0d48b6125a40006082f
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2011-03-04 12:46:44 +0000

    drawing surface now wrapped in ViewPanel (no longer extends it)
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@4319 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-nodetrix/trunk/src/main/java/fr/inria/zvtm/nodetrix/viewer/Viewer.java
+++ zvtm-nodetrix/trunk/src/main/java/fr/inria/zvtm/nodetrix/viewer/Viewer.java
@@ -122,7 +122,7 @@ public class Viewer {
         mView.setNotifyCursorMoved(true);
         mView.setAntialiasing(antialiased);
         mView.setBackgroundColor(ConfigManager.BACKGROUND_COLOR);
-		mView.getPanel().addComponentListener(eh);
+		mView.getPanel().getComponent().addComponentListener(eh);
 		ComponentAdapter ca0 = new ComponentAdapter(){
 			public void componentResized(ComponentEvent e){
 				updatePanelSize();
@@ -145,7 +145,7 @@ public class Viewer {
     }
     
     void updatePanelSize(){
-        Dimension d = mView.getPanel().getSize();
+        Dimension d = mView.getPanel().getComponent().getSize();
         panelWidth = d.width;
 		panelHeight = d.height;
 		nm.updateOverviewLocation();
