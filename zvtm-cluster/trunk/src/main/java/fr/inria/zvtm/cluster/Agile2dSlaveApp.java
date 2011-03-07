298a10b41a2eeadf819c6070ded9694fe18e61c9
Romain Primet <>
2011-03-07 15:57:45 +0000

    supply main method for Agile2dSlaveApp
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@4334 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-cluster/trunk/src/main/java/fr/inria/zvtm/cluster/Agile2dSlaveApp.java
+++ zvtm-cluster/trunk/src/main/java/fr/inria/zvtm/cluster/Agile2dSlaveApp.java
@@ -2,6 +2,13 @@ package fr.inria.zvtm.cluster;
 
 import fr.inria.zvtm.engine.AgilePanelType;
 import fr.inria.zvtm.engine.View;
+import fr.inria.zvtm.engine.VirtualSpaceManager;
+
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
 
 public class Agile2dSlaveApp extends SlaveApp {
     public Agile2dSlaveApp(SlaveOptions options){
@@ -15,6 +22,30 @@ public class Agile2dSlaveApp extends SlaveApp {
 
     @Override protected String getViewType(){
         return AgilePanelType.AGILE_VIEW;
+    }
+
+    public static void main(String[] args){
+        SlaveOptions options = new SlaveOptions();
+        CmdLineParser parser = new CmdLineParser(options);
+        try{
+            parser.parseArgument(args);
+        } catch(CmdLineException ex){
+            System.err.println(ex.getMessage());
+            parser.printUsage(System.err);
+            return;
+        }
+
+        if(options.help){
+            System.err.println("Usage: Agile2dSlaveApp [options] where options are: ");
+            parser.printUsage(System.err);
+            return;
+        }
+
+        SlaveApp app = new Agile2dSlaveApp(options);
+        SlaveUpdater updater = new SlaveUpdater(options.appName,
+                options.blockNumber);
+        updater.setAppDelegate(app);
+        updater.startOperation();
     }
 }
 
