553c27359da7e5a531309d7f0944c538f69f1437
Romain Primet <>
2011-03-07 15:16:05 +0000

    version number
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@4333 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-cluster/trunk/src/main/resources/scripts/local_slave.sh
+++ zvtm-cluster/trunk/src/main/resources/scripts/local_slave.sh
@@ -3,7 +3,7 @@
 
 DEPS_CP=`mvn org.apache.maven.plugins:maven-dependency-plugin:2.0:build-classpath -DexcludeArtifactIds="zvtm" | grep jgroups`
 
-ZVTM_CLUSTER_VERSION="0.2.5-SNAPSHOT"
+ZVTM_CLUSTER_VERSION="0.2.6-SNAPSHOT"
 
 java -Djgroups.bind_addr="127.0.0.1" -Djava.net.preferIPv4Stack=true -cp .:target/zvtm-cluster-"$ZVTM_CLUSTER_VERSION".jar:$DEPS_CP fr.inria.zvtm.cluster.SlaveApp -b 0 "$@"
 
