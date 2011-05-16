04fd9e5d29922370f227e15e6c8a70a4019a980e
Julien Altieri <>
2011-05-16 14:17:05 +0000

    add MetisseViewer and MetisseMain
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@4447 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ zvtm-metisse-cluster/trunk/src/main/java/fr/inria/zvtm/cluster/MetisseViewer.java
@@ -0,0 +1,48 @@
+package fr.inria.zvtm.cluster;
+
+import java.awt.Color;
+import java.util.Vector;
+
+import fr.inria.zvtm.cluster.ClusterGeometry;
+import fr.inria.zvtm.cluster.ClusteredView;
+import fr.inria.zvtm.engine.Camera;
+import fr.inria.zvtm.gui.Viewer;
+
+
+public class MetisseViewer extends Viewer {
+	public MetisseViewer(boolean isClient){
+		super(isClient);
+	}
+
+	@Override protected void preInitHook(){
+		if(!isClient)vsm.setMaster("intothewild");
+	}
+
+	@Override protected void viewCreatedHook(Vector<Camera> cameras){
+		if(!isClient){
+			if(!MetisseMain.CLUSTERMODE)return;
+			if(MetisseMain.SMALLMODE){
+				ClusterGeometry clGeom = new ClusterGeometry(600,400,2,2);
+				//                options.blockWidth,
+				//                options.blockHeight,
+				//                options.numCols,
+				//                options.numRows);
+				ClusteredView cv = new ClusteredView(clGeom,1,2,2,cameras);
+				cv.setBackgroundColor(Color.LIGHT_GRAY);
+				vsm.addClusteredView(cv);
+			}
+			else{
+				ClusterGeometry clGeom = new ClusterGeometry(2560,1600,8,4);
+				ClusteredView cv = new ClusteredView(clGeom,3,8,4,cameras);
+				//                    options.numRows-1, //origin (block number)
+				//                    options.numCols, //use complete
+				//                    options.numRows, //cluster surface
+				//                    cameras);
+				cv.setBackgroundColor(Color.LIGHT_GRAY);
+				vsm.addClusteredView(cv);
+			}
+		}
+
+	}
+}
+
