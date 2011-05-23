e2e476dc892bd2975fb0d71ad39b5fdd28ac2fd2
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2011-05-23 09:56:19 +0000

    svn kwd id
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@4460 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-core/trunk/src/main/java/fr/inria/zvtm/event/SwingListener.java
+++ zvtm-core/trunk/src/main/java/fr/inria/zvtm/event/SwingListener.java
@@ -3,7 +3,7 @@
  *  (c) COPYRIGHT INRIA (Institut National de Recherche en Informatique et en Automatique), 2011.
  *  Licensed under the GNU LGPL. For full terms see the file COPYING.
  *
- * $Id:  $
+ * $Id$
  */
 package fr.inria.zvtm.event;
 
