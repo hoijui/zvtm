4ebf46706cd87b70aa416957f6f38d6f6b46580b
Julien Altieri <>
2011-06-24 07:34:51 +0000

    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@4543 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-metisse-cluster/trunk/launch_master.sh
+++ zvtm-metisse-cluster/trunk/launch_master.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -Djgroups.bind_addr="192.168.0.52" -Djava.net.preferIPv4Stack=True -cp .:target/args4j-2.0.12.jar:target/aspectjrt-1.6.2.jar:target/commons-logging-1.1.jar:target/jgroups-2.7.0.GA.jar:target/log4j-1.2.14.jar:target/slf4j-api-1.5.9-RC0.jar:target/slf4j-log4j12-1.5.9-RC0.jar:target/timingframework-1.0.jar:target/zvtm-cluster-0.2.6-SNAPSHOT.jar:target/zvtm-metisse-cluster-0.0.2-SNAPSHOT.jar:target/javaosc-1.0.jar fr.inria.zvtm.cluster.MetisseMain 1 0
+java -Xmx4g -Djgroups.bind_addr="192.168.0.2" -Djava.net.preferIPv4Stack=True -cp .:target/args4j-2.0.12.jar:target/aspectjrt-1.6.2.jar:target/commons-logging-1.1.jar:target/jgroups-2.7.0.GA.jar:target/log4j-1.2.14.jar:target/slf4j-api-1.5.9-RC0.jar:target/slf4j-log4j12-1.5.9-RC0.jar:target/timingframework-1.0.jar:target/zvtm-cluster-0.2.6-SNAPSHOT.jar:target/zvtm-metisse-cluster-0.0.2-SNAPSHOT.jar:target/javaosc-1.0.jar fr.inria.zvtm.cluster.MetisseMain 1 0
 
