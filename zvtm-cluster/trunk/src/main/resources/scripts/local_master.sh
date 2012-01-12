1c9c2129c081da5146954931a5f70fc5172882a5
Romain Primet <>
2012-01-12 11:40:48 +0000

    update local run scripts
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@4700 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-cluster/trunk/src/main/resources/scripts/local_master.sh
+++ zvtm-cluster/trunk/src/main/resources/scripts/local_master.sh
@@ -3,7 +3,7 @@
 
 DEPS_CP=`mvn org.apache.maven.plugins:maven-dependency-plugin:2.0:build-classpath -DexcludeArtifactIds="zvtm" | grep jgroups`
 
-ZVTM_CLUSTER_VERSION="0.2.6-SNAPSHOT"
+ZVTM_CLUSTER_VERSION="0.2.7-SNAPSHOT"
 CLASSNAME="fr.inria.zvtm.cluster.examples.VImageExample"
 
 java -Djgroups.bind_addr="127.0.0.1" -Djava.net.preferIPv4Stack=true -cp .:target/zvtm-cluster-"$ZVTM_CLUSTER_VERSION".jar:$DEPS_CP "$CLASSNAME" "$@"
