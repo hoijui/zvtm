f1f138cab3556483e270a88b63bd716a8b275fed
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2012-10-22 18:26:37 +0000

    (c) statement
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@4867 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-core/trunk/src/main/java/fr/inria/zvtm/widgets/MenuItem.java
+++ zvtm-core/trunk/src/main/java/fr/inria/zvtm/widgets/MenuItem.java
@@ -1,8 +1,16 @@
+/*   Copyright (c) INRIA, 2011-2012. All Rights Reserved
+ *   Licensed under the GNU LGPL. For full terms see the file COPYING.
+ *
+ * $Id$
+ */
+
 package fr.inria.zvtm.widgets;
 
 /**
  * A menu item.
+ *@author R.Primet, INRIA
  */
+
 public class MenuItem<T> {
     private final String text;
     private final T userObject;
