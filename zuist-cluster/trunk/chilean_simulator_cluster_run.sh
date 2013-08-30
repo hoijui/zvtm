6d6a3d25e9cd0b7b28b7d56be4647e69e8b4e9c6
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2013-08-30 15:44:56 +0000

    scripts to launch Inria Chile's wall simulator
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@4994 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ zuist-cluster/trunk/chilean_simulator_cluster_run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function colNum {
+  case "$1" in
+	  "a" ) return 0;;
+	  "b" ) return 1;;
+	  "c" ) return 2;;
+  esac
+}
+
+#start client nodes
+for col in {a..c}
+do
+	for row in {1..4}
+      do
+		  colNum $col
+		  SLAVENUM1=`expr $? \* 8 + $row - 1`
+		  SLAVENUM2=`expr $SLAVENUM1 + 4`
+		  colNum $col
+		  X1=`expr $? \* 400`
+		  colNum $col
+		  X2=`expr $? \* 400 + 200`
+		  Y=`expr $row \* 112`
+		  java -XX:+DoEscapeAnalysis -XX:+UseConcMarkSweepGC -Xmx512m -cp target/commons-logging-1.1.jar:target/args4j-2.0.23.jar:target/aspectjrt-1.6.2.jar:target/jgroups-2.7.0.GA.jar:target/log4j-1.2.14.jar:target/slf4j-api-1.5.9-RC0.jar:target/slf4j-log4j12-1.5.9-RC0.jar:target/timingframework-1.0.jar:target/zvtm-cluster-0.2.8-SNAPSHOT.jar:target/zuist-cluster-0.2.1-SNAPSHOT.jar fr.inria.zvtm.cluster.SlaveApp -u -b $SLAVENUM1 -x $X1 -y $Y -n ZuistCluster $* &
+		  java -XX:+DoEscapeAnalysis -XX:+UseConcMarkSweepGC -Xmx512m -cp target/commons-logging-1.1.jar:target/args4j-2.0.23.jar:target/aspectjrt-1.6.2.jar:target/jgroups-2.7.0.GA.jar:target/log4j-1.2.14.jar:target/slf4j-api-1.5.9-RC0.jar:target/slf4j-log4j12-1.5.9-RC0.jar:target/timingframework-1.0.jar:target/zvtm-cluster-0.2.8-SNAPSHOT.jar:target/zuist-cluster-0.2.1-SNAPSHOT.jar fr.inria.zvtm.cluster.SlaveApp -u -b $SLAVENUM2 -x $X2 -y $Y -n ZuistCluster $* &
+      done
+done
