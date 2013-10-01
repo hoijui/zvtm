e65974abcff00ba5062f09723e8096194a3d29cc
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2013-10-01 18:38:36 +0000

    additional test config
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@5017 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-cluster-basicui/trunk/src/main/java/fr/inria/zvtm/basicui/WildViewer.java
+++ zvtm-cluster-basicui/trunk/src/main/java/fr/inria/zvtm/basicui/WildViewer.java
@@ -31,76 +31,88 @@ import fr.inria.zvtm.glyphs.VRectangle;
 
 public class WildViewer {
 
-	static final int VIEW_W = 1024;
-	static final int VIEW_H = 768;
+    static final int VIEW_W = 1024;
+    static final int VIEW_H = 768;
 
-	VirtualSpaceManager vsm = VirtualSpaceManager.INSTANCE;
-	VirtualSpace mSpace;
-	Camera mCamera;
-	View mView;
-	ViewListener eh;
+    VirtualSpaceManager vsm = VirtualSpaceManager.INSTANCE;
+    VirtualSpace mSpace;
+    Camera mCamera;
+    View mView;
+    ViewListener eh;
 
-	static final short CONFIG_WALL = 0;
-	static final short CONFIG_SIMULATOR = 1;
-	static final short CONFIG_SINGLE = 2;
+    static final short CONFIG_WALL = 0;
+    static final short CONFIG_SIMULATOR = 1;
+    static final short CONFIG_SINGLE = 2;
+    static final short CONFIG_MINIWALL = 3;
 
-	public WildViewer(short config){
+    public WildViewer(short config){
         vsm.setMaster("WildViewer");
         mSpace = vsm.addVirtualSpace("mainSpace");
         mCamera = mSpace.addCamera();
         Vector cameras = new Vector();
         cameras.add(mCamera);
-		// local view (on master)
+        // local view (on master)
         mView = vsm.addFrameView(cameras, "zvtm-cluster-basicui", View.STD_VIEW, VIEW_W, VIEW_H, true);
-		mView.setAntialiasing(true);
+        mView.setAntialiasing(true);
         eh = new WildViewerEvtHld(this);
         mView.setListener(eh, 0);
         mView.setBackgroundColor(Color.BLACK);
-		mView.getCursor().setColor(Color.WHITE);
-		mView.getCursor().setHintColor(Color.WHITE);
+        mView.getCursor().setColor(Color.WHITE);
+        mView.getCursor().setHintColor(Color.WHITE);
         // clustered view (wall)
-		if (config == CONFIG_WALL){
-		    // wall made of 6 x 4 32" displays (1920x1080 each, with approx 100px bezels -- overlay mode)
-			ClusterGeometry cg = new ClusterGeometry(2120, 1280, 6, 4);
-	        Vector ccameras = new Vector();
-	        ccameras.add(mCamera);
-	        ClusteredView cv = new ClusteredView(cg, 3, 6, 4, ccameras);
-	        vsm.addClusteredView(cv);
-		}
-		else if (config == CONFIG_SIMULATOR){
-			// simulator on a single machine, with 6x4 small windows pretending to be different tiles
-			ClusterGeometry cg = new ClusterGeometry(200, 112, 6, 4);
-	        Vector ccameras = new Vector();
-	        ccameras.add(mCamera);
-	        ClusteredView cv = new ClusteredView(cg, 3, 6, 4, ccameras);
-	        vsm.addClusteredView(cv);
-		}
-		else if (config == CONFIG_SINGLE){
-	        ClusterGeometry cg = new ClusterGeometry(800, 600, 1, 1);
-	        Vector ccameras = new Vector();
-	        ccameras.add(mCamera);
-	        ClusteredView cv = new ClusteredView(cg, 0, 1, 1, ccameras);
-	        vsm.addClusteredView(cv);
-		}
-	}
+        if (config == CONFIG_WALL){
+            // wall made of 6 x 4 32" displays (1920x1080 each, with approx 100px bezels -- overlay mode)
+            ClusterGeometry cg = new ClusterGeometry(2120, 1280, 6, 4);
+            Vector ccameras = new Vector();
+            ccameras.add(mCamera);
+            ClusteredView cv = new ClusteredView(cg, 3, 6, 4, ccameras);
+            vsm.addClusteredView(cv);
+        }
 
-	void init(){
-		mSpace.addGlyph(new VCircle(0, 0 , 0, 400, Color.RED));
-		for (float angle=0;angle<2*Math.PI;){
-			mSpace.addGlyph(new VRectangle(400*Math.cos(angle), 400*Math.sin(angle), 10, 20, 20, Color.WHITE));
-			angle += Math.PI/12d;
-		}
-	}
+        else if (config == CONFIG_MINIWALL){
+            // wall made of 2 x 2 30" displays (1920x1080 each, with approx 100px bezels -- overlay mode)
+            ClusterGeometry cg = new ClusterGeometry(2120, 1280, 2, 2);
+            Vector ccameras = new Vector();
+            ccameras.add(mCamera);
+            ClusteredView cv = new ClusteredView(cg, 1, 2, 2, ccameras);
+            vsm.addClusteredView(cv);
+        }
 
-	public static void main(String[] args){
-		short cfg = CONFIG_WALL;
-		for (String arg:args){
-			if (arg.equals("-single")){cfg = CONFIG_SINGLE;}
-			else if (arg.equals("-simulator")){cfg = CONFIG_SIMULATOR;}
-			else if (arg.equals("-wall")){cfg = CONFIG_WALL;}
-		}
-		(new WildViewer(cfg)).init();
-	}
+        else if (config == CONFIG_SIMULATOR){
+            // simulator on a single machine, with 6x4 small windows pretending to be different tiles
+            ClusterGeometry cg = new ClusterGeometry(200, 112, 6, 4);
+            Vector ccameras = new Vector();
+            ccameras.add(mCamera);
+            ClusteredView cv = new ClusteredView(cg, 3, 6, 4, ccameras);
+            vsm.addClusteredView(cv);
+        }
+        else if (config == CONFIG_SINGLE){
+            ClusterGeometry cg = new ClusterGeometry(800, 600, 1, 1);
+            Vector ccameras = new Vector();
+            ccameras.add(mCamera);
+            ClusteredView cv = new ClusteredView(cg, 0, 1, 1, ccameras);
+            vsm.addClusteredView(cv);
+        }
+    }
+
+    void init(){
+        mSpace.addGlyph(new VCircle(0, 0 , 0, 400, Color.RED));
+        for (float angle=0;angle<2*Math.PI;){
+            mSpace.addGlyph(new VRectangle(400*Math.cos(angle), 400*Math.sin(angle), 10, 20, 20, Color.WHITE));
+            angle += Math.PI/12d;
+        }
+    }
+
+    public static void main(String[] args){
+        short cfg = CONFIG_WALL;
+        for (String arg:args){
+            if (arg.equals("-single")){cfg = CONFIG_SINGLE;}
+            else if (arg.equals("-simulator")){cfg = CONFIG_SIMULATOR;}
+            else if (arg.equals("-mw")){cfg = CONFIG_MINIWALL;}
+            else if (arg.equals("-wall")){cfg = CONFIG_WALL;}
+        }
+        (new WildViewer(cfg)).init();
+    }
 
 }
 
@@ -130,22 +142,22 @@ class WildViewerEvtHld extends ViewAdapter {
         v.setDrawDrag(true);
     }
 
-	public void release1(ViewPanel v, int mod, int jpx, int jpy, MouseEvent e){
-	    application.mCamera.setXspeed(0);
+    public void release1(ViewPanel v, int mod, int jpx, int jpy, MouseEvent e){
+        application.mCamera.setXspeed(0);
         application.mCamera.setYspeed(0);
         application.mCamera.setZspeed(0);
         v.setDrawDrag(false);
         panning = false;
-	}
+    }
 
     public void mouseDragged(ViewPanel v, int mod, int buttonNumber, int jpx, int jpy, MouseEvent e){
-		if (panning){
+        if (panning){
             Camera c = v.cams[0];
-			double a = (c.focal+Math.abs(c.altitude))/c.focal;
-			application.mCamera.setXspeed((long)((jpx-lastJPX)*(a/PAN_SPEED_COEF)));
-			application.mCamera.setYspeed((long)((lastJPY-jpy)*(a/PAN_SPEED_COEF)));
-			application.mCamera.setZspeed(0);
-		}
+            double a = (c.focal+Math.abs(c.altitude))/c.focal;
+            application.mCamera.setXspeed((long)((jpx-lastJPX)*(a/PAN_SPEED_COEF)));
+            application.mCamera.setYspeed((long)((lastJPY-jpy)*(a/PAN_SPEED_COEF)));
+            application.mCamera.setZspeed(0);
+        }
     }
 
     public void mouseWheelMoved(ViewPanel v, short wheelDirection, int jpx, int jpy, MouseWheelEvent e){
@@ -162,12 +174,12 @@ class WildViewerEvtHld extends ViewAdapter {
         }
     }
 
-	public void enterGlyph(Glyph g){
-	    g.highlight(true, null);
-	}
+    public void enterGlyph(Glyph g){
+        g.highlight(true, null);
+    }
 
-	public void exitGlyph(Glyph g){
-	    g.highlight(false, null);
-	}
+    public void exitGlyph(Glyph g){
+        g.highlight(false, null);
+    }
 
 }
