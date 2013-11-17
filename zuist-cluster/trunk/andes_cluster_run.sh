9780466b7313a9c30f8b6e7a8bdf35dea02e91a8
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2013-11-17 15:26:53 +0000

    ANDEScreen launch scripts
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@5033 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ zuist-cluster/trunk/andes_cluster_run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function colNum {
+  case "$1" in
+          "a" ) return 0;;
+          "b" ) return 2;;
+          "c" ) return 4;;
+  esac
+}
+
+#start client nodes
+for col in {a..c}
+do
+        for row in {1..4}
+      do
+                  colNum $col
+		  #echo `expr $? \* 4 + $row - 1`
+		  SLAVENUM1=`expr $? \* 4 + $row - 1`
+      SLAVENUM2=`expr $SLAVENUM1 + 4`
+      #ssh wall@$col$row.wall.inria.cl -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "sudo sysctl -w kern.ipc.maxsockbuf=80000000"
+      #ssh wall@$col$row.wall.inria.cl -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "sudo sysctl -w net.inet.tcp.recvspace=40000000"
+      #ssh wall@$col$row.wall.inria.cl -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "sudo sysctl -w net.inet.tcp.sendspace=40000000"
+      ssh wall@$col$row.wall.inria.cl -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "export DISPLAY=:0.0 && cd /home/wall/zvtm-code/zuist-cluster/trunk && java -XX:+DoEscapeAnalysis -XX:+UseConcMarkSweepGC -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr="\"$col$row.wall.inria.cl\"" -Xmx4g -cp target/commons-logging-1.1.jar:target/args4j-2.0.23.jar:target/aspectjrt-1.6.2.jar:target/jgroups-2.7.0.GA.jar:target/log4j-1.2.14.jar:target/slf4j-api-1.5.9-RC0.jar:target/slf4j-log4j12-1.5.9-RC0.jar:target/timingframework-1.0.jar:target/zvtm-cluster-0.2.8-SNAPSHOT.jar:target/zuist-cluster-0.2.1-SNAPSHOT.jar fr.inria.zvtm.cluster.SlaveApp -n ZuistCluster -b $SLAVENUM1 -f" $* &
+      ssh wall@$col$row.wall.inria.cl -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "export DISPLAY=:0.1 &&  cd /home/wall/zvtm-code/zuist-cluster/trunk && java -XX:+DoEscapeAnalysis -XX:+UseConcMarkSweepGC -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr="\"$col$row.wall.inria.cl\"" -Xmx4g -cp target/commons-logging-1.1.jar:target/args4j-2.0.23.jar:target/aspectjrt-1.6.2.jar:target/jgroups-2.7.0.GA.jar:target/log4j-1.2.14.jar:target/slf4j-api-1.5.9-RC0.jar:target/slf4j-log4j12-1.5.9-RC0.jar:target/timingframework-1.0.jar:target/zvtm-cluster-0.2.8-SNAPSHOT.jar:target/zuist-cluster-0.2.1-SNAPSHOT.jar fr.inria.zvtm.cluster.SlaveApp -n ZuistCluster -b $SLAVENUM2 -f" $* &
+     done
+done
