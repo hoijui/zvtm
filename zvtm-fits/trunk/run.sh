cb8779b49b4245638be3476d51e1a383dc3d8275
Fernando del Campo <>
2014-04-23 21:33:43 +0000

    0.1.3 version
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@5114 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-fits/trunk/run.sh
+++ zvtm-fits/trunk/run.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-java -Xmx2048M -Xms512M -cp .:target/zvtm-core-0.11.0-SNAPSHOT.jar:target/zvtm-fits-0.1.3-SNAPSHOT.jar:target/jsky-3.0.jar:target/hcompress-0.52.jar fr.inria.zvtm.fits.examples.JSkyFitsExample "$@" 
+#java -Xmx2048M -Xms512M -cp .:target/zvtm-core-0.11.0-SNAPSHOT.jar:target/zvtm-fits-0.1.3-SNAPSHOT.jar:target/jsky-3.0.jar:target/hcompress-0.52.jar fr.inria.zvtm.fits.examples.JSkyFitsExample "$@" 
+java -Xmx2048M -Xms512M -cp .:target/zvtm-core-0.11.0-SNAPSHOT.jar:target/zvtm-fits-0.1.3-SNAPSHOT.jar:target/jsky-3.0.jar:target/hcompress-0.52.jar fr.inria.zvtm.fits.examples.FitsExample "$@" 
