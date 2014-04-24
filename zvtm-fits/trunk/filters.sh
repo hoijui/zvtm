40ffe1e03eebd418ce7558ae6eeb76594f785d12
Fernando del Campo <>
2014-04-24 14:51:32 +0000

    Changed script about the version 0.1.4-SNAPSHOT
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@5117 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-fits/trunk/filters.sh
+++ zvtm-fits/trunk/filters.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp target/zvtm-fits-0.1.3-SNAPSHOT.jar:target/zvtm-core-0.11.0-SNAPSHOT.jar:target/timingframework-1.0.jar fr.inria.zvtm.fits.filters.FilterVisualizer "$@" 
+java -cp target/zvtm-fits-0.1.4-SNAPSHOT.jar:target/zvtm-core-0.11.2-SNAPSHOT.jar:target/timingframework-1.0.jar fr.inria.zvtm.fits.filters.FilterVisualizer "$@" 
