cfa3202af4072b1a6a53f05bf4aa2b7f462bec79
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2014-10-01 12:10:42 +0000

    only one display per computer
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@5239 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-cluster-calibrator/trunk/wilder_cluster_run.sh
+++ zvtm-cluster-calibrator/trunk/wilder_cluster_run.sh
@@ -14,18 +14,20 @@ function colIP {
   esac
 }
 
-#start client nodes
-for col in {a..b}
-do
+#start client nodes A
   for row in {1..5}
     do
-      colNum $col
-      SLAVENUM1=`expr $? \* 5 + $row - 1`
-      SLAVENUM2=`expr $SLAVENUM1 + 5`
-      colIP $col
-      ssh wild@192.168.2.$?$row -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "export DISPLAY=:0.0 && cd /home/wild/zvtm-cluster-calibrator/trunk && java -XX:+DoEscapeAnalysis -XX:+UseConcMarkSweepGC -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr="\"192.168.2.$?$row\"" -Xmx4g -cp target/commons-logging-1.1.jar:target/args4j-2.0.23.jar:target/aspectjrt-1.6.2.jar:target/jgroups-2.7.0.GA.jar:target/log4j-1.2.14.jar:target/slf4j-api-1.5.9-RC0.jar:target/slf4j-log4j12-1.5.9-RC0.jar:target/timingframework-1.0.jar:target/zvtm-cluster-0.2.8-SNAPSHOT.jar fr.inria.zvtm.cluster.SlaveApp -n Calibrator -b $SLAVENUM1 -f -a" $* &
-      sleep 1
-      colIP $col
-      ssh wild@192.168.2.$?$row -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "export DISPLAY=:0.1 && cd /home/wild/zvtm-cluster-calibrator/trunk && java -XX:+DoEscapeAnalysis -XX:+UseConcMarkSweepGC -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr="\"192.168.2.$?$row\"" -Xmx4g -cp target/commons-logging-1.1.jar:target/args4j-2.0.23.jar:target/aspectjrt-1.6.2.jar:target/jgroups-2.7.0.GA.jar:target/log4j-1.2.14.jar:target/slf4j-api-1.5.9-RC0.jar:target/slf4j-log4j12-1.5.9-RC0.jar:target/timingframework-1.0.jar:target/zvtm-cluster-0.2.8-SNAPSHOT.jar fr.inria.zvtm.cluster.SlaveApp -n Calibrator -b $SLAVENUM2 -f -a" $* &
+      SLAVENUM=`expr $? \* 5 + $row - 1`
+      # echo "192.168.2.1"$row
+      # echo $SLAVENUM
+      ssh wild@192.168.2.1$row -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "export DISPLAY=:0.0 && cd /home2/wild/workspace/zvtm/zvtm-cluster-calibrator/trunk && java -XX:+DoEscapeAnalysis -XX:+UseConcMarkSweepGC -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr="\"192.168.2.1$row\"" -Xmx4g -cp target/commons-logging-1.1.jar:target/args4j-2.0.23.jar:target/aspectjrt-1.6.2.jar:target/jgroups-2.7.0.GA.jar:target/log4j-1.2.14.jar:target/slf4j-api-1.5.9-RC0.jar:target/slf4j-log4j12-1.5.9-RC0.jar:target/timingframework-1.0.jar:target/zvtm-cluster-0.2.8-SNAPSHOT.jar fr.inria.zvtm.cluster.SlaveApp -n Calibrator -b $SLAVENUM -f -a" $* &
     done
-done
+
+#start client nodes B
+  for row in {1..5}
+    do
+      SLAVENUM=`expr $? \* 5 + $row - 1 + 5`
+      # echo "192.168.2.2"$row
+      # echo $SLAVENUM
+      ssh wild@192.168.2.2$row -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "export DISPLAY=:0.0 && cd /home2/wild/workspace/zvtm/zvtm-cluster-calibrator/trunk && java -XX:+DoEscapeAnalysis -XX:+UseConcMarkSweepGC -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr="\"192.168.2.2$row\"" -Xmx4g -cp target/commons-logging-1.1.jar:target/args4j-2.0.23.jar:target/aspectjrt-1.6.2.jar:target/jgroups-2.7.0.GA.jar:target/log4j-1.2.14.jar:target/slf4j-api-1.5.9-RC0.jar:target/slf4j-log4j12-1.5.9-RC0.jar:target/timingframework-1.0.jar:target/zvtm-cluster-0.2.8-SNAPSHOT.jar fr.inria.zvtm.cluster.SlaveApp -n Calibrator -b $SLAVENUM -f -a" $* &
+    done
