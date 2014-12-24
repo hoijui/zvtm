9be54a7d7213ac9db8915cc090cbf3f9c27a458f
Fernando del Campo <>
2014-12-24 16:04:36 +0000

    single_cluster_run
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@5258 74e165f3-c697-40a6-91af-552c8936d7fe

--- zuist-cluster-fits/trunk/single_cluster_run.sh
+++ zuist-cluster-fits/trunk/single_cluster_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -XX:+DoEscapeAnalysis -XX:+UseConcMarkSweepGC -Djava.net.preferIPv4Stack=true -Xmx4g -cp target/commons-logging-1.1.jar:target/args4j-2.0.23.jar:target/aspectjrt-1.6.2.jar:target/jgroups-2.7.0.GA.jar:target/log4j-1.2.14.jar:target/slf4j-api-1.5.9-RC0.jar:target/slf4j-log4j12-1.5.9-RC0.jar:target/timingframework-1.0.jar:target/zuist-cluster-fits-0.2.0-SNAPSHOT.jar fr.inria.zvtm.cluster.SlaveApp -n FitsViewer -b 0
+java -XX:+DoEscapeAnalysis -XX:+UseConcMarkSweepGC -Djava.net.preferIPv4Stack=true -Xmx4096M -Dcom.sun.media.jai.disableMediaLib=true -cp target/commons-logging-1.1.jar:target/args4j-2.0.23.jar:target/aspectjrt-1.6.2.jar:target/jgroups-2.7.0.GA.jar:target/log4j-1.2.14.jar:target/slf4j-api-1.5.9-RC0.jar:target/slf4j-log4j12-1.5.9-RC0.jar:target/timingframework-1.0.jar:target/zuist-cluster-fits-0.2.0-SNAPSHOT.jar fr.inria.zvtm.cluster.SlaveApp -n JSkyFitsViewer -b 0
