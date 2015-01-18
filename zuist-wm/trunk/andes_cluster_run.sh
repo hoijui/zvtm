29354b546fa2bb68e3a4d25a4a59c18201be5df7
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2015-01-18 11:28:12 +0000

    dependency and launch script cleanup
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@5293 74e165f3-c697-40a6-91af-552c8936d7fe

--- zuist-wm/trunk/andes_cluster_run.sh
+++ zuist-wm/trunk/andes_cluster_run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+JARS="target/commons-logging-1.1.jar"
+JARS=$JARS":target/args4j-2.0.23.jar"
+JARS=$JARS":target/aspectjrt-1.6.5.jar"
+JARS=$JARS":target/jgroups-2.7.0.GA.jar"
+JARS=$JARS":target/log4j-1.2.17.jar"
+JARS=$JARS":target/slf4j-api-1.7.10.jar"
+JARS=$JARS":target/slf4j-log4j12-1.7.10.jar"
+JARS=$JARS":target/timingframework-1.0.jar"
+JARS=$JARS":target/zuist-wm-0.2.0-SNAPSHOT.jar"
+
 function colNum {
   case "$1" in
           "a" ) return 0;;
@@ -18,8 +28,8 @@ do
       SLAVENUM2=`expr $SLAVENUM1 + 4`
       ssh wall@$col$row.wall.inria.cl -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "sudo sysctl -w net.core.wmem_max=660000"
       ssh wall@$col$row.wall.inria.cl -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "sudo sysctl -w net.core.rmem_max=24000000"
-      ssh wall@$col$row.wall.inria.cl -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "export DISPLAY=:0.0 && cd /home/wall/zvtm-code/zuist-wm/trunk && java -XX:+DoEscapeAnalysis -XX:+UseConcMarkSweepGC -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr="\"$col$row.wall.inria.cl\"" -Xmx4g -cp target/commons-logging-1.1.jar:target/args4j-2.0.23.jar:target/aspectjrt-1.6.2.jar:target/jgroups-2.7.0.GA.jar:target/log4j-1.2.14.jar:target/slf4j-api-1.5.9-RC0.jar:target/slf4j-log4j12-1.5.9-RC0.jar:target/timingframework-1.0.jar:target/zuist-wm-0.2.0-SNAPSHOT.jar fr.inria.zvtm.cluster.SlaveApp -n WildWorldExplorer -b $SLAVENUM1 -f" $* &
+      ssh wall@$col$row.wall.inria.cl -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "export DISPLAY=:0.0 && cd /home/wall/zvtm-code/zuist-wm/trunk && java -XX:+DoEscapeAnalysis -XX:+UseConcMarkSweepGC -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr="\"$col$row.wall.inria.cl\"" -Xmx4g -cp $JARS fr.inria.zvtm.cluster.SlaveApp -n WildWorldExplorer -b $SLAVENUM1 -f" $* &
       sleep 1
-      ssh wall@$col$row.wall.inria.cl -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "export DISPLAY=:0.1 && cd /home/wall/zvtm-code/zuist-wm/trunk && java -XX:+DoEscapeAnalysis -XX:+UseConcMarkSweepGC -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr="\"$col$row.wall.inria.cl\"" -Xmx4g -cp target/commons-logging-1.1.jar:target/args4j-2.0.23.jar:target/aspectjrt-1.6.2.jar:target/jgroups-2.7.0.GA.jar:target/log4j-1.2.14.jar:target/slf4j-api-1.5.9-RC0.jar:target/slf4j-log4j12-1.5.9-RC0.jar:target/timingframework-1.0.jar:target/zuist-wm-0.2.0-SNAPSHOT.jar fr.inria.zvtm.cluster.SlaveApp -n WildWorldExplorer -b $SLAVENUM2 -f" $* &
+      ssh wall@$col$row.wall.inria.cl -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "export DISPLAY=:0.1 && cd /home/wall/zvtm-code/zuist-wm/trunk && java -XX:+DoEscapeAnalysis -XX:+UseConcMarkSweepGC -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr="\"$col$row.wall.inria.cl\"" -Xmx4g -cp $JARS fr.inria.zvtm.cluster.SlaveApp -n WildWorldExplorer -b $SLAVENUM2 -f" $* &
     done
 done
--- zuist-wm/trunk/andes_master_run.sh
+++ zuist-wm/trunk/andes_master_run.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-java -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr="master.wall.inria.cl" -Xmx1g -cp target/args4j-2.0.23.jar:target/aspectjrt-1.6.2.jar:target/jgroups-2.7.0.GA.jar:target/log4j-1.2.14.jar:target/slf4j-api-1.5.9-RC0.jar:target/slf4j-log4j12-1.5.9-RC0.jar:target/timingframework-1.0.jar:target/zuist-cluster-0.2.1-SNAPSHOT.jar:target/commons-logging-1.0.3.jar:target/zuist-wm-0.2.0-SNAPSHOT.jar fr.inria.zuist.app.wm.WildWorldExplorer -r 4 -c 6 -bw 2020 -bh 1180 "$@"
 
+IP="192.168.1.213"
+
+JARS="target/aspectjrt-1.6.5.jar"
+JARS=$JARS":target/jgroups-2.7.0.GA.jar"
+JARS=$JARS":target/log4j-1.2.17.jar"
+JARS=$JARS":target/slf4j-api-1.7.10.jar"
+JARS=$JARS":target/slf4j-log4j12-1.7.10.jar"
+JARS=$JARS":target/timingframework-1.0.jar"
+JARS=$JARS":target/xercesImpl-2.8.1.jar"
+JARS=$JARS":target/xml-apis-1.3.03.jar"
+JARS=$JARS":target/xmlParserAPIs-2.6.2.jar"
+JARS=$JARS":target/zvtm-svg-0.2.1.jar"
+JARS=$JARS":target/commons-logging-1.1.jar"
+JARS=$JARS":target/args4j-2.0.23.jar"
+JARS=$JARS":target/zuist-wm-0.2.0-SNAPSHOT.jar"
+
+java -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=$IP -Xmx1g -cp $JARS fr.inria.zuist.app.wm.WildWorldExplorer -r 4 -c 6 -bw 2020 -bh 1180 "$@"
