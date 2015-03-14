242fecd052d1793f59bb4e13cf607daac988c0f9
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2015-03-14 16:25:59 +0000

    removed useless flags
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@5365 74e165f3-c697-40a6-91af-552c8936d7fe

--- zuist-engine/trunk/run.sh
+++ zuist-engine/trunk/run.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-java -Xmx2048M -Xms512M -Djava.library.path=target/lib -Djogamp.gluegen.UseTempJarCache=false -jar target/zuist-engine-0.4.1-SNAPSHOT.jar "$@"
- 
+java -Xmx2048M -Xms512M -jar target/zuist-engine-0.4.1-SNAPSHOT.jar "$@"
