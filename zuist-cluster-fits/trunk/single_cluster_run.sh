39ecf8f9b4a478dad693fb0cf689d4204fd65e18
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2015-03-23 19:18:53 +0000

    cleaned up dependencies
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@5398 74e165f3-c697-40a6-91af-552c8936d7fe

--- zuist-cluster-fits/trunk/single_cluster_run.sh
+++ zuist-cluster-fits/trunk/single_cluster_run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-java -XX:+DoEscapeAnalysis -XX:+UseConcMarkSweepGC -Djava.net.preferIPv4Stack=true -Xmx4096M -Dcom.sun.media.jai.disableMediaLib=true -cp target/commons-logging-1.1.jar:target/args4j-2.0.23.jar:target/aspectjrt-1.6.2.jar:target/jgroups-2.7.0.GA.jar:target/log4j-1.2.14.jar:target/slf4j-api-1.5.9-RC0.jar:target/slf4j-log4j12-1.5.9-RC0.jar:target/timingframework-1.0.jar:target/zuist-cluster-fits-0.2.0-SNAPSHOT.jar fr.inria.zvtm.cluster.SlaveApp -n JSkyFitsViewer -b 0
+JARS="target/aspectjrt-1.6.5.jar"
+JARS=$JARS":target/args4j-2.0.29.jar"
+JARS=$JARS":target/jgroups-2.7.0.GA.jar"
+JARS=$JARS":target/log4j-1.2.17.jar"
+JARS=$JARS":target/slf4j-api-1.7.10.jar"
+JARS=$JARS":target/slf4j-log4j12-1.7.10.jar"
+JARS=$JARS":target/timingframework-1.0.jar"
+JARS=$JARS":target/commons-logging-1.1.jar"
+JARS=$JARS":target/zvtm-cluster-0.2.8.jar"
+JARS=$JARS":target/zuist-cluster-fits-0.2.0-SNAPSHOT.jar"
+
+java -XX:+DoEscapeAnalysis -XX:+UseConcMarkSweepGC -Djava.net.preferIPv4Stack=true -Xmx4096M -Xms2048M -Dcom.sun.media.jai.disableMediaLib=true -cp $JARS fr.inria.zvtm.cluster.SlaveApp -n JSkyFitsViewer -b 0
--- zuist-cluster-fits/trunk/single_master_run.sh
+++ zuist-cluster-fits/trunk/single_master_run.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-#java -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr="192.168.5.22" -Xmx2048M -cp target/commons-logging-1.1.jar:target/args4j-2.0.23.jar:target/aspectjrt-1.6.2.jar:target/jgroups-2.7.0.GA.jar:target/log4j-1.2.14.jar:target/slf4j-api-1.5.9-RC0.jar:target/slf4j-log4j12-1.5.9-RC0.jar:target/timingframework-1.0.jar:target/zuist-cluster-fits-0.2.0-SNAPSHOT.jar fr.inria.zuist.viewer.FitsViewer -r 1 -c 1 -bw 800 -bh 600 "$@"
+LIB="target/aspectjrt-1.6.5.jar"
+LIB=$LIB":target/args4j-2.0.29.jar"
+LIB=$LIB":target/jgroups-2.7.0.GA.jar"
+LIB=$LIB":target/log4j-1.2.17.jar"
+LIB=$LIB":target/slf4j-api-1.7.10.jar"
+LIB=$LIB":target/slf4j-log4j12-1.7.10.jar"
+LIB=$LIB":target/timingframework-1.0.jar"
+LIB=$LIB":target/commons-logging-1.1.jar"
+LIB=$LIB":target/zvtm-cluster-0.2.8.jar"
+LIB=$LIB":target/zvtm-fits-0.1.4-SNAPSHOT.jar"
+LIB=$LIB":target/zvtm-svg-0.2.2-SNAPSHOT.jar"
 
-java -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr="192.168.1.23" -Xmx2048M -Dcom.sun.media.jai.disableMediaLib=true -cp target/commons-logging-1.1.jar:target/args4j-2.0.23.jar:target/aspectjrt-1.6.2.jar:target/jgroups-2.7.0.GA.jar:target/log4j-1.2.14.jar:target/slf4j-api-1.5.9-RC0.jar:target/slf4j-log4j12-1.5.9-RC0.jar:target/timingframework-1.0.jar:target/zuist-cluster-fits-0.2.0-SNAPSHOT.jar fr.inria.zuist.viewer.JSkyFitsViewer -r 1 -c 1 -bw 800 -bh 600 "$@"
+JAR=target/zuist-cluster-fits-0.2.0-SNAPSHOT.jar
+
+rm target/zvtm-core-0.11.2.jar
+
+ADDR=192.168.1.23
+
+java -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr="$ADDR" -Xmx4096M -Xms2048M -Dcom.sun.media.jai.disableMediaLib=true -cp $LIB:$JAR fr.inria.zuist.viewer.JSkyFitsViewer -r 1 -c 1 -bw 800 -bh 600 "$@"
