89244cb7e12ca902272b8908394f81bf6224e4c6
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2015-03-25 09:25:23 +0000

    wilder launch scripts
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@5403 74e165f3-c697-40a6-91af-552c8936d7fe

---
+++ zuist-cluster-fits/trunk/wilder_master_run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+IP="192.168.2.2"
+
+LIB="target/aspectjrt-1.6.5.jar"
+LIB=$LIB":target/args4j-2.0.29.jar"
+LIB=$LIB":target/jgroups-2.7.0.GA.jar"
+LIB=$LIB":target/log4j-1.2.17.jar"
+LIB=$LIB":target/slf4j-api-1.7.10.jar"
+LIB=$LIB":target/slf4j-log4j12-1.7.10.jar"
+LIB=$LIB":target/timingframework-1.0.jar"
+LIB=$LIB":target/commons-logging-1.1.jar"
+LIB=$LIB":target/zvtm-cluster-0.2.8.jar"
+LIB=$LIB":target/zvtm-fits-0.1.4-SNAPSHOT.jar"
+LIB=$LIB":target/zvtm-svg-0.2.2-SNAPSHOT.jar"
+
+JAR=target/zuist-cluster-fits-0.2.0-SNAPSHOT.jar
+
+java -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=$IP -Xmx4096M -Xms2048M -Dcom.sun.media.jai.disableMediaLib=true -cp $LIB:$JAR fr.inria.zuist.viewer.JSkyFitsViewer -r 5 -c 15 -bw 960 -bh 960 "$@"
