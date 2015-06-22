774ced76c6527562b5515a56abd75a03a18fc699
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2015-06-22 07:43:51 +0000

    ivoafits available locally in lib/
    git-svn-id: file:///home/hoijui/src/IsaViz/tmp2/zvtm.svn/code@5630 74e165f3-c697-40a6-91af-552c8936d7fe

--- zvtm-fits/trunk/prebuild.sh
+++ zvtm-fits/trunk/prebuild.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-#Download and add the ivoa.fits archive to the local 
+#Download and add the ivoa.fits archive to the local
 #maven repository.
 PREBUILD_ROOT=`pwd`
 
 cd `mktemp -d` &&\
-wget http://skyservice.pha.jhu.edu/develop/vo/ivoafits/ivoafits-0.3.jar &&\
-mvn install:install-file -DgroupId=ivoa.fits -DartifactId=fits -Dversion=0.3.0 -Dpackaging=jar -Dfile=ivoafits-0.3.jar &&\
+#wget http://skyservice.pha.jhu.edu/develop/vo/ivoafits/ivoafits-0.3.jar &&\
+mvn install:install-file -DgroupId=ivoa.fits -DartifactId=fits -Dversion=0.3.0 -Dpackaging=jar -Dfile=$PREBUILD_ROOT/lib/ivoafits-0.3.jar &&\
 mvn install:install-file -DgroupId=jsky -DartifactId=jsky -Dversion=3.0 -Dpackaging=jar -Dfile=$PREBUILD_ROOT/lib/jsky-3.0.jar &&
 mvn install:install-file -DgroupId=hcompress -DartifactId=hcompress -Dversion=0.52 -Dpackaging=jar -Dfile=$PREBUILD_ROOT/lib/hcompress-0.52.jar
-
